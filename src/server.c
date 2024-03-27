174bb38d6dcb3a8336ffeda8a77f873331cbb2c6
Roshan Khatri <rvkhatri@amazon.com>
2024-03-27 22:36:33 +0000

    Adds new Placeholderkv logo to server logs.
    Signed-off-by: Roshan Khatri <rvkhatri@amazon.com>

--- src/server.c
+++ src/server.c
@@ -4526,7 +4526,7 @@ int finishShutdown(void) {
 
 
     serverLog(LL_WARNING,"%s is now ready to exit, bye bye...",
-        server.sentinel_mode ? "Sentinel" : "Redis");
+        server.sentinel_mode ? "Sentinel" : "Placeholderkv");
     return C_OK;
 
 error:
@@ -7152,9 +7152,9 @@ int main(int argc, char **argv) {
     int background = server.daemonize && !server.supervised;
     if (background) daemonize();
 
-    serverLog(LL_NOTICE, "oO0OoO0OoO0Oo Redis is starting oO0OoO0OoO0Oo");
+    serverLog(LL_NOTICE, "oO0OoO0OoO0Oo Placeholderkv is starting oO0OoO0OoO0Oo");
     serverLog(LL_NOTICE,
-        "Redis version=%s, bits=%d, commit=%s, modified=%d, pid=%d, just started",
+        "Placeholderkv version=%s, bits=%d, commit=%s, modified=%d, pid=%d, just started",
             REDIS_VERSION,
             (sizeof(long) == 8) ? 64 : 32,
             redisGitSHA1(),

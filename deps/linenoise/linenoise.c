289eb47eb96d30f08543d721880fe99b623abd48
Mikel Olasagasti Uranga <mikel@olasagasti.info>
2024-03-30 14:23:50 +0100

    Replace offensive term (#86)
    Signed-off-by: Mikel Olasagasti Uranga <mikel@olasagasti.info>

--- deps/linenoise/linenoise.c
+++ deps/linenoise/linenoise.c
@@ -233,7 +233,7 @@ static int isUnsupportedTerm(void) {
     return 0;
 }
 
-/* Raw mode: 1960 magic shit. */
+/* Raw mode: 1960's magic. */
 static int enableRawMode(int fd) {
     struct termios raw;
 

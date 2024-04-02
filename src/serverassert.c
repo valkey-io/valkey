74fd92f991b25971d28fc904306f7a919df8a351
0del <bany.y0599@gmail.com>
2024-04-02 22:17:49 +0700

    fix: remove 'redisassert.h' in comment
    Signed-off-by: 0del <bany.y0599@gmail.com>

--- src/serverassert.c
+++ src/serverassert.c
@@ -1,8 +1,8 @@
-/* redisassert.c -- Implement the default _serverAssert and _serverPanic which 
+/* serverassert.c -- Implement the default _serverAssert and _serverPanic which 
  * simply print stack trace to standard error stream.
  * 
  * This file is shared by those modules that try to print some logs about stack trace 
- * but don't have their own implementations of functions in redisassert.h.
+ * but don't have their own implementations of functions in serverassert.h.
  *
  * ----------------------------------------------------------------------------
  *

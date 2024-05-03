5b1fd222ed97430dfeef27a95aeb7e1fb8fd1352
Madelyn Olson <madelyneolson@gmail.com>
2024-05-02 20:00:04 -0700

    An initial simple unit test framework (#344)
    The core idea was to take a lot of the stuff from the C unity framework
    and adapt it a bit here. Each file in the `unit` directory that starts
    with `test_` is automatically assumed to be a test suite. Within each
    file, all functions that start with `test_` are assumed to be a test.
    
    See unit/README.md for details about the implementation.
    
    Instead of compiling basically a net new binary, the way the tests are
    compiled is that the main valkey server is compiled as a static archive,
    which we then compile the individual test files against to create a new
    test executable. This is not all that important now, other than it makes
    the compilation simpler, but what it will allow us to do is overwrite
    functions in the archive to enable mocking for cross compilation unit
    functions. There are also ways to enable mocking from within the same
    compilation unit, but I don't know how important this is.
    
    Tests are also written in one of two styles:
    1. Including the header file and directly calling functions from the
    archive.
    2. Importing the original file, and then calling the functions. This
    second approach is cool because we can call static functions. It won't
    mess up the archive either.
    
    ---------
    
    Signed-off-by: Madelyn Olson <madelyneolson@gmail.com>

---
+++ src/unit/test_files.h
@@ -0,0 +1,31 @@
+/* Do not modify this file, it's automatically generated from utils/generate-unit-test-header.py */
+typedef int unitTestProc(int argc, char **argv, int flags);
+
+typedef struct unitTest {
+    char *name;
+    unitTestProc *proc;
+} unitTest;
+
+int test_crc64(int argc, char **argv, int flags);
+int test_crc64combine(int argc, char **argv, int flags);
+int test_intsetValueEncodings(int argc, char **argv, int flags);
+int test_intsetBasicAdding(int argc, char **argv, int flags);
+int test_intsetLargeNumberRandomAdd(int argc, char **argv, int flags);
+int test_intsetUpgradeFromint16Toint32(int argc, char **argv, int flags);
+int test_intsetUpgradeFromint16Toint64(int argc, char **argv, int flags);
+int test_intsetUpgradeFromint32Toint64(int argc, char **argv, int flags);
+int test_intsetStressLookups(int argc, char **argv, int flags);
+int test_intsetStressAddDelete(int argc, char **argv, int flags);
+
+unitTest __test_crc64_c[] = {{"test_crc64", test_crc64}, {NULL, NULL}};
+unitTest __test_crc64combine_c[] = {{"test_crc64combine", test_crc64combine}, {NULL, NULL}};
+unitTest __test_intset_c[] = {{"test_intsetValueEncodings", test_intsetValueEncodings}, {"test_intsetBasicAdding", test_intsetBasicAdding}, {"test_intsetLargeNumberRandomAdd", test_intsetLargeNumberRandomAdd}, {"test_intsetUpgradeFromint16Toint32", test_intsetUpgradeFromint16Toint32}, {"test_intsetUpgradeFromint16Toint64", test_intsetUpgradeFromint16Toint64}, {"test_intsetUpgradeFromint32Toint64", test_intsetUpgradeFromint32Toint64}, {"test_intsetStressLookups", test_intsetStressLookups}, {"test_intsetStressAddDelete", test_intsetStressAddDelete}, {NULL, NULL}};
+
+struct unitTestSuite {
+    char *filename;
+    unitTest *tests;
+} unitTestSuite[] = {
+    {"test_crc64.c", __test_crc64_c},
+    {"test_crc64combine.c", __test_crc64combine_c},
+    {"test_intset.c", __test_intset_c},
+};

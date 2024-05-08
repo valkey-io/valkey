da123925bd43a553a22b7dabb9d31e62d6ec66c2
Vadym Khoptynets <vadymkh@amazon.com>
2024-05-08 18:54:08 +0000

    Adjust sds types
    sds type should be determined based on the size of the underlying
    buffer, not the logical length of the sds. Currently we truncate the
    alloc field in case buffer is larger than we can handle. It leads to a
    mismatch between alloc field and the actual size of the buffer. Even
    considering that alloc doesn't include header size and the null
    terminator.
    
    It also leads to a waste of memory with jemalloc. For example, let's
    consider creation of sds of length 253. According to the length, the
    appropriate type is SDS_TYPE_8. But we allocate `253 + sizeof(struct
    sdshdr8) + 1` bytes, which sums to 257 bytes. In this case jemalloc
    allocates buffer from the next size bucket. With current configuration
    on Linux it's 320 bytes. So we end up with 320 bytes buffer, while we
    can't address more than 255.
    
    The same happens with other types and length close enough to the
    appropriate powers of 2.
    
    The downside of the adjustment is that with allocators that do not
    allocate larger than requested chunks (like GNU allocator), we switch
    to a larger type "too early". It leads to small waste of
    memory. Specifically: sds of length 31 takes 35 bytes instead of 33 (2
    bytes wasted) sds of length 255 takes 261 bytes instead of 259 (2
    bytes wasted) sds of length 65,535 takes 65,545 bytes instead of
    65,541 (4 bytes wasted) sds of length 4,294,967,295 takes
    4,294,967,313 bytes instead of 4,294,967,305 (8 bytes wasted)
    
    Signed-off-by: Vadym Khoptynets <vadymkh@amazon.com>

--- src/sds.c
+++ src/sds.c
@@ -54,11 +54,12 @@ static inline int sdsHdrSize(char type) {
 }
 
 static inline char sdsReqType(size_t string_size) {
-    if (string_size < 1 << 5) return SDS_TYPE_5;
-    if (string_size < 1 << 8) return SDS_TYPE_8;
-    if (string_size < 1 << 16) return SDS_TYPE_16;
+    if (sizeof(struct sdshdr5) + string_size + 1 <= 1<<5) return SDS_TYPE_5;
+    if (sizeof(struct sdshdr8) + string_size + 1 <= 1<<8) return SDS_TYPE_8;
+    if (sizeof(struct sdshdr16) + string_size + 1 <= 1<<16) return SDS_TYPE_16;
 #if (LONG_MAX == LLONG_MAX)
-    if (string_size < 1ll << 32) return SDS_TYPE_32;
+    if (sizeof(struct sdshdr32) + string_size + 1 <= 1ll<<32)
+        return SDS_TYPE_32;
     return SDS_TYPE_64;
 #else
     return SDS_TYPE_32;

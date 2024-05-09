16d5af278dae1a9024093c7801c75bdeb58573fd
Shivshankar-Reddy <shiva.sheri.github@gmail.com>
2024-05-09 17:37:25 +0000

    add asserts for the expected values
    Signed-off-by: Shivshankar-Reddy <shiva.sheri.github@gmail.com>
    Signed-off-by: hwware <wen.hui.ware@gmail.com>

--- src/unit/test_zipmap.c
+++ src/unit/test_zipmap.c
@@ -34,7 +34,7 @@ static void zipmapRepr(unsigned char *p) {
     printf("\n");
 }
 
-int test_zipmap(int argc, char *argv[], int flags) {
+int test_zipmapLookUpLargeKey(int argc, char *argv[], int flags) {
     unsigned char *zm;
 
     UNUSED(argc);
@@ -69,10 +69,41 @@ int test_zipmap(int argc, char *argv[], int flags) {
 
         zm = zipmapSet(zm,buf,512,(unsigned char*) "long",4,NULL);
         if (zipmapGet(zm,buf,512,&value,&vlen)) {
-            printf("  <long key> is associated to the %d bytes value: %.*s\n",
-                vlen, vlen, value);
+            printf("  <long key> is associated to the %d bytes value: %.*s val:%s\n",
+                vlen, vlen, value,value);
+            TEST_ASSERT(4 == vlen);
+            TEST_ASSERT(strncmp("long", (const char*)value, vlen) == 0);
         }
     }
+    zfree(zm);
+    return 0;
+}
+
+int test_zipmapPerformDirectLookup(int argc, char *argv[], int flags) {
+    unsigned char *zm;
+
+    UNUSED(argc);
+    UNUSED(argv);
+    UNUSED(flags);
+
+    zm = zipmapNew();
+
+    zm = zipmapSet(zm,(unsigned char*) "name",4, (unsigned char*) "foo",3,NULL);
+    zm = zipmapSet(zm,(unsigned char*) "surname",7, (unsigned char*) "foo",3,NULL);
+    zm = zipmapSet(zm,(unsigned char*) "age",3, (unsigned char*) "foo",3,NULL);
+    zipmapRepr(zm);
+
+    zm = zipmapSet(zm,(unsigned char*) "hello",5, (unsigned char*) "world!",6,NULL);
+    zm = zipmapSet(zm,(unsigned char*) "foo",3, (unsigned char*) "bar",3,NULL);
+    zm = zipmapSet(zm,(unsigned char*) "foo",3, (unsigned char*) "!",1,NULL);
+    zipmapRepr(zm);
+    zm = zipmapSet(zm,(unsigned char*) "foo",3, (unsigned char*) "12345",5,NULL);
+    zipmapRepr(zm);
+    zm = zipmapSet(zm,(unsigned char*) "new",3, (unsigned char*) "xx",2,NULL);
+    zm = zipmapSet(zm,(unsigned char*) "noval",5, (unsigned char*) "",0,NULL);
+    zipmapRepr(zm);
+    zm = zipmapDel(zm,(unsigned char*) "new",3,NULL);
+    zipmapRepr(zm);
 
     printf("\nPerform a direct lookup:\n");
     {
@@ -82,16 +113,60 @@ int test_zipmap(int argc, char *argv[], int flags) {
         if (zipmapGet(zm,(unsigned char*) "foo",3,&value,&vlen)) {
             printf("  foo is associated to the %d bytes value: %.*s\n",
                 vlen, vlen, value);
+            TEST_ASSERT(5 == vlen);
+            TEST_ASSERT(strcmp("12345", (const char*)value));
         }
     }
+    zfree(zm);
+    return 0;
+}
+
+int test_zipmapIterateThroughElements(int argc, char *argv[], int flags) {
+    unsigned char *zm;
+
+    UNUSED(argc);
+    UNUSED(argv);
+    UNUSED(flags);
+
+    zm = zipmapNew();
+
+    zm = zipmapSet(zm,(unsigned char*) "name",4, (unsigned char*) "foo",3,NULL);
+    zm = zipmapSet(zm,(unsigned char*) "surname",7, (unsigned char*) "foo",3,NULL);
+    zm = zipmapSet(zm,(unsigned char*) "age",3, (unsigned char*) "foo",3,NULL);
+    zipmapRepr(zm);
+
+    zm = zipmapSet(zm,(unsigned char*) "hello",5, (unsigned char*) "world!",6,NULL);
+    zm = zipmapSet(zm,(unsigned char*) "foo",3, (unsigned char*) "bar",3,NULL);
+    zm = zipmapSet(zm,(unsigned char*) "foo",3, (unsigned char*) "!",1,NULL);
+    zipmapRepr(zm);
+    zm = zipmapSet(zm,(unsigned char*) "foo",3, (unsigned char*) "12345",5,NULL);
+    zipmapRepr(zm);
+    zm = zipmapSet(zm,(unsigned char*) "new",3, (unsigned char*) "xx",2,NULL);
+    zm = zipmapSet(zm,(unsigned char*) "noval",5, (unsigned char*) "",0,NULL);
+    zipmapRepr(zm);
+    zm = zipmapDel(zm,(unsigned char*) "new",3,NULL);
+    zipmapRepr(zm);
+
     printf("\nIterate through elements:\n");
     {
         unsigned char *i = zipmapRewind(zm);
         unsigned char *key, *value;
         unsigned int klen, vlen;
+        char* expected_key[] = {"name", "surname", "age", "hello", "foo", "noval"};
+        char* expected_value[] = {"foo", "foo", "foo", "world!", "12345", NULL};
+        unsigned int expected_klen[] = {4,7,3,5,3,5};
+        unsigned int expected_vlen[] = {3,3,3,6,5,0};
+        int iter = 0;
 
         while((i = zipmapNext(i,&key,&klen,&value,&vlen)) != NULL) {
             printf("  %d:%.*s => %d:%.*s\n", klen, klen, key, vlen, vlen, value);
+            char *tmp = expected_key[iter];
+            TEST_ASSERT(klen == expected_klen[iter]);
+            TEST_ASSERT(strncmp((const char*)tmp, (const char*)key, klen) == 0);
+            tmp = expected_value[iter];
+            TEST_ASSERT(vlen == expected_vlen[iter]);
+            TEST_ASSERT(strncmp((const char*)tmp, (const char*)value, vlen) == 0);
+            iter++;
         }
     }
     zfree(zm);

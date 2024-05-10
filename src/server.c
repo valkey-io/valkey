4949bdc8165e07d1a58329afdcaee0169e698632
Karthick Ariyaratnam <karthyuom@gmail.com>
2024-05-10 17:24:28 +0000

    Remove endiantest.
    Signed-off-by: Karthick Ariyaratnam <karthyuom@gmail.com>

--- src/server.c
+++ src/server.c
@@ -6905,7 +6905,6 @@ struct serverTest {
     {"ziplist", ziplistTest},
     {"quicklist", quicklistTest},
     {"zipmap", zipmapTest},
-    {"endianconv", endianconvTest},
     {"zmalloc", zmalloc_test},
     {"sds", sdsTest},
     {"dict", dictTest},

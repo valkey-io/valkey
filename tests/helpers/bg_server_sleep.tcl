5943fd079b80d334d5975af1999bab55160417dd
naglera <anagler123@gmail.com>
2024-05-22 16:10:36 +0000

    Rename redis to valkey in bg_server_sleep
    Signed-off-by: naglera <anagler123@gmail.com>

--- tests/helpers/bg_server_sleep.tcl
+++ tests/helpers/bg_server_sleep.tcl
@@ -1,8 +1,8 @@
-source tests/support/redis.tcl
+source tests/support/valkey.tcl
 source tests/support/util.tcl
 
 proc bg_server_sleep {host port sec} {
-    set r [redis $host $port 0]
+    set r [valkey $host $port 0]
     $r client setname SLEEP_HANDLER
     $r debug sleep $sec
 }

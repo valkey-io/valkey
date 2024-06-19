ae2d4217e147996bd6c546f559aa564f873f9203
kukey <wei.kukey@gmail.com>
2024-06-19 08:48:58 +0800

    Add new SCRIPT SHOW subcommand to dump script via sha1 (#617)
    In some scenarios, the business may not be able to find the
    previously used Lua script and only have a SHA signature.
    Or there are multiple identical evalsha's args in monitor/slowlog,
    and admin is not able to distinguish the script body.
    
    Add a new script subcommmand to show the contents of script
    given the scripts sha1. Returns a NOSCRIPT error if the script
    is not present in the cache.
    
    Usage: `SCRIPT SHOW sha1`
    Complexity: `O(1)`
    
    Closes #604.
    Doc PR: https://github.com/valkey-io/valkey-doc/pull/143
    
    ---------
    
    Signed-off-by: wei.kukey <wei.kukey@gmail.com>
    Signed-off-by: Madelyn Olson <madelyneolson@gmail.com>
    Co-authored-by: Madelyn Olson <madelyneolson@gmail.com>
    Co-authored-by: Binbin <binloveplay1314@qq.com>
    Co-authored-by: Viktor Söderqvist <viktor.soderqvist@est.tech>

--- src/server.c
+++ src/server.c
@@ -1814,7 +1814,7 @@ void createSharedObjects(void) {
     shared.syntaxerr = createObject(OBJ_STRING, sdsnew("-ERR syntax error\r\n"));
     shared.sameobjecterr = createObject(OBJ_STRING, sdsnew("-ERR source and destination objects are the same\r\n"));
     shared.outofrangeerr = createObject(OBJ_STRING, sdsnew("-ERR index out of range\r\n"));
-    shared.noscripterr = createObject(OBJ_STRING, sdsnew("-NOSCRIPT No matching script. Please use EVAL.\r\n"));
+    shared.noscripterr = createObject(OBJ_STRING, sdsnew("-NOSCRIPT No matching script.\r\n"));
     createSharedObjectsWithCompat();
     shared.primarydownerr = createObject(
         OBJ_STRING, sdsnew("-MASTERDOWN Link with MASTER is down and replica-serve-stale-data is set to 'no'.\r\n"));

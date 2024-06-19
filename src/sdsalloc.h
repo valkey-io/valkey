0143b7c9dd7ec38fec5469a1055d96e8f4f0984c
poiuj <1099644+poiuj@users.noreply.github.com>
2024-06-20 02:13:55 +0300

    Add zfree_with_size to optimize sdsfree since we can get zmalloc_size from the header (#453)
    ### Description ###
    zfree updates memory statistics. It gets the size of the buffer from
    jemalloc by calling zmalloc_size. This operation is costly. We can avoid
    it if we know the buffer size. For example, we can calculate size of sds
    from the data we have in its header.
    
    This commit introduces zfree_with_size function that accepts both
    pointer to a buffer, and its size. zfree is refactored to call
    zfree_with_size.
    
    sdsfree uses the new interface for all but SDS_TYPE_5.
    
    ### Benchmark ###
    
    Dataset is 3 million strings. Each benchmark run uses its own value size
    (8192, 512, and 120). The benchmark is 100% write load for 5 minutes.
    
    ```
    value size       new tps      old tps      %       new us/call    old us/call    %
    8k               272088.53    269971.75    0.78    1.83           1.92           -4.69
    512              356881.91    352856.72    1.14    1.27           1.35           -5.93
    120              377523.81    368774.78    2.37    1.14           1.19           -4.20
    ```
    
    ---------
    
    Signed-off-by: Vadym Khoptynets <vadymkh@amazon.com>
    Signed-off-by: Madelyn Olson <madelyneolson@gmail.com>
    Co-authored-by: Madelyn Olson <madelyneolson@gmail.com>

--- src/sdsalloc.h
+++ src/sdsalloc.h
@@ -45,6 +45,7 @@
 #define s_trymalloc ztrymalloc
 #define s_tryrealloc ztryrealloc
 #define s_free zfree
+#define s_free_with_size zfree_with_size
 #define s_malloc_usable zmalloc_usable
 #define s_realloc_usable zrealloc_usable
 #define s_trymalloc_usable ztrymalloc_usable

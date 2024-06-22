0e0b7c1d8ed85ef25bec0e97095cd4e6afccc615
Lipeng Zhu <lipeng.zhu@intel.com>
2024-06-21 20:18:13 -0400

    embed the struct io_uring itself instead of a pointer.
    Signed-off-by: Lipeng Zhu <lipeng.zhu@intel.com>

--- src/io_uring.c
+++ src/io_uring.c
@@ -9,30 +9,28 @@
 #define IO_URING_DEPTH 256 /* io_uring instance queue depth. */
 
 struct io_uring_context {
-    struct io_uring *ring;
+    struct io_uring ring;
     size_t queue_len;
 };
 
 io_uring_context *createIOUring(void) {
     io_uring_context *uring_context = zmalloc(sizeof(struct io_uring_context));
+    uring_context->queue_len = 0;
     struct io_uring_params params;
-    struct io_uring *ring = zmalloc(sizeof(struct io_uring));
     memset(&params, 0, sizeof(params));
     /* On success, io_uring_queue_init_params(3) returns 0 and ring will
      * point to the shared memory containing the io_uring queues.
      * On failure -errno is returned. */
-    if (io_uring_queue_init_params(IO_URING_DEPTH, ring, &params) < 0) return NULL;
-    uring_context->ring = ring;
-    uring_context->queue_len = 0;
+    if (io_uring_queue_init_params(IO_URING_DEPTH, &uring_context->ring, &params) < 0) return NULL;
     return uring_context;
 }
 
 /* Submit fdatasync request to io_uring. */
 int ioUringPrepFsyncAndSubmit(io_uring_context *ring_context, int fd) {
-    struct io_uring_sqe *sqe = io_uring_get_sqe(ring_context->ring);
+    struct io_uring_sqe *sqe = io_uring_get_sqe(&ring_context->ring);
     io_uring_prep_fsync(sqe, fd, IORING_FSYNC_DATASYNC);
     ring_context->queue_len++;
-    int ret = io_uring_submit(ring_context->ring);
+    int ret = io_uring_submit(&ring_context->ring);
     if (ret < 0) {
         errno = -ret;
         return IO_URING_ERR;
@@ -43,13 +41,13 @@ int ioUringPrepFsyncAndSubmit(io_uring_context *ring_context, int fd) {
 int ioUringWaitFsyncBarrier(io_uring_context *ring_context) {
     struct io_uring_cqe *cqe;
     while (ring_context->queue_len) {
-        int ret = io_uring_wait_cqe(ring_context->ring, &cqe);
+        int ret = io_uring_wait_cqe(&ring_context->ring, &cqe);
         if (ret == 0) {
             if (cqe->res < 0) {
                 errno = -cqe->res;
                 return IO_URING_ERR;
             }
-            io_uring_cqe_seen(ring_context->ring, cqe);
+            io_uring_cqe_seen(&ring_context->ring, cqe);
             ring_context->queue_len--;
         } else {
             if (ret < 0) {
@@ -63,7 +61,7 @@ int ioUringWaitFsyncBarrier(io_uring_context *ring_context) {
 
 void freeIOUring(io_uring_context *ring_context) {
     if (ring_context) {
-        io_uring_queue_exit(ring_context->ring);
+        io_uring_queue_exit(&ring_context->ring);
         zfree(ring_context);
     }
 }

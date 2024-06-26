naglera/placeholderkv

Rdb channel for full sync
In this PR we introduce the main benefit of rdb channel by continuously
steaming the COB in parallel to the RDB and thus keeping the primary
side COB small AND accelerating the overall sync process.
By streaming the replication data to the replica during the full sync,
we reduce
1. Memory load from the primary node.
2. CPU load from the primary main process (will be introduced in later
   PR).
This opens up possibilities to future improvements with better
TLS connection handling and removal of the the need to pipeline the RDB
from the child process to the main.

Squashed commit of the following:

commit 7d4756681655ae1c6e04681710740e422a6def4c
Merge: 97894e318 72538622f
Author: naglera <anagler123@gmail.com>
Date:   Wed May 22 08:15:26 2024 +0000

    Merge branch 'unstable' into rdb-channel

commit 97894e318198438ce2f32fba5f530572e93ca345
Author: Amit Nagler <amitnagl@amazon.com>
Date:   Sun May 19 15:21:58 2024 +0000

    fix psync stat error count

    rdb channel sync should also count psync failures.
    This commit fix tests "replication partial resync" to run with rdb
    channel enabled

commit a51b0a541d8bf303d132181c056516fbd24e5afa
Author: Amit Nagler <amitnagl@amazon.com>
Date:   Sun May 19 11:06:04 2024 +0000

    fix test PSYNC2: Full resync after Master restart when too many key expired

commit 96c93f3f8cf11a92ae4ebacff3443375e007806d
Author: Amit Nagler <amitnagl@amazon.com>
Date:   Sun May 19 09:17:45 2024 +0000

    Allow auth using rdb channel

commit 9916f80fed647dc66252b34442c747532154d5f3
Author: Amit Nagler <amitnagl@amazon.com>
Date:   Thu May 16 12:59:44 2024 +0000

    fix use after free error & adjust test's configuration

commit c34326198f27096b3dda102d428d7f6382989342
Author: Amit Nagler <amitnagl@amazon.com>
Date:   Thu May 16 12:19:33 2024 +0000

    spell check fix

commit e2decc4934326b8bc5c77737062e89b414ecdfd8
Author: Amit Nagler <amitnagl@amazon.com>
Date:   Tue May 7 13:54:26 2024 +0000

    Fix replicat

tests/unit/auth.tcl
@@ -47,6 +47,7 @@ start_server {tags {"auth external:skip"} overrides {requirepass foobar}} {
     }
 }
 
+foreach rdbchann {yes no} {
 start_server {tags {"auth_binary_password external:skip"}} {
     test {AUTH fails when binary password is wrong} {
         r config set requirepass "abc\x00def"
@@ -65,12 +66,13 @@ start_server {tags {"auth_binary_password external:skip"}} {
         set master_port [srv -1 port]
         set slave [srv 0 client]
 
-        test {MASTERAUTH test with binary password} {
+        test "MASTERAUTH test with binary password rdbchannel = $rdbchann" {
             $master config set requirepass "abc\x00def"
-
+            $master config set repl-rdb-channel $rdbchann
             # Configure the replica with masterauth
             set loglines [count_log_lines 0]
             $slave config set masterauth "abc"
+            $slave config set repl-rdb-channel $rdbchann
             $slave slaveof $master_host $master_port
 
             # Verify replica is not able to sync with master
@@ -86,4 +88,5 @@ start_server {tags {"auth_binary_password external:skip"}} {
             }
         }
     }
+}
 }

valkey-io/valkey

tests/unit/auth.tcl
@@ -47,6 +47,7 @@ start_server {tags {"auth external:skip"} overrides {requirepass foobar}} {
     }
 }
 
+foreach rdbchann {yes no} {
 start_server {tags {"auth_binary_password external:skip"}} {
     test {AUTH fails when binary password is wrong} {
         r config set requirepass "abc\x00def"
@@ -65,12 +66,13 @@ start_server {tags {"auth_binary_password external:skip"}} {
         set master_port [srv -1 port]
         set slave [srv 0 client]
 
-        test {primaryauth test with binary password} {
+        test "primaryauth test with binary password rdbchannel = $rdbchann" {
             $master config set requirepass "abc\x00def"
-
-            # Configure the replica with primaryauth
+            $master config set repl-rdb-channel $rdbchann
+            # Configure the replica with masterauth
             set loglines [count_log_lines 0]
             $slave config set primaryauth "abc"
+            $slave config set repl-rdb-channel $rdbchann
             $slave slaveof $master_host $master_port
 
             # Verify replica is not able to sync with master
@@ -86,4 +88,5 @@ start_server {tags {"auth_binary_password external:skip"}} {
             }
         }
     }
+}
 }
20287431d800100df6d49ea647ba869fa6596020
Viktor Söderqvist <viktor.soderqvist@est.tech>
2024-07-02 17:05:36 +0200

    Add tests for CLUSTER SHARDS and CLUSTER NODES
    Signed-off-by: Viktor Söderqvist <viktor.soderqvist@est.tech>

--- tests/unit/cluster/announce-client-ip.tcl
+++ tests/unit/cluster/announce-client-ip.tcl
@@ -8,13 +8,36 @@ start_cluster 2 2 {tags {external:skip cluster} overrides {cluster-replica-no-fa
             puts "CONFIG SET for node $j: $res"
         }
 
+        # CLUSTER SLOTS
         wait_for_condition 50 100 {
             [are_cluster_announced_ips_propagated {111.222.111.*}]
         } else {
             fail "cluster-announce-client-ipv4 were not propagated"
         }
 
-        # Redirects use the IP address
+        # CLUSTER SHARDS
+        for {set j 0} {$j < [llength $::servers]} {incr j} {
+            foreach shard [R $j CLUSTER SHARDS] {
+                foreach node [dict get $shard "nodes"] {
+                    set ip [dict get $node "ip"]
+                    set endpoint [dict get $node "endpoint"]
+                    assert_match "111.222.111*" $ip
+                    assert_match "111.222.111*" $endpoint
+                }
+            }
+        }
+
+        # CLUSTER NODES
+        for {set j 0} {$j < [llength $::servers]} {incr j} {
+            set lines [split [R $j CLUSTER NODES] "\r\n"]
+            foreach l $lines {
+                set l [string trim $l]
+                if {$l eq {}} continue
+                assert_equal 1 [regexp {^[0-9a-f]+ 111\.222\.111\.[0-9]} $l]
+            }
+        }
+
+        # Redirects
         catch {R 0 set foo foo} e
         assert_match "MOVED * 111.222.111*:*" $e
 
@@ -34,13 +57,36 @@ start_cluster 2 2 {tags {external:skip cluster} overrides {cluster-replica-no-fa
             lappend clients [valkey ::1 [srv $level port] 0 $::tls]
         }
 
+        # CLUSTER SLOTS
         wait_for_condition 50 100 {
             [are_cluster_announced_ips_propagated "cafe:1234::*" $clients] eq 1
         } else {
             fail "cluster-announce-client-ipv6 were not propagated"
         }
 
-        # Redirect use the IP address
+        # CLUSTER SHARDS
+        for {set j 0} {$j < [llength $::servers]} {incr j} {
+            foreach shard [[lindex $clients $j] CLUSTER SHARDS] {
+                foreach node [dict get $shard "nodes"] {
+                    set ip [dict get $node "ip"]
+                    set endpoint [dict get $node "endpoint"]
+                    assert_match "cafe:1234::*" $ip
+                    assert_match "cafe:1234::*" $endpoint
+                }
+            }
+        }
+
+        # CLUSTER NODES
+        for {set j 0} {$j < [llength $::servers]} {incr j} {
+            set lines [split [[lindex $clients $j] CLUSTER NODES] "\r\n"]
+            foreach l $lines {
+                set l [string trim $l]
+                if {$l eq {}} continue
+                assert_equal 1 [regexp {^[0-9a-f]+ cafe:1234::[0-9]} $l]
+            }
+        }
+
+        # Redirects
         catch {[lindex $clients 0] set foo foo} e
         assert_match "MOVED * cafe:1234::*:*" $e
 

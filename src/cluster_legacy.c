VoletiRam/valkey_ram

Remove topology argument and cleanup related code changes
Remove topology argument and cleanup related code changes.

Signed-off-by: Ram Prasad Voleti <ramvolet@amazon.com>

src/cluster_legacy.c
@@ -64,8 +64,8 @@ void clusterUpdateState(void);
 int clusterNodeCoversSlot(clusterNode *n, int slot);
 list *clusterGetNodesInMyShard(clusterNode *node);
 int clusterNodeAddSlave(clusterNode *master, clusterNode *slave);
-int clusterNodeAddMaster(clusterNode *master);
-int clusterNodeRemoveMaster(clusterNode *master);
+int clusterNodeAddToMasters(clusterNode *master);
+int clusterNodeRemoveFromMasters(clusterNode *master);
 int clusterAddSlot(clusterNode *n, int slot);
 int clusterDelSlot(int slot);
 int clusterDelNodeSlots(clusterNode *node);
@@ -231,7 +231,7 @@ int auxShardIdSetter(clusterNode *n, void *value, int length) {
     }
     /* Initially, during the load, add every node as master until the respective
      * role is assigned with the persisted shard ID. */
-    clusterNodeAddMaster(n);
+    clusterNodeAddToMasters(n);
     return C_OK;
 }
 
@@ -598,7 +598,7 @@ int clusterLoadConfig(char *filename) {
             /* Since the role of node is decided as replica, remove it from
              * master list which was added initially during the load and continue
              * maintain the persisted master list */
-            clusterNodeRemoveMaster(n);
+            clusterNodeRemoveFromMasters(n);
             n->slaveof = master;
             clusterNodeAddSlave(master,n);
         } else if (auxFieldHandlers[af_shard_id].isPresent(n) == 0) {
@@ -606,7 +606,7 @@ int clusterLoadConfig(char *filename) {
              * This happens if we are loading a nodes.conf generated by
              * an older version of the server. We should manually update the
              * shard membership in this case */
-            clusterNodeAddMaster(n);
+            clusterNodeAddToMasters(n);
         }
 
         /* Set ping sent / pong received timestamps */
@@ -1027,7 +1027,7 @@ void clusterInit(void) {
         serverLog(LL_NOTICE,"No cluster configuration found, I'm %.40s",
             myself->name);
         clusterAddNode(myself);
-        clusterNodeAddMaster(myself);
+        clusterNodeAddToMasters(myself);
         saveconf = 1;
     }
     if (saveconf) clusterSaveConfigOrDie(1);
@@ -1147,7 +1147,7 @@ void clusterReset(int hard) {
     }
 
     /* Re-populate masters */
-    clusterNodeAddMaster(myself);
+    clusterNodeAddToMasters(myself);
 
     /* Make sure to persist the new config and update the state. */
     clusterDoBeforeSleep(CLUSTER_TODO_SAVE_CONFIG|
@@ -1496,7 +1496,7 @@ int clusterNodeRemoveSlave(clusterNode *master, clusterNode *slave) {
     return C_ERR;
 }
 
-int clusterNodeRemoveMaster(clusterNode *master) {
+int clusterNodeRemoveFromMasters(clusterNode *master) {
     for (int j = 0; j < server.cluster->nummasters; j++) {
         if (server.cluster->masters[j] == master) {
             if ((j+1) < server.cluster->nummasters) {
@@ -1527,7 +1527,7 @@ int clusterNodeAddSlave(clusterNode *master, clusterNode *slave) {
     return C_OK;
 }
 
-int clusterNodeAddMaster(clusterNode *master) {
+int clusterNodeAddToMasters(clusterNode *master) {
     /* If it's already a master, don't add it again. */
     for (int j = 0; j < server.cluster->nummasters; j++)
         if (server.cluster->masters[j] == master) return C_ERR;
@@ -1560,7 +1560,7 @@ void freeClusterNode(clusterNode *n) {
 
     /* Remove this node from the list of slaves or masters. */
     if (nodeIsSlave(n) && n->slaveof) clusterNodeRemoveSlave(n->slaveof,n);
-    else clusterNodeRemoveMaster(n);
+    else clusterNodeRemoveFromMasters(n);
 
     /* Unlink from the set of nodes. */
     nodename = sdsnewlen(n->name, CLUSTER_NAMELEN);
@@ -1593,8 +1593,7 @@ void clusterAddNode(clusterNode *node) {
  * 1) Mark all the slots handled by it as unassigned.
  * 2) Remove all the failure reports sent by this node and referenced by
  *    other nodes.
- * 3) Remove the node from the owning shard
- * 4) Free the node with freeClusterNode() that will in turn remove it
+ * 3) Free the node with freeClusterNode() that will in turn remove it
  *    from the hash table and from the list of slaves of its master, if
  *    it is a slave node.
  */
@@ -1623,7 +1622,7 @@ void clusterDelNode(clusterNode *delnode) {
     }
     dictReleaseIterator(di);
 
-    /* 4) Free the node, unlinking it from the cluster. */
+    /* 3) Free the node, unlinking it from the cluster. */
     freeClusterNode(delnode);
 }
 
@@ -1669,7 +1668,7 @@ void clusterRenameNode(clusterNode *node, char *newname) {
     serverAssert(retval == DICT_OK);
     memcpy(node->name, newname, CLUSTER_NAMELEN);
     clusterAddNode(node);
-    clusterNodeAddMaster(node);
+    clusterNodeAddToMasters(node);
 }
 
 /* -----------------------------------------------------------------------------
@@ -2233,7 +2232,7 @@ void clusterProcessGossipSection(clusterMsg *hdr, clusterLink *link) {
                 node->tls_port = msg_tls_port;
                 node->cport = ntohs(g->cport);
                 clusterAddNode(node);
-                clusterNodeAddMaster(node);
+                clusterNodeAddToMasters(node);
             }
         }
 
@@ -2326,7 +2325,7 @@ void clusterSetNodeAsMaster(clusterNode *n) {
     n->flags &= ~CLUSTER_NODE_SLAVE;
     n->slaveof = NULL;
 
-    clusterNodeAddMaster(n);
+    clusterNodeAddToMasters(n);
 
     /* Update config and state. */
     clusterDoBeforeSleep(CLUSTER_TODO_SAVE_CONFIG|
@@ -3262,7 +3261,7 @@ int clusterProcessPacket(clusterLink *link) {
                     if (sender->slaveof)
                         clusterNodeRemoveSlave(sender->slaveof,sender);
                     else
-                        clusterNodeRemoveMaster(sender);
+                        clusterNodeRemoveFromMasters(sender);
                     serverLog(LL_NOTICE,
                               "Node %.40s (%s) is now a replica of node %.40s (%s) in shard %.40s",
                               sender->name,
@@ -5493,7 +5492,7 @@ void clusterSetMaster(clusterNode *n, int closeSlots) {
     serverAssert(myself->numslots == 0);
 
     if (clusterNodeIsMaster(myself)) {
-        clusterNodeRemoveMaster(myself);
+        clusterNodeRemoveFromMasters(myself);
         myself->flags |= CLUSTER_NODE_SLAVE;
     } else {
         if (myself->slaveof)
@@ -5870,7 +5869,7 @@ void clusterUpdateSlots(client *c, unsigned char *slots, int del) {
 }
 
 /* Add detailed information of a node to the output buffer of the given client. */
-void addNodeDetailsToShardReply(client *c, clusterNode *node, int topology) {
+void addNodeDetailsToShardReply(client *c, clusterNode *node) {
     int reply_count = 0;
     void *node_replylen = addReplyDeferredLen(c);
     addReplyBulkCString(c, "id");
@@ -5914,51 +5913,49 @@ void addNodeDetailsToShardReply(client *c, clusterNode *node, int topology) {
     addReplyBulkCString(c, nodeIsSlave(node) ? "replica" : "master");
     reply_count++;
 
-    if (!topology) {
-        addReplyBulkCString(c, "replication-offset");
-        addReplyLongLong(c, node_offset);
-        reply_count++;
+    addReplyBulkCString(c, "replication-offset");
+    addReplyLongLong(c, node_offset);
+    reply_count++;
 
-        addReplyBulkCString(c, "health");
-        const char *health_msg = NULL;
-        if (nodeFailed(node)) {
-            health_msg = "fail";
-        } else if (nodeIsSlave(node) && node_offset == 0) {
-            health_msg = "loading";
-        } else {
-            health_msg = "online";
-        }
-        addReplyBulkCString(c, health_msg);
-        reply_count++;
+    addReplyBulkCString(c, "health");
+    const char *health_msg = NULL;
+    if (nodeFailed(node)) {
+        health_msg = "fail";
+    } else if (nodeIsSlave(node) && node_offset == 0) {
+        health_msg = "loading";
+    } else {
+        health_msg = "online";
     }
+    addReplyBulkCString(c, health_msg);
+    reply_count++;
 
     setDeferredMapLen(c, node_replylen, reply_count);
 }
 
 /* Add the shard reply of a single shard based off the given primary node. */
-void addShardReplyForClusterShards(client *c, clusterNode* n, int topology) {
+void addShardReplyForClusterShards(client *c, clusterNode* primary) {
     addReplyMapLen(c, 2);
     addReplyBulkCString(c, "slots");
 
-    if (n->slot_info_pairs != NULL) {
-        serverAssert((n->slot_info_pairs_count % 2) == 0);
-        addReplyArrayLen(c, n->slot_info_pairs_count);
-        for (int i = 0; i < n->slot_info_pairs_count; i++)
-            addReplyLongLong(c, (unsigned long)n->slot_info_pairs[i]);
+    if (primary->slot_info_pairs != NULL) {
+        serverAssert((primary->slot_info_pairs_count % 2) == 0);
+        addReplyArrayLen(c, primary->slot_info_pairs_count);
+        for (int i = 0; i < primary->slot_info_pairs_count; i++)
+            addReplyLongLong(c, (unsigned long)primary->slot_info_pairs[i]);
     } else {
         /* If no slot info pair is provided, the node owns no slots */
         addReplyArrayLen(c, 0);
     }
 
-    list *nodes = clusterGetNodesInMyShard(n);
+    list *nodes = clusterGetNodesInMyShard(primary);
 
     addReplyBulkCString(c, "nodes");
     addReplyArrayLen(c, listLength(nodes));
     listIter li;
     listRewind(nodes, &li);
     for (listNode *ln = listNext(&li); ln != NULL; ln = listNext(&li)) {
-        n = listNodeValue(ln);
-        addNodeDetailsToShardReply(c, n, topology);
+        clusterNode *n = listNodeValue(ln);
+        addNodeDetailsToShardReply(c, n);
         clusterFreeNodesSlotsInfo(n);
     }
     listRelease(nodes);
@@ -5967,13 +5964,12 @@ void addShardReplyForClusterShards(client *c, clusterNode* n, int topology) {
 /* Add to the output buffer of the given client, an array of slot (start, end)
  * pair owned by the shard, also the primary and set of replica(s) along with
  * information about each node. */
-void clusterCommandShards(client *c, int topology) {
-    serverAssert(server.cluster->nummasters > 0);
+void clusterCommandShards(client *c) {
     addReplyArrayLen(c, server.cluster->nummasters);
     /* This call will add slot_info_pairs to all nodes */
     clusterGenNodesSlotsInfo(0);
     for (int i = 0; i < server.cluster->nummasters; i++) {
-        addShardReplyForClusterShards(c, server.cluster->masters[i], topology);
+        addShardReplyForClusterShards(c, server.cluster->masters[i]);
     }
 }
 

valkey-io/valkey

src/cluster_legacy.c
@@ -64,8 +64,8 @@ void clusterUpdateState(void);
 int clusterNodeCoversSlot(clusterNode *n, int slot);
 list *clusterGetNodesInMyShard(clusterNode *node);
 int clusterNodeAddReplica(clusterNode *primary, clusterNode *replica);
-int clusterNodeAddMaster(clusterNode *master);
-int clusterNodeRemoveMaster(clusterNode *master);
+int clusterNodeAddToMasters(clusterNode *master);
+int clusterNodeRemoveFromMasters(clusterNode *master);
 int clusterAddSlot(clusterNode *n, int slot);
 int clusterDelSlot(int slot);
 int clusterDelNodeSlots(clusterNode *node);
@@ -239,7 +239,7 @@ int auxShardIdSetter(clusterNode *n, void *value, int length) {
     }
     /* Initially, during the load, add every node as master until the respective
      * role is assigned with the persisted shard ID. */
-    clusterNodeAddMaster(n);
+    clusterNodeAddToMasters(n);
     return C_OK;
 }
 
@@ -599,7 +599,7 @@ int clusterLoadConfig(char *filename) {
             /* Since the role of node is decided as replica, remove it from
              * master list which was added initially during the load and continue
              * maintain the persisted master list */
-            clusterNodeRemoveMaster(n);
+            clusterNodeRemoveFromMasters(n);
             n->replicaof = primary;
             clusterNodeAddReplica(primary, n);
         } else if (auxFieldHandlers[af_shard_id].isPresent(n) == 0) {
@@ -607,7 +607,7 @@ int clusterLoadConfig(char *filename) {
              * This happens if we are loading a nodes.conf generated by
              * an older version of the server. We should manually update the
              * shard membership in this case */
-            clusterNodeAddMaster(n);
+            clusterNodeAddToMasters(n);
         }
 
         /* Set ping sent / pong received timestamps */
@@ -1016,7 +1016,7 @@ void clusterInit(void) {
         myself = server.cluster->myself = createClusterNode(NULL, CLUSTER_NODE_MYSELF | CLUSTER_NODE_PRIMARY);
         serverLog(LL_NOTICE, "No cluster configuration found, I'm %.40s", myself->name);
         clusterAddNode(myself);
-        clusterNodeAddMaster(myself);
+        clusterNodeAddToMasters(myself);
         saveconf = 1;
     }
     if (saveconf) clusterSaveConfigOrDie(1);
@@ -1148,7 +1148,7 @@ void clusterReset(int hard) {
     }
 
     /* Re-populate masters */
-    clusterNodeAddMaster(myself);
+    clusterNodeAddToMasters(myself);
 
     /* Make sure to persist the new config and update the state. */
     clusterDoBeforeSleep(CLUSTER_TODO_SAVE_CONFIG | CLUSTER_TODO_UPDATE_STATE | CLUSTER_TODO_FSYNC_CONFIG);
@@ -1488,7 +1488,7 @@ int clusterNodeRemoveReplica(clusterNode *primary, clusterNode *replica) {
     return C_ERR;
 }
 
-int clusterNodeRemoveMaster(clusterNode *master) {
+int clusterNodeRemoveFromMasters(clusterNode *master) {
     for (int j = 0; j < server.cluster->nummasters; j++) {
         if (server.cluster->masters[j] == master) {
             if ((j+1) < server.cluster->nummasters) {
@@ -1518,7 +1518,7 @@ int clusterNodeAddReplica(clusterNode *primary, clusterNode *replica) {
     return C_OK;
 }
 
-int clusterNodeAddMaster(clusterNode *master) {
+int clusterNodeAddToMasters(clusterNode *master) {
     /* If it's already a master, don't add it again. */
     for (int j = 0; j < server.cluster->nummasters; j++)
         if (server.cluster->masters[j] == master) return C_ERR;
@@ -1550,7 +1550,7 @@ void freeClusterNode(clusterNode *n) {
 
     /* Remove this node from the list of replicas of its primary. */
     if (nodeIsReplica(n) && n->replicaof) clusterNodeRemoveReplica(n->replicaof, n);
-    else clusterNodeRemoveMaster(n);
+    else clusterNodeRemoveFromMasters(n);
 
     /* Unlink from the set of nodes. */
     nodename = sdsnewlen(n->name, CLUSTER_NAMELEN);
@@ -1582,8 +1582,7 @@ void clusterAddNode(clusterNode *node) {
  * 1) Mark all the slots handled by it as unassigned.
  * 2) Remove all the failure reports sent by this node and referenced by
  *    other nodes.
- * 3) Remove the node from the owning shard
- * 4) Free the node with freeClusterNode() that will in turn remove it
+ * 3) Free the node with freeClusterNode() that will in turn remove it
  *    from the hash table and from the list of replicas of its primary, if
  *    it is a replica node.
  */
@@ -1609,7 +1608,7 @@ void clusterDelNode(clusterNode *delnode) {
     }
     dictReleaseIterator(di);
 
-    /* 4) Free the node, unlinking it from the cluster. */
+    /* 3) Free the node, unlinking it from the cluster. */
     freeClusterNode(delnode);
 }
 
@@ -1652,7 +1651,7 @@ void clusterRenameNode(clusterNode *node, char *newname) {
     serverAssert(retval == DICT_OK);
     memcpy(node->name, newname, CLUSTER_NAMELEN);
     clusterAddNode(node);
-    clusterNodeAddMaster(node);
+    clusterNodeAddToMasters(node);
 }
 
 /* -----------------------------------------------------------------------------
@@ -2178,7 +2177,7 @@ void clusterProcessGossipSection(clusterMsg *hdr, clusterLink *link) {
                 node->tls_port = msg_tls_port;
                 node->cport = ntohs(g->cport);
                 clusterAddNode(node);
-                clusterNodeAddMaster(node);
+                clusterNodeAddToMasters(node);
             }
         }
 
@@ -2269,7 +2268,7 @@ void clusterSetNodeAsPrimary(clusterNode *n) {
     n->flags &= ~CLUSTER_NODE_REPLICA;
     n->replicaof = NULL;
 
-    clusterNodeAddMaster(n);
+    clusterNodeAddToMasters(n);
 
     /* Update config and state. */
     clusterDoBeforeSleep(CLUSTER_TODO_SAVE_CONFIG | CLUSTER_TODO_UPDATE_STATE);
@@ -3118,8 +3117,10 @@ int clusterProcessPacket(clusterLink *link) {
                     if (sender->replicaof)
                         clusterNodeRemoveReplica(sender->replicaof, sender);
                     else
-                        clusterNodeRemoveMaster(sender);
-
+                        clusterNodeRemoveFromMasters(sender);
+                    serverLog(LL_NOTICE, "Node %.40s (%s) is now a replica of node %.40s (%s) in shard %.40s",
+                              sender->name, sender->human_nodename, primary->name, primary->human_nodename,
+                              sender->shard_id);
                     clusterNodeAddReplica(primary, sender);
                     sender->replicaof = primary;
 
@@ -5193,7 +5194,7 @@ void clusterSetPrimary(clusterNode *n, int closeSlots) {
     serverAssert(myself->numslots == 0);
 
     if (clusterNodeIsPrimary(myself)) {
-        clusterNodeRemoveMaster(myself);
+        clusterNodeRemoveFromMasters(myself);
         myself->flags |= CLUSTER_NODE_REPLICA;
         clusterCloseAllSlots();
     } else {
@@ -5560,7 +5561,7 @@ long long getNodeReplicationOffset(clusterNode *node) {
 }
 
 /* Add detailed information of a node to the output buffer of the given client. */
-void addNodeDetailsToShardReply(client *c, clusterNode *node, int topology) {
+void addNodeDetailsToShardReply(client *c, clusterNode *node) {
     int reply_count = 0;
     void *node_replylen = addReplyDeferredLen(c);
     addReplyBulkCString(c, "id");
@@ -5599,50 +5600,48 @@ void addNodeDetailsToShardReply(client *c, clusterNode *node, int topology) {
     addReplyBulkCString(c, nodeIsReplica(node) ? "replica" : "master");
     reply_count++;
 
-    if (!topology) {
-        addReplyBulkCString(c, "replication-offset");
-        addReplyLongLong(c, node_offset);
-        reply_count++;
+    addReplyBulkCString(c, "replication-offset");
+    addReplyLongLong(c, node_offset);
+    reply_count++;
 
-        addReplyBulkCString(c, "health");
-        const char *health_msg = NULL;
-        if (nodeFailed(node)) {
-            health_msg = "fail";
-        } else if (nodeIsReplica(node) && node_offset == 0) {
-            health_msg = "loading";
-        } else {
-            health_msg = "online";
-        }
-        addReplyBulkCString(c, health_msg);
-        reply_count++;
+    addReplyBulkCString(c, "health");
+    const char *health_msg = NULL;
+    if (nodeFailed(node)) {
+        health_msg = "fail";
+    } else if (nodeIsReplica(node) && node_offset == 0) {
+        health_msg = "loading";
+    } else {
+        health_msg = "online";
     }
+    addReplyBulkCString(c, health_msg);
+    reply_count++;
 
     setDeferredMapLen(c, node_replylen, reply_count);
 }
 
 /* Add the shard reply of a single shard based off the given primary node. */
-void addShardReplyForClusterShards(client *c, clusterNode* n, int topology) {
+void addShardReplyForClusterShards(client *c, clusterNode* primary) {
     addReplyMapLen(c, 2);
     addReplyBulkCString(c, "slots");
 
-    if (n->slot_info_pairs != NULL) {
-        serverAssert((n->slot_info_pairs_count % 2) == 0);
-        addReplyArrayLen(c, n->slot_info_pairs_count);
-        for (int i = 0; i < n->slot_info_pairs_count; i++) addReplyLongLong(c, (unsigned long)n->slot_info_pairs[i]);
+    if (primary->slot_info_pairs != NULL) {
+        serverAssert((primary->slot_info_pairs_count % 2) == 0);
+        addReplyArrayLen(c, primary->slot_info_pairs_count);
+        for (int i = 0; i < primary->slot_info_pairs_count; i++) addReplyLongLong(c, (unsigned long)primary->slot_info_pairs[i]);
     } else {
         /* If no slot info pair is provided, the node owns no slots */
         addReplyArrayLen(c, 0);
     }
 
-    list *nodes = clusterGetNodesInMyShard(n);
+    list *nodes = clusterGetNodesInMyShard(primary);
 
     addReplyBulkCString(c, "nodes");
     addReplyArrayLen(c, listLength(nodes));
     listIter li;
     listRewind(nodes, &li);
     for (listNode *ln = listNext(&li); ln != NULL; ln = listNext(&li)) {
-        n = listNodeValue(ln);
-        addNodeDetailsToShardReply(c, n, topology);
+        clusterNode *n = listNodeValue(ln);
+        addNodeDetailsToShardReply(c, n);
         clusterFreeNodesSlotsInfo(n);
     }
     listRelease(nodes);
@@ -5651,13 +5650,12 @@ void addShardReplyForClusterShards(client *c, clusterNode* n, int topology) {
 /* Add to the output buffer of the given client, an array of slot (start, end)
  * pair owned by the shard, also the primary and set of replica(s) along with
  * information about each node. */
-void clusterCommandShards(client *c, int topology) {
-    serverAssert(server.cluster->nummasters > 0);
+void clusterCommandShards(client *c) {
     addReplyArrayLen(c, server.cluster->nummasters);
     /* This call will add slot_info_pairs to all nodes */
     clusterGenNodesSlotsInfo(0);
     for (int i = 0; i < server.cluster->nummasters; i++) {
-        addShardReplyForClusterShards(c, server.cluster->masters[i], topology);
+        addShardReplyForClusterShards(c, server.cluster->masters[i]);
     }
 }
 
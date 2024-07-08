05e946fc92dc5d034e5982bcb88f752d214e0f49
KarthikSubbarao <karthikrs2021@gmail.com>
2024-07-08 16:52:08 +0000

    clang-format-check suggestions
    Signed-off-by: KarthikSubbarao <karthikrs2021@gmail.com>

--- src/server.h
+++ src/server.h
@@ -2595,15 +2595,18 @@ void serverSetCpuAffinity(const char *cpulist);
 void dictVanillaFree(dict *d, void *val);
 
 /* ERROR STATS constants */
-#define ERROR_STATS_LUA_LIMIT 128 /* After the errors RAX reaches this limit, instead of tracking 
-                                    custom LUA errors, we track the error under the symbol below. */
+#define ERROR_STATS_LUA_LIMIT                                                                                          \
+    128 /* After the errors RAX reaches this limit, instead of tracking                                                \
+          custom LUA errors, we track the error under the symbol below. */
 #define LUA_ERRORSTATS_OVERFLOW_ERR "LUA_ERRORSTATS_OVERFLOW"
 
 /* afterErrorReply flags */
-#define ERR_REPLY_FLAG_NO_STATS_UPDATE (1ULL<<0) /* Indicating that we should not update
-                                                    error stats after sending error reply */
-#define ERR_REPLY_FLAG_LUA (1ULL<<1) /* Indicating that the error message is from LUA replying
-                                        to a client */
+#define ERR_REPLY_FLAG_NO_STATS_UPDATE                                                                                 \
+    (1ULL << 0) /* Indicating that we should not update                                                                \
+                   error stats after sending error reply */
+#define ERR_REPLY_FLAG_LUA                                                                                             \
+    (1ULL << 1) /* Indicating that the error message is from LUA replying                                              \
+                   to a client */
 
 /* networking.c -- Networking and Client related operations */
 client *createClient(connection *conn);

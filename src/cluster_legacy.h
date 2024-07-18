c6f9bb5103d4cb99860e18af3554822249d125cd
Roshan Khatri <rvkhatri@amazon.com>
2024-07-18 03:07:05 +0000

    spell check
    Signed-off-by: Roshan Khatri <rvkhatri@amazon.com>

--- src/cluster_legacy.h
+++ src/cluster_legacy.h
@@ -96,7 +96,7 @@ typedef struct clusterNodeFailReport {
 #define CLUSTERMSG_TYPE_PUBLISHSHARD_LIGHT 12   /* Pub/Sub Publish shard propagation using light header*/
 #define CLUSTERMSG_TYPE_COUNT 13                /* Total number of message types. */
 
-/* Macros for condition which are repeadedly used. */
+/* Macros for condition which are repeatedly used. */
 #define IS_PUBSUB_LIGHT_MESSAGE (type == CLUSTERMSG_TYPE_PUBLISH_LIGHT || type == CLUSTERMSG_TYPE_PUBLISHSHARD_LIGHT)
 
 /* Initially we don't know our "name", but we'll find it once we connect

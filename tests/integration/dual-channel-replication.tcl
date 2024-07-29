f9a6539475ce859a86327e7844f9b25c2c5d984c
naglera <58042354+naglera@users.noreply.github.com>
2024-07-29 11:55:36 +0300

    Update tests/integration/dual-channel-replication.tcl
    Co-authored-by: ranshid <88133677+ranshid@users.noreply.github.com>
    Signed-off-by: naglera <58042354+naglera@users.noreply.github.com>

--- tests/integration/dual-channel-replication.tcl
+++ tests/integration/dual-channel-replication.tcl
@@ -26,8 +26,7 @@ proc get_client_id_by_last_cmd {r cmd} {
 proc wait_and_resume_process idx {
     set pid [srv $idx pid]
     wait_for_condition 50 1000 {
-        [exec ps -o state= -p $pid] eq "T" ||
-        [exec ps -o state= -p $pid] eq "Z"
+        [exec ps -o state= -p $pid] eq "T"
     } else {
         fail "Process $pid didn't stop"
     }

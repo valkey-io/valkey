2e5e11d98ed7e087dfd9c177e1e2ca1a750fb4f3
Wen Hui <wen.hui.ware@gmail.com>
2024-08-03 11:30:55 -0400

    Update redis.conf to valkey.conf in log message (#855)
    Update redis.conf to valkey.conf
    
    Signed-off-by: hwware <wen.hui.ware@gmail.com>

--- src/replication.c
+++ src/replication.c
@@ -4474,7 +4474,7 @@ void replicationCron(void) {
     if (server.primary_host && server.repl_state == REPL_STATE_TRANSFER &&
         (time(NULL) - server.repl_transfer_lastio) > server.repl_timeout) {
         serverLog(LL_WARNING, "Timeout receiving bulk data from PRIMARY... If the problem persists try to set the "
-                              "'repl-timeout' parameter in redis.conf to a larger value.");
+                              "'repl-timeout' parameter in valkey.conf to a larger value.");
         cancelReplicationHandshake(1);
     }
 

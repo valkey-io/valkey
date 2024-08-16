a2f4aedd23cc2f5cbe57d56ee24a60cee9feb9b8
Wen Hui <wen.hui.ware@gmail.com>
2024-08-16 09:46:36 -0400

    Add 4 commands for sentinel and update most test cases and json files (#789)
    Add 4 new commands for Sentinel (reference
    https://github.com/valkey-io/valkey/issues/36)
    
    Sentinel GET-PRIMARY-ADDR-BY-NAME
    Sentinel PRIMARY
    Sentinel PRIMARIES
    Sentinel IS-PRIMARY-DOWN-BY-ADDR
    
    and deprecate 4 old commands:
    
    Sentinel GET-MASTER-ADDR-BY-NAME
    Sentinel MASTER
    Sentinel MASTERS
    Sentinel IS-MASTER-DOWN-BY-ADDR
    
    and all sentinel tests pass here
    https://github.com/hwware/valkey/actions/runs/9962102363/job/27525124583
    
    Note: 
    
    1. runtest-sentinel pass all test cases
    2. I finished a sentinel rolling upgrade test: 1 primary 2 replicas 3
    sentinel
       there are 4 steps in this test scenario: 
    step 1: all 3 sentinel nodes run old sentinel, shutdown primary, and
    then new primary can be voted successfully.
    step 2: replace sentinel 1 with new sentinel bin file, and then shutdown
    primary, and then another new primary can be voted successfully
    step 3: replace sentinel 2 with new sentinel bin file, and then shutdown
    primary, and then another new primary can be voted successfully
    step 4: replace sentinel 3 with new sentinel bin file, and then shutdown
    primary, and then another new primary can be voted successfully
       
    We can see, even mixed version sentinel running, whole system still
    works.
    
    ---------
    
    Signed-off-by: hwware <wen.hui.ware@gmail.com>

--- tests/sentinel/tests/02-replicas-reconf.tcl
+++ tests/sentinel/tests/02-replicas-reconf.tcl
@@ -29,18 +29,18 @@ proc 02_crash_and_failover {} {
     uplevel 1 {
         test "Crash the primary and force a failover" {
             set old_port [RPort $master_id]
-            set addr [S 0 SENTINEL GET-MASTER-ADDR-BY-NAME mymaster]
+            set addr [S 0 SENTINEL GET-PRIMARY-ADDR-BY-NAME mymaster]
             assert {[lindex $addr 1] == $old_port}
             kill_instance valkey $master_id
             foreach_sentinel_id id {
                 wait_for_condition 1000 50 {
-                    [lindex [S $id SENTINEL GET-MASTER-ADDR-BY-NAME mymaster] 1] != $old_port
+                    [lindex [S $id SENTINEL GET-PRIMARY-ADDR-BY-NAME mymaster] 1] != $old_port
                 } else {
                     fail "At least one Sentinel did not receive failover info"
                 }
             }
             restart_instance valkey $master_id
-            set addr [S 0 SENTINEL GET-MASTER-ADDR-BY-NAME mymaster]
+            set addr [S 0 SENTINEL GET-PRIMARY-ADDR-BY-NAME mymaster]
             set master_id [get_instance_id_by_port valkey [lindex $addr 1]]
         }
     }
@@ -74,7 +74,7 @@ test "Wait for failover to end" {
     while {$inprogress} {
         set inprogress 0
         foreach_sentinel_id id {
-            if {[dict exists [S $id SENTINEL MASTER mymaster] failover-state]} {
+            if {[dict exists [S $id SENTINEL PRIMARY mymaster] failover-state]} {
                 incr inprogress
             }
         }

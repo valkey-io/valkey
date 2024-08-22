3c8b81b2b923bbf49bfad63564f9a1affc6e82fb
Ran Shidlansik <ranshid@amazon.com>
2024-08-22 10:30:31 +0300

    format fix
    Signed-off-by: Ran Shidlansik <ranshid@amazon.com>

--- src/server.h
+++ src/server.h
@@ -2174,7 +2174,7 @@ struct valkeyServer {
     int cluster_slot_stats_enabled;                        /* Cluster slot usage statistics tracking enabled. */
     /* Debug config that goes along with cluster_drop_packet_filter. When set, the link is closed on packet drop. */
     uint32_t debug_cluster_close_link_on_packet_drop : 1;
-    sds cached_cluster_slot_info[CACHE_CONN_TYPE_MAX]; /* Index in array is a bitwise or of CACHE_CONN_TYPE_* */
+    sds cached_cluster_slot_info[CACHE_CONN_TYPE_MAX];    /* Index in array is a bitwise or of CACHE_CONN_TYPE_* */
     sds cached_cluster_my_slot_info[CACHE_CONN_TYPE_MAX]; /* Index in array of owned slots which is a bitwise or of
                                                              CACHE_CONN_TYPE_* */
     /* Scripting */

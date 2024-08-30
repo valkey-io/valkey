d4cf5eb7fce19e7ca68e84880c5636517f4d2536
Viktor Söderqvist <viktor.soderqvist@est.tech>
2024-08-30 15:19:04 +0200

    Delete unused parts of zipmap
    Deletes zipmapSet, zipmapGet, etc. Only keep iterator and validate integrity.
    
    Adjust unit tests to not use zipmapSet, etc.
    
    Signed-off-by: Viktor Söderqvist <viktor.soderqvist@est.tech>

--- src/unit/test_zipmap.c
+++ src/unit/test_zipmap.c
@@ -1,129 +1,93 @@
 #include "../zipmap.c"
 #include "test_help.h"
 
-static void zipmapRepr(unsigned char *p) {
-    unsigned int l;
-
-    p++;
-    while (1) {
-        if (p[0] == ZIPMAP_END) {
-            break;
-        } else {
-            unsigned char e;
-
-            l = zipmapDecodeLength(p);
-            p += zipmapEncodeLength(NULL, l);
-            if (l != 0 && fwrite(p, l, 1, stdout) == 0) perror("fwrite");
-            p += l;
-
-            l = zipmapDecodeLength(p);
-            p += zipmapEncodeLength(NULL, l);
-            e = *p++;
-            if (l != 0 && fwrite(p, l, 1, stdout) == 0) perror("fwrite");
-            p += l + e;
-            if (e) {
-                while (e--);
-            }
-        }
-    }
-}
-
-int test_zipmapLookUpLargeKey(int argc, char *argv[], int flags) {
-    unsigned char *zm;
-    UNUSED(argc);
-    UNUSED(argv);
-    UNUSED(flags);
-    zm = zipmapNew();
-
-    zm = zipmapSet(zm, (unsigned char *)"name", 4, (unsigned char *)"foo", 3, NULL);
-    zm = zipmapSet(zm, (unsigned char *)"surname", 7, (unsigned char *)"foo", 3, NULL);
-    zm = zipmapSet(zm, (unsigned char *)"age", 3, (unsigned char *)"foo", 3, NULL);
-    zipmapRepr(zm);
-
-    zm = zipmapSet(zm, (unsigned char *)"hello", 5, (unsigned char *)"world!", 6, NULL);
-    zm = zipmapSet(zm, (unsigned char *)"foo", 3, (unsigned char *)"bar", 3, NULL);
-    zm = zipmapSet(zm, (unsigned char *)"foo", 3, (unsigned char *)"!", 1, NULL);
-    zipmapRepr(zm);
-    zm = zipmapSet(zm, (unsigned char *)"foo", 3, (unsigned char *)"12345", 5, NULL);
-    zipmapRepr(zm);
-    zm = zipmapSet(zm, (unsigned char *)"new", 3, (unsigned char *)"xx", 2, NULL);
-    zm = zipmapSet(zm, (unsigned char *)"noval", 5, (unsigned char *)"", 0, NULL);
-    zipmapRepr(zm);
-    zm = zipmapDel(zm, (unsigned char *)"new", 3, NULL);
-    zipmapRepr(zm);
-
-    unsigned char buf[512];
-    unsigned char *value;
-    unsigned int vlen, i;
-    for (i = 0; i < 512; i++) buf[i] = 'a';
-    zm = zipmapSet(zm, buf, 512, (unsigned char *)"long", 4, NULL);
-    if (zipmapGet(zm, buf, 512, &value, &vlen)) {
-        TEST_ASSERT(4 == vlen);
-        TEST_ASSERT(strncmp("long", (const char *)value, vlen) == 0);
-    }
-    zfree(zm);
+int test_zipmapIterateWithLargeKey(int argc, char *argv[], int flags) {
     return 0;
-}
-
-int test_zipmapPerformDirectLookup(int argc, char *argv[], int flags) {
-    unsigned char *zm;
     UNUSED(argc);
     UNUSED(argv);
     UNUSED(flags);
-    zm = zipmapNew();
 
-    zm = zipmapSet(zm, (unsigned char *)"name", 4, (unsigned char *)"foo", 3, NULL);
-    zm = zipmapSet(zm, (unsigned char *)"surname", 7, (unsigned char *)"foo", 3, NULL);
-    zm = zipmapSet(zm, (unsigned char *)"age", 3, (unsigned char *)"foo", 3, NULL);
-    zipmapRepr(zm);
+    char zm[] = "\x04"
+                "\x04"
+                "name"
+                "\x03\x00"
+                "foo"
+                "\x07"
+                "surname"
+                "\x03\x00"
+                "foo"
+                "noval"
+                "\x00\x00"
+                "\xfe\x00\x02\x00\x00"
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+                "\x04\x00"
+                "long"
+                "\xff";
+    TEST_ASSERT(zipmapValidateIntegrity((unsigned char *)zm, sizeof zm - 1, 1));
 
-    zm = zipmapSet(zm, (unsigned char *)"hello", 5, (unsigned char *)"world!", 6, NULL);
-    zm = zipmapSet(zm, (unsigned char *)"foo", 3, (unsigned char *)"bar", 3, NULL);
-    zm = zipmapSet(zm, (unsigned char *)"foo", 3, (unsigned char *)"!", 1, NULL);
-    zipmapRepr(zm);
-    zm = zipmapSet(zm, (unsigned char *)"foo", 3, (unsigned char *)"12345", 5, NULL);
-    zipmapRepr(zm);
-    zm = zipmapSet(zm, (unsigned char *)"new", 3, (unsigned char *)"xx", 2, NULL);
-    zm = zipmapSet(zm, (unsigned char *)"noval", 5, (unsigned char *)"", 0, NULL);
-    zipmapRepr(zm);
-    zm = zipmapDel(zm, (unsigned char *)"new", 3, NULL);
-    zipmapRepr(zm);
-    unsigned char *value;
-    unsigned int vlen;
+    unsigned char *p = zipmapRewind((unsigned char *)zm);
+    unsigned char *key, *value;
+    unsigned int klen, vlen;
+    char buf[512];
+    memset(buf, 'a', 512);
+    char *expected_key[] = {"name", "surname", "noval", buf};
+    char *expected_value[] = {"foo", "foo", NULL, "long"};
+    unsigned int expected_klen[] = {4, 7, 5, 512};
+    unsigned int expected_vlen[] = {3, 3, 0, 4};
+    int iter = 0;
 
-    if (zipmapGet(zm, (unsigned char *)"foo", 3, &value, &vlen)) {
-        TEST_ASSERT(5 == vlen);
-        TEST_ASSERT(!strncmp("12345", (const char *)value, vlen));
+    while ((p = zipmapNext(p, &key, &klen, &value, &vlen)) != NULL) {
+        char *tmp = expected_key[iter];
+        TEST_ASSERT(klen == expected_klen[iter]);
+        TEST_ASSERT(strncmp((const char *)tmp, (const char *)key, klen) == 0);
+        tmp = expected_value[iter];
+        TEST_ASSERT(vlen == expected_vlen[iter]);
+        TEST_ASSERT(strncmp((const char *)tmp, (const char *)value, vlen) == 0);
+        iter++;
     }
-    zfree(zm);
     return 0;
 }
 
 int test_zipmapIterateThroughElements(int argc, char *argv[], int flags) {
-    unsigned char *zm;
     UNUSED(argc);
     UNUSED(argv);
     UNUSED(flags);
-    zm = zipmapNew();
 
-    zm = zipmapSet(zm, (unsigned char *)"name", 4, (unsigned char *)"foo", 3, NULL);
-    zm = zipmapSet(zm, (unsigned char *)"surname", 7, (unsigned char *)"foo", 3, NULL);
-    zm = zipmapSet(zm, (unsigned char *)"age", 3, (unsigned char *)"foo", 3, NULL);
-    zipmapRepr(zm);
+    char zm[] = "\x06"
+                "\x04"
+                "name"
+                "\x03\x00"
+                "foo"
+                "\x07"
+                "surname"
+                "\x03\x00"
+                "foo"
+                "\x03"
+                "age"
+                "\x03\x00"
+                "foo"
+                "\x05"
+                "hello"
+                "\x06\x00"
+                "world!"
+                "\x03"
+                "foo"
+                "\x05\x00"
+                "12345"
+                "\x05"
+                "noval"
+                "\x00\x00"
+                "\xff";
+    TEST_ASSERT(zipmapValidateIntegrity((unsigned char *)zm, sizeof zm - 1, 1));
 
-    zm = zipmapSet(zm, (unsigned char *)"hello", 5, (unsigned char *)"world!", 6, NULL);
-    zm = zipmapSet(zm, (unsigned char *)"foo", 3, (unsigned char *)"bar", 3, NULL);
-    zm = zipmapSet(zm, (unsigned char *)"foo", 3, (unsigned char *)"!", 1, NULL);
-    zipmapRepr(zm);
-    zm = zipmapSet(zm, (unsigned char *)"foo", 3, (unsigned char *)"12345", 5, NULL);
-    zipmapRepr(zm);
-    zm = zipmapSet(zm, (unsigned char *)"new", 3, (unsigned char *)"xx", 2, NULL);
-    zm = zipmapSet(zm, (unsigned char *)"noval", 5, (unsigned char *)"", 0, NULL);
-    zipmapRepr(zm);
-    zm = zipmapDel(zm, (unsigned char *)"new", 3, NULL);
-    zipmapRepr(zm);
-
-    unsigned char *i = zipmapRewind(zm);
+    unsigned char *i = zipmapRewind((unsigned char *)zm);
     unsigned char *key, *value;
     unsigned int klen, vlen;
     char *expected_key[] = {"name", "surname", "age", "hello", "foo", "noval"};
@@ -141,6 +105,5 @@ int test_zipmapIterateThroughElements(int argc, char *argv[], int flags) {
         TEST_ASSERT(strncmp((const char *)tmp, (const char *)value, vlen) == 0);
         iter++;
     }
-    zfree(zm);
     return 0;
 }

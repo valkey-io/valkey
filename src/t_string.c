47ef2025844b9e066de77c0ba34f03561a86f459
Binbin <binloveplay1314@qq.com>
2024-09-05 10:53:24 +0800

    remove debug code
    Signed-off-by: Binbin <binloveplay1314@qq.com>

--- src/t_string.c
+++ src/t_string.c
@@ -317,8 +317,6 @@ void setCommand(client *c) {
 
     c->argv[2] = tryObjectEncoding(c->argv[2]);
     setGenericCommand(c,flags,c->argv[1],c->argv[2],expire,unit,NULL,NULL);
-
-    serverAssert(1==0);
 }
 
 void setnxCommand(client *c) {

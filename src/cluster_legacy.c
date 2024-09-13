0270e716d7f2975011b34c21d4a0254ec4095f56
Binbin <binloveplay1314@qq.com>
2024-09-13 15:13:22 +0800

    fix format
    Signed-off-by: Binbin <binloveplay1314@qq.com>

--- src/cluster_legacy.c
+++ src/cluster_legacy.c
@@ -3115,14 +3115,16 @@ int clusterProcessPacket(clusterLink *link) {
             clusterDoBeforeSleep(CLUSTER_TODO_SAVE_CONFIG | CLUSTER_TODO_FSYNC_CONFIG);
 
             if (server.cluster->failover_auth_time && sender->configEpoch >= server.cluster->failover_auth_epoch) {
-                /* Another node has claimed an epoch greater than or equal to ours. If we have an ongoing election, 
-                 * reset it because we cannot win with an epoch smaller than or equal to the incoming claim. 
-                 * This allows us to start a new election as soon as possible. */
+                /* Another node has claimed an epoch greater than or equal to ours.
+                 * If we have an ongoing election,  reset it because we cannot win
+                 * with an epoch smaller than or equal to the incoming claim. This
+                 * allows us to start a new election as soon as possible. */
                 server.cluster->failover_auth_time = 0;
                 serverLog(LL_WARNING,
-                          "Failover election in progress for epoch %llu, but received a claim from "
-                          "node %.40s (%s) with an equal or higher epoch %llu. Resetting the election since we cannot win.",
-                          (unsigned long long)server.cluster->failover_auth_epoch, sender->name, sender->human_nodename, (unsigned long long)sender->configEpoch);
+                          "Failover election in progress for epoch %llu, but received a claim from node %.40s (%s) "
+                          "with an equal or higher epoch %llu. Resetting the election since we cannot win.",
+                          (unsigned long long)server.cluster->failover_auth_epoch, sender->name, sender->human_nodename,
+                          (unsigned long long)sender->configEpoch);
                 clusterDoBeforeSleep(CLUSTER_TODO_HANDLE_FAILOVER);
             }
         }

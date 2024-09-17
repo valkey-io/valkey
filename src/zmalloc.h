acd74b72570f9a2e088ab474700b04b2252f18a5
Ricardo Dias <ricardo.dias@percona.com>
2024-09-17 16:29:15 +0100

    Changes `tcmalloc.h` header location (#1033)
    This commit changes the `tcmalloc.h` header location from the
    deprecated location `google/` to `gperftools/`.
    
    *Why we're doing this now?*
    
    The location `google/tcmalloc.h` has been deprecated for more
    than 10 years in favor of `gperftools/tcmalloc.h`, and the
    deprecated location will be removed in the next release of
    gperftools.
    
    Signed-off-by: Ricardo Dias <ricardo.dias@percona.com>

--- src/zmalloc.h
+++ src/zmalloc.h
@@ -39,7 +39,7 @@
 
 #if defined(USE_TCMALLOC)
 #define ZMALLOC_LIB ("tcmalloc-" __xstr(TC_VERSION_MAJOR) "." __xstr(TC_VERSION_MINOR))
-#include <google/tcmalloc.h>
+#include <gperftools/tcmalloc.h>
 #if (TC_VERSION_MAJOR == 1 && TC_VERSION_MINOR >= 6) || (TC_VERSION_MAJOR > 1)
 #define HAVE_MALLOC_SIZE 1
 #define zmalloc_size(p) tc_malloc_size(p)

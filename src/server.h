50c8df646a630e94e8483da287db5e6a62dd2e0d
Masahiro Ide <masahiro.ide@linecorp.com>
2024-09-30 10:05:08 +0900

    Eliminate hashTypeIterator memory allocation by assigning it on stack
    At https://github.com/redis/redis/pull/11245, we provided dict iterator API
    to initialize/release the iterators without memory allocation.
    This change is for hash iterator.
    
    | test        |  unstable(ac569c09f8) rpc | this patch (rpc) | improvements |
    | ----------- | ------------------------- | ---------------- | ------------ |
    | HGETALL h1  | 78192.2                   | 79802.09         |  2.058888227 |
    | HGETALL h10 | 75075.08                  | 74850.3          | -0.29940694  |
    | HGETALL h25 | 68634.18                  | 69541.03         |  1.321280447 |
    | HGETALL h50 | 60639.14                  | 59548.62         | -1.798376428 |
    
    Signed-off-by: Masahiro Ide <masahiro.ide@lycorp.co.jp>

--- src/server.h
+++ src/server.h
@@ -2603,7 +2603,7 @@ typedef struct {
 
     unsigned char *fptr, *vptr;
 
-    dictIterator *di;
+    dictIterator di;
     dictEntry *de;
 } hashTypeIterator;
 
@@ -3370,7 +3370,7 @@ void hashTypeTryConversion(robj *subject, robj **argv, int start, int end);
 int hashTypeExists(robj *o, sds key);
 int hashTypeDelete(robj *o, sds key);
 unsigned long hashTypeLength(const robj *o);
-hashTypeIterator *hashTypeInitIterator(robj *subject);
+void hashTypeInitIterator(robj *subject, hashTypeIterator *hi);
 void hashTypeReleaseIterator(hashTypeIterator *hi);
 int hashTypeNext(hashTypeIterator *hi);
 void hashTypeCurrentFromListpack(hashTypeIterator *hi,

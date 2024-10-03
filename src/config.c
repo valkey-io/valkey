8ffdd55c9b63921943f837e30c7020a3fe6dd471
hwware <wen.hui.ware@gmail.com>
2024-10-03 12:32:53 +0000

    adjust clang-format
    Signed-off-by: hwware <wen.hui.ware@gmail.com>

--- src/config.c
+++ src/config.c
@@ -2526,7 +2526,7 @@ static int updateMaxmemory(const char **err) {
                       "depending on the maxmemory-policy.",
                       server.maxmemory, used);
         }
-	calculateMaxAvailableMemory();
+        calculateMaxAvailableMemory();
         startEvictionTimeProc();
     }
     return 1;

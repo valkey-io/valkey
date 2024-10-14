755b11e04076ea3ee1ffb146379e4fd7a2072a10
Ran Shidlansik <ranshid@amazon.com>
2024-10-14 10:15:29 +0300

    deflake test ync should continue if not all slaves dropped dual-channel-replication
    sometimes when dual-channel is turned off the tested replica might disconnect on COB overrun.
    disable the replica COB limit in order to prevent such cases
    
    Signed-off-by: Ran Shidlansik <ranshid@amazon.com>

--- tests/integration/dual-channel-replication.tcl
+++ tests/integration/dual-channel-replication.tcl
@@ -834,11 +834,12 @@ start_server {tags {"dual-channel-replication external:skip"}} {
     $primary config set dual-channel-replication-enabled yes
     $primary config set loglevel debug
     $primary config set repl-diskless-sync-delay 5
-    
+    $primary config set client-output-buffer-limit "replica 0 0 0"
+
     # Generating RDB will cost 5s(10000 * 0.0005s)
     $primary debug populate 10000 primary 1
     $primary config set rdb-key-save-delay 500
-
+    
     $primary config set dual-channel-replication-enabled $dualchannel
 
     start_server {} {

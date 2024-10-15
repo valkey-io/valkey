8a54ec212d7594be68e9f4f7524e12e0b54e2d95
Roshan Swain <swainroshan001@gmail.com>
2024-10-15 06:14:14 +0530

    feat (deps): adds fast_float library and a c wrapper for from_chars for replacing strtod

---
+++ deps/fast_float/fast_float_strtod.h
@@ -0,0 +1,15 @@
+#ifndef FAST_FLOAT_STRTOD_H
+#define FAST_FLOAT_STRTOD_H
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+    double fast_float_strtod(const char *nptr, char **endptr);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // FAST_FLOAT_STRTOD_H

e34b0914c8aef11e64df4af6bde92545161154bf
Amit Nagler <58042354+naglera@users.noreply.github.com>
2024-12-01 15:33:43 +0200

    Split dual-channel COB overrun tests to separate servers (#1374)
    1. The test isn't waiting long enough for the output buffer to overrun.
    This problem is happening because an error from the previous test is
    bleeding into the current test's logs. The simplest fix would be to
    split these tests.
    2. Increased replication timeout to ensure sync fails due to output
    buffer overrun before a timeout occurs.
    
    Fixes #1367
    
    Signed-off-by: naglera <anagler123@gmail.com>
    (cherry picked from commit 7043ef0bbb627b66bcaa75351b1b141c96852df8)
    (cherry picked from commit c40dc4ecb62b408c00e32560611e233505dfed87)

--- tests/integration/dual-channel-replication.tcl
+++ tests/integration/dual-channel-replication.tcl
@@ -159,7 +159,7 @@ start_server {tags {"dual-channel-replication external:skip"}} {
         set load_handle1 [start_one_key_write_load $primary_host $primary_port 100 "mykey1"]
         set load_handle2 [start_one_key_write_load $primary_host $primary_port 100 "mykey2"]
         set load_handle3 [start_one_key_write_load $primary_host $primary_port 100 "mykey3"]
-        
+
         # wait for load handlers to start
         wait_for_condition 50 1000 {
             ([$primary get "mykey1"] != "") &&
@@ -802,7 +802,7 @@ start_server {tags {"dual-channel-replication external:skip"}} {
         set replica_port [srv 0 port]
         set replica_log [srv 0 stdout]
         set replica_pid  [srv 0 pid]
-        
+
         set load_handle0 [start_write_load $primary_host $primary_port 60]
         set load_handle1 [start_write_load $primary_host $primary_port 60]
         set load_handle2 [start_write_load $primary_host $primary_port 60]
@@ -810,7 +810,7 @@ start_server {tags {"dual-channel-replication external:skip"}} {
 
         $replica config set dual-channel-replication-enabled yes
         $replica config set loglevel debug
-        $replica config set repl-timeout 10
+        $replica config set repl-timeout 60
         $primary config set repl-backlog-size 1mb
 
         test "Test dual-channel-replication primary gets cob overrun before established psync" {
@@ -852,6 +852,37 @@ start_server {tags {"dual-channel-replication external:skip"}} {
         } else {
             fail "Primary should abort sync"
         }
+        stop_write_load $load_handle0
+        stop_write_load $load_handle1
+        stop_write_load $load_handle2
+    }
+}
+
+start_server {tags {"dual-channel-replication external:skip"}} {
+    set primary [srv 0 client]
+    set primary_host [srv 0 host]
+    set primary_port [srv 0 port]
+    set loglines [count_log_lines 0]
+
+    $primary config set repl-diskless-sync yes
+    $primary config set dual-channel-replication-enabled yes
+    $primary config set client-output-buffer-limit "replica 1100k 0 0"
+    $primary config set loglevel debug
+    start_server {} {
+        set replica [srv 0 client]
+        set replica_host [srv 0 host]
+        set replica_port [srv 0 port]
+        set replica_log [srv 0 stdout]
+        set replica_pid  [srv 0 pid]
+
+        set load_handle0 [start_write_load $primary_host $primary_port 60]
+        set load_handle1 [start_write_load $primary_host $primary_port 60]
+        set load_handle2 [start_write_load $primary_host $primary_port 60]
+
+        $replica config set dual-channel-replication-enabled yes
+        $replica config set loglevel debug
+        $replica config set repl-timeout 60
+        $primary config set repl-backlog-size 1mb
         
         $replica debug pause-after-fork 1
         $primary debug populate 1000 primary 100000

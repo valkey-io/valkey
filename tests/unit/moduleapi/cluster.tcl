45bcc730f5e85a8d2829a8fb6a99c1c3d0d736c9
Nikhil Manglore <nmanglor@amazon.com>
2025-02-24 18:21:34 +0000

    Refactored Code
    Signed-off-by: Nikhil Manglore <nmanglor@amazon.com>

--- tests/unit/moduleapi/cluster.tcl
+++ tests/unit/moduleapi/cluster.tcl
@@ -21,26 +21,16 @@ start_cluster 3 0 [list config_lines $modules] {
         } else {
             fail "node 2 or node 3 didn't receive cluster module message"
         }
-    }
-}
-
-start_cluster 3 0 [list config_lines $modules] {
-    set node1 [srv 0 client]
-    set node2 [srv -1 client]
-    set node3 [srv -2 client]
-
-    test "Cluster module message DING/DONG acknowledgment" {
-        assert_equal OK [$node1 test.pingall]
-        wait_for_condition 50 100 {
-            [CI 0 cluster_stats_messages_module_received] eq 2 &&
-            [CI 1 cluster_stats_messages_module_received] eq 1 &&
-            [CI 2 cluster_stats_messages_module_received] eq 1
-        } else {
-            fail "node 2 or node 3 didn't receive DING messages or node 1 didn't receive DONG message"
-        }
-
         verify_log_message -1 "*DING (type 1) RECEIVED*Hey*" 0
         verify_log_message -2 "*DING (type 1) RECEIVED*Hey*" 0
+    }
+
+    test "Cluster module receive message API - VM_RegisterClusterMessageReceiver" {
+        wait_for_condition 50 100 {
+            [CI 0 cluster_stats_messages_module_received] eq 2
+        } else {
+            fail "node 1 didn't receive DONG messages"
+        }
         assert_equal 2 [count_log_message 0 "* <cluster> DONG (type 2) RECEIVED*"]
     }
 }

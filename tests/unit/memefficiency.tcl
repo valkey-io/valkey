88a7fdc7636586e85b36d6dc0440b7518ea6752c
Jim Brunner <brunnerj@amazon.com>
2025-02-25 13:48:20 -0800

    Update tests/unit/memefficiency.tcl
    Signed-off-by: Jim Brunner <brunnerj@amazon.com>

--- tests/unit/memefficiency.tcl
+++ tests/unit/memefficiency.tcl
@@ -434,7 +434,7 @@ run_solo {defrag} {
             # number of total fields.  zsets are progressively increasing sizes.
             set n 200000
 
-            perform_defrag_test $title {
+            perform_defrag_test $title populate {
                 set rd [valkey_deferring_client]
                 set val [string repeat A 200]
                 set k 0

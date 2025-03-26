31ce0f65dffd1bd06f8bccdfa06351adb8a57539
jeon1226 <jeon1226@gmail.com>
2025-03-26 13:09:49 -0700

    Set errno to 0 when successfully return the value
    Signed-off-by: jeon1226 <jeon1226@gmail.com>

--- src/module.c
+++ src/module.c
@@ -9770,6 +9770,7 @@ ValkeyModuleString *VM_GetCurrentUserName(ValkeyModuleCtx *ctx) {
         errno = EINVAL;
         return NULL;
     }
+    errno = 0;
     return VM_CreateString(ctx, ctx->client->user->name, sdslen(ctx->client->user->name));
 }
 

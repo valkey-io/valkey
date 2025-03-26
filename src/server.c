d9557a88348b901be852fd0f174fdf4d28b54bd7
xbasel <103044017+xbasel@users.noreply.github.com>
2025-03-26 03:11:54 +0200

    Initialize repl_cur_reads_per_io_event
    Signed-off-by: xbasel <103044017+xbasel@users.noreply.github.com>

--- src/server.c
+++ src/server.c
@@ -2807,6 +2807,7 @@ void initServer(void) {
     server.reply_buffer_resizing_enabled = 1;
     server.client_mem_usage_buckets = NULL;
     server.repl_last_rate_update = 0;
+    server.repl_cur_reads_per_io_event = 0;
     resetReplicationBuffer();
 
     /* Make sure the locale is set on startup based on the config file. */

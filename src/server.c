3f790da071e3a3586badd295aaa14f316709e3e7
yairgott <yairgott@gmail.com>
2025-04-17 19:20:11 +0000

    removing a defensive call to commitDeferredReplyBuffer just after processing a command
    Signed-off-by: yairgott <yairgott@gmail.com>

--- src/server.c
+++ src/server.c
@@ -3745,7 +3745,6 @@ void call(client *c, int flags) {
 
     exitExecutionUnit();
 
-    commitDeferredReplyBuffer(c, 1);
     /* In case client is blocked after trying to execute the command,
      * it means the execution is not yet completed and we MIGHT reprocess the command in the future. */
     if (!c->flag.blocked) c->flag.executing_command = 0;

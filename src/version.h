12007c952dfec96c15e9c2195fc049b5ac230fb9
Jacob Murphy <jkmurphy@google.com>
2025-04-23 18:31:47 +0000

    Add release notes for 7.2.9
    Signed-off-by: Jacob Murphy <jkmurphy@google.com>

--- src/version.h
+++ src/version.h
@@ -1,6 +1,6 @@
 #define SERVER_NAME "valkey"
-#define VALKEY_VERSION "7.2.8"
-#define VALKEY_VERSION_NUM 0x00070208
+#define VALKEY_VERSION "7.2.9"
+#define VALKEY_VERSION_NUM 0x00070209
 
 /* Redis compatibility version, should never
  * exceed 7.2.x. */

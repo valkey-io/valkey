c3031837402d51703b184143ad13889774d2a3d3
Lipeng Zhu <zhu.lipeng@outlook.com>
2025-05-23 16:31:35 +0800

    Update src/util.c
    Co-authored-by: Viktor Söderqvist <viktor.soderqvist@est.tech>
    Signed-off-by: Lipeng Zhu <zhu.lipeng@outlook.com>

--- src/util.c
+++ src/util.c
@@ -626,6 +626,8 @@ static int string2llScalar(const char *s, size_t slen, long long *value) {
 
 #if HAVE_IFUNC && HAVE_X86_SIMD
 __attribute__((no_sanitize_address)) static int (*string2ll_resolver(void))(const char *, size_t, long long *) {
+    /* Ifunc resolvers run before ASan initialization and before CPU detection
+     * is initialized, so disable ASan and init CPU detection here. */
     __builtin_cpu_init();
     if (__builtin_cpu_supports("avx512f") &&
         __builtin_cpu_supports("avx512vl") &&

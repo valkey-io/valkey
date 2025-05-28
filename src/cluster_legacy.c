0b7343942e12ea910ed152bc7a9b5a00c5b07770
Madelyn Olson <madelyneolson@gmail.com>
2025-05-28 19:15:07 +0100

    Correctly cast the extension lengths (#2144)
    Correctly use a 32 bit integer for accumulating the length of ping
    extensions.
    
    The current code may accidentally truncate the length of an
    extension that is greater than 64kb and fail the validation check. We
    don't currently emit any extensions that are this length, but if we were
    to do so in the future we might have issues with older nodes (without
    this fix) will silently drop packets from newer nodes. We should
    backport this to all versions.
    
    Signed-off-by: Madelyn Olson <madelyneolson@gmail.com>
    Signed-off-by: Harkrishn Patro <harkrisp@amazon.com>

--- src/cluster_legacy.c
+++ src/cluster_legacy.c
@@ -3136,7 +3136,7 @@ int clusterIsValidPacket(clusterLink *link) {
         if (hdr->mflags[0] & CLUSTERMSG_FLAG0_EXT_DATA) {
             clusterMsgPingExt *ext = getInitialPingExt(hdr, count);
             while (extensions--) {
-                uint16_t extlen = getPingExtLength(ext);
+                uint32_t extlen = getPingExtLength(ext);
                 if (extlen % 8 != 0) {
                     serverLog(LL_WARNING, "Received a %s packet without proper padding (%d bytes)",
                               clusterGetMessageTypeString(type), (int)extlen);

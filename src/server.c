255004b440d388fbe1fa79d35df0916fbb8c3b89
Binbin <binloveplay1314@qq.com>
2025-06-04 12:33:28 +0800

    Remove dead conditions around the multi/exec check
    After #723, this became dead conditions since we will reject
    the command in advance and now it won't be able to go here
    anymore (call won't be called anymore).
    
    Signed-off-by: Binbin <binloveplay1314@qq.com>

--- src/server.c
+++ src/server.c
@@ -4340,7 +4340,7 @@ int processCommand(client *c) {
 
     /* Exec the command */
     if (c->flag.multi && c->cmd->proc != execCommand && c->cmd->proc != discardCommand &&
-        c->cmd->proc != multiCommand && c->cmd->proc != watchCommand && c->cmd->proc != quitCommand &&
+        c->cmd->proc != quitCommand &&
         c->cmd->proc != resetCommand) {
         queueMultiCommand(c, cmd_flags);
         addReply(c, shared.queued);

1e9274ceb1eb1e0e089f7c3c5166fe40877805d0
Josh Soref <2119212+jsoref@users.noreply.github.com>
2025-06-05 18:58:23 -0400

    spelling: previously
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- src/module.c
+++ src/module.c
@@ -8187,7 +8187,7 @@ ValkeyModuleBlockedClient *VM_BlockClientOnAuth(ValkeyModuleCtx *ctx,
     return bc;
 }
 
-/* Get the private data that was previusely set on a blocked client */
+/* Get the private data that was previously set on a blocked client */
 void *VM_BlockClientGetPrivateData(ValkeyModuleBlockedClient *blocked_client) {
     return blocked_client->privdata;
 }

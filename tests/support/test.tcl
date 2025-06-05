50e04db72d96657a946f14c7037dbf360c9e3216
Josh Soref <2119212+jsoref@users.noreply.github.com>
2025-06-05 17:10:27 -0400

    spelling: condition
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- tests/support/test.tcl
+++ tests/support/test.tcl
@@ -125,7 +125,7 @@ proc assert_refcount_morethan {key ref} {
 # Wait for the specified condition to be true, with the specified number of
 # max retries and delay between retries. Otherwise the 'elsescript' is
 # executed. If 'debugscript' is provided, it is executed after failure of
-# the confition (before the retry delay).
+# the condition (before the retry delay).
 proc wait_for_condition {maxtries delay e _else_ elsescript {_debug_ ""} {debugscript ""}} {
     while {[incr maxtries -1] >= 0} {
         set errcode [catch {uplevel 1 [list expr $e]} result]

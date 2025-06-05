8531a0d7d9d26feb4453fa27c7c261691e7f66d9
Josh Soref <2119212+jsoref@users.noreply.github.com>
2025-06-05 19:00:40 -0400

    spelling: rehashing
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- src/unit/test_rax.c
+++ src/unit/test_rax.c
@@ -47,7 +47,7 @@ long long _ustime(void);                  /* From test_crc64combine.c */
  * used in order to test the radix tree implementation against something that
  * will always "tell the truth" :-) */
 
-/* This is huge but we want it fast enough without reahshing needed. */
+/* This is huge but we want it fast enough without rehashing needed. */
 #define HT_TABLE_SIZE 100000
 typedef struct htNode {
     uint64_t keylen;

220c365f670853eb9d5bd8385b94666f9d2714b1
Binbin <binloveplay1314@qq.com>
2025-06-23 20:31:42 +0800

    Update src/aof.c
    Co-authored-by: Viktor Söderqvist <viktor.soderqvist@est.tech>
    Signed-off-by: Binbin <binloveplay1314@qq.com>

--- src/aof.c
+++ src/aof.c
@@ -2470,7 +2470,7 @@ void bgrewriteaofCommand(client *c) {
     }
 }
 
-/* Note that we may call this function in signal handle 'sigShutdownHandler',
+/* Note that we may call this function in signal handler 'sigShutdownHandler',
  * so we need guarantee all functions we call are async-signal-safe.
  * If we call this function from signal handle, we won't call bg_unlink that
  * is not async-signal-safe. */

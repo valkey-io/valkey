7af74bdf86c737390076232f37f73fc4c2124215
yairgott <yairgott@gmail.com>
2025-07-03 02:16:26 +0000

    fixing some issues
    Signed-off-by: yairgott <yairgott@gmail.com>

--- tests/modules/hash_externalizer.c
+++ tests/modules/hash_externalizer.c
@@ -9,14 +9,13 @@ int hash_extern(ValkeyModuleCtx *ctx, ValkeyModuleString **argv, int argc) {
     size_t buf_len;
     const char *buf = ValkeyModule_StringPtrLen(argv[3], &buf_len);
 
-    /* Test some varargs. (In real-world, use a loop and set one at a time.) */
     return ValkeyModule_HashExternalize(key, argv[2], buf, buf_len);
 }
 
 int ValkeyModule_OnLoad(ValkeyModuleCtx *ctx, ValkeyModuleString **argv, int argc) {
     VALKEYMODULE_NOT_USED(argv);
     VALKEYMODULE_NOT_USED(argc);
-    if (ValkeyModule_Init(ctx, "hash", 1, VALKEYMODULE_APIVER_1) ==
+    if (ValkeyModule_Init(ctx, "hash.extern", 1, VALKEYMODULE_APIVER_1) ==
         VALKEYMODULE_OK &&
         ValkeyModule_CreateCommand(ctx, "hash.extern", hash_extern, "write",
                                   1, 1, 1) == VALKEYMODULE_OK) {

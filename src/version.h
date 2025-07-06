71e6ebdce6f487fc445ed153c3c96deaeaeee388
Ran Shidlansik <ranshid@amazon.com>
2025-07-07 00:55:48 +0300

    Release notes for 8.0.4
    Signed-off-by: Ran Shidlansik <ranshid@amazon.com>

--- src/version.h
+++ src/version.h
@@ -4,8 +4,8 @@
  * similar. */
 #define SERVER_NAME "valkey"
 #define SERVER_TITLE "Valkey"
-#define VALKEY_VERSION "8.0.3"
-#define VALKEY_VERSION_NUM 0x00080003
+#define VALKEY_VERSION "8.0.4"
+#define VALKEY_VERSION_NUM 0x00080004
 
 /* Redis OSS compatibility version, should never
  * exceed 7.2.x. */

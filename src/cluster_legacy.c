bdbd9a61ba491cb452abbf39987c7b5d3e38a856
Pari Mohite <parimoh@amazon.com>
2025-08-04 17:15:13 +0000

    access module ID with different method
    Signed-off-by: Pari Mohite <parimoh@amazon.com>

--- src/cluster_legacy.c
+++ src/cluster_legacy.c
@@ -4032,7 +4032,8 @@ void clusterWriteHandler(connection *conn) {
             server.cluster_bus_pubsub_bytes_sent += nwritten;
         } else if (type == CLUSTERMSG_TYPE_MODULE) {
             /* This is for any module-related traffic */
-            updateModuleTraffic(server.cluster_bus_module_id_traffic, ntohu64(msg->data.module.msg.module_id), nwritten, true);
+            uint64_t module_id = ntohu64(((clusterMsg*)msgblock->data)->data.module.msg.module_id);
+            updateModuleTraffic(server.cluster_bus_module_id_traffic, module_id, nwritten, true);
         } else {
             /* This is admin traffic (PING, PONG, MEET, FAIL, etc.) */
             server.cluster_bus_admin_bytes_sent += nwritten;

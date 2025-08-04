6a965acb81f930dbe8ac75d17ec394981121e221
Ran Shidlansik <ranshid@amazon.com>
2025-08-04 22:35:31 +0300

    make valkey-rdb-check identical to server check
    Signed-off-by: Ran Shidlansik <ranshid@amazon.com>

--- src/valkey-check-rdb.c
+++ src/valkey-check-rdb.c
@@ -612,13 +612,19 @@ int redis_check_rdb(char *rdbfilename, FILE *fp) {
     rdb.update_cksum = rdbLoadProgressCallback;
     if (rioRead(&rdb, buf, 9) == 0) goto eoferr;
     buf[9] = '\0';
-    if (memcmp(buf, "REDIS0", 6) != 0 && memcmp(buf, "VALKEY", 6) != 0) {
+    bool is_valkey_magic;
+    if (memcmp(buf, "REDIS0", 6) == 0) {
+        is_valkey_magic = false;
+    } else if (memcmp(buf, "VALKEY", 6) == 0) {
+        is_valkey_magic = true;
+    } else {
         rdbCheckError("Wrong signature trying to load DB from file");
         goto err;
     }
     rdbver = atoi(buf + 6);
-
-    if (rdbver < 1 || rdbver > RDB_VERSION || (rdbver >= RDB_FOREIGN_VERSION_MIN && rdbver <= RDB_FOREIGN_VERSION_MAX)) {
+    if (rdbver < 1 || rdbver > RDB_VERSION ||
+        (rdbver >= RDB_FOREIGN_VERSION_MIN && rdbver <= RDB_FOREIGN_VERSION_MAX) ||
+        (rdbver > RDB_FOREIGN_VERSION_MAX && !is_valkey_magic)) {
         rdbCheckError("Can't handle RDB format version %d", rdbver);
         goto err;
     }

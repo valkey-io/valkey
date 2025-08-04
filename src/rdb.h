438564d0f569628d526ed87855eaa542fa9197a6
Ran Shidlansik <ranshid@amazon.com>
2025-08-04 21:58:58 +0300

    fix rdb version documentation
    Signed-off-by: Ran Shidlansik <ranshid@amazon.com>

--- src/rdb.h
+++ src/rdb.h
@@ -41,8 +41,7 @@
  *
  * RDB 11 is the last open-source Redis RDB version, used by Valkey 7.x and 8.x.
  *
- * RDB 12+ are non-open-source Redis formats.
- * VALKEY 80+ are Valkey open-source versions.
+ * RDB 12-79 are reserved for Redis non-compatible RDB formats  
  *
  * We start using high rdb version numbers since Valkey 9.0. This is in order to avoid
  * collisions with non-OSS Redis RDB versions.

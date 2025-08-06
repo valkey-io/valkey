27310ab0c9613341b3f834f907879c4b55ab79eb
uriyage <78144248+uriyage@users.noreply.github.com>
2025-08-06 17:02:10 +0300

    [CRASH] Fix missing check for executing client (#2347)
    Fix missing check for executing client in `lookupKey` function
    
    The `lookupKey` function in db.c accesses
    `server.executing_client->cmd->proc` without first verifying that
    `server.executing_client` is not NULL. This was introduced in #1499
    where the check for executing client was added without verifying it
    could be null.
    
    The server crashes with a null pointer dereference when the
    current_client's flag.no_touch is set.
    ```
    27719 valkey-server *
    /lib64/libpthread.so.0(+0x118e0)[0x7f34cb96a8e0]
    src/valkey-server 127.0.0.1:21113(lookupKey+0xf5)[0x4a14b7]
    src/valkey-server 127.0.0.1:21113(lookupKeyReadWithFlags+0x50)[0x4a15fc]
    src/valkey-server 127.0.0.1:21113[0x52b8f1]
    src/valkey-server 127.0.0.1:21113(handleClientsBlockedOnKeys+0xa5)[0x52b16f]
    src/valkey-server 127.0.0.1:21113(processCommand+0xf1e)[0x4712c9]
    src/valkey-server 127.0.0.1:21113(processCommandAndResetClient+0x35)[0x490fd5]
    src/valkey-server 127.0.0.1:21113(processInputBuffer+0xe1)[0x4912e5]
    src/valkey-server 127.0.0.1:21113(readQueryFromClient+0x8c)[0x49177b]
    src/valkey-server 127.0.0.1:21113[0x57daa6]
    src/valkey-server 127.0.0.1:21113[0x57e280]
    src/valkey-server 127.0.0.1:21113(aeProcessEvents+0x261)[0x45b259]
    src/valkey-server 127.0.0.1:21113(aeMain+0x2a)[0x45b450]
    src/valkey-server 127.0.0.1:21113(main+0xd43)[0x479bf6]
    /lib64/libc.so.6(__libc_start_main+0xea)[0x7f34cb5cd13a]
    src/valkey-server 127.0.0.1:21113(_start+0x2a)[0x454e3a]
    ```
    
    Added a null check for `server.executing_client` before attempting to
    dereference it:
    
    Added a regression test in tests/unit/type/list.tcl.
    
    ---------
    
    Signed-off-by: Uri Yagelnik <uriy@amazon.com>
    Signed-off-by: Ran Shidlansik <ranshid@amazon.com>
    Co-authored-by: Ran Shidlansik <ranshid@amazon.com>

--- src/db.c
+++ src/db.c
@@ -124,8 +124,9 @@ robj *lookupKey(serverDb *db, robj *key, int flags) {
         /* Update the access time for the ageing algorithm.
          * Don't do it if we have a saving child, as this will trigger
          * a copy on write madness. */
-        if (server.current_client && server.current_client->flag.no_touch &&
-            server.executing_client->cmd->proc != touchCommand)
+        if ((flags & LOOKUP_NOTOUCH) == 0 &&
+            server.current_client && server.current_client->flag.no_touch &&
+            server.executing_client && server.executing_client->cmd->proc != touchCommand)
             flags |= LOOKUP_NOTOUCH;
         if (!hasActiveChildProcess() && !(flags & LOOKUP_NOTOUCH)) {
             /* Shared objects can't be stored in the database. */

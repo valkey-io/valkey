37a006c2bfc6a03d9c1f2cc83d8aaecce38c25d6
Yair Gottdenker <yairg@google.com>
2025-09-10 20:14:49 +0000

    fixing module test
    Signed-off-by: Yair Gottdenker <yairg@google.com>

--- tests/unit/moduleapi/hash_stringref.tcl
+++ tests/unit/moduleapi/hash_stringref.tcl
@@ -9,8 +9,6 @@ start_server {tags {"modules"}} {
         assert {$status == 1}
         r hset k f hello1
         assert_equal "0" [r hash.has_stringref k f]
-        set status [catch {r hash.set_stringref k f hello2} errmsg]
-        assert {$status == 1}
         r hash.set_stringref k f hello1
         assert_equal "hello1" [r hget k f]
         assert_equal "1" [r hash.has_stringref k f]

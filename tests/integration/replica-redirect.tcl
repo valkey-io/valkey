9401618908866edc796747345130c46d52a4cda2
cjx-zar <jxchenczar@foxmail.com>
2025-09-28 11:00:22 +0800

    add more tests
    Signed-off-by: cjx-zar <jxchenczar@foxmail.com>
    
    Co-authored-by: Simon Baatz <gmbnomis@gmail.com>

--- tests/integration/replica-redirect.tcl
+++ tests/integration/replica-redirect.tcl
@@ -185,75 +185,69 @@ start_server {tags {needs:repl external:skip}} {
 
             $rd_xread close
         }
-    }
-}
 
-start_server {tags {needs:repl external:skip}} {
-    start_server {} {
-        set master [srv 0 client]
-        set master_host [srv 0 host]
-        set master_port [srv 0 port]
-        set replica [srv -1 client]
-        set replica_host [srv -1 host]
-        set replica_port [srv -1 port]
+        test {blocked clients behavior during failover} {
+            r -1 replicaof no one
+            r -1 flushall
+            r replicaof $primary_host $primary_port
+            wait_replica_online $primary
 
-        $replica replicaof $master_host $master_port
-        wait_for_condition 100 100 {
-            [string match *slave*online* [$master info replication]] &&
-            [s -1 master_link_status] eq {up}
-        } else {
-            fail "Replica not online."
-        }
+            # Client blocking on primary
+            set rd0 [valkey_deferring_client -1]
+            $rd0 CLIENT CAPA REDIRECT
+            assert_match "OK" [$rd0 read]
+            $rd0 BLPOP mylist 0
 
-        test {client with REDIRECT capability receives REDIRECT error during failover} {
-            set rd [valkey_deferring_client]
-            $rd client capa redirect
-            assert_match "OK" [$rd read]
+            # Readonly client blocking on primary
+            set rd0_ro [valkey_deferring_client -1]
+            $rd0_ro CLIENT CAPA REDIRECT
+            assert_match "OK" [$rd0_ro read]
+            $rd0_ro READONLY
+            assert_match "OK" [$rd0_ro read]
+            $rd0_ro XREAD BLOCK 0 STREAMS mystream 0-0
 
-            # client blocking on primary
-            $rd blpop mylist 0
-
-            r failover to $replica_host $replica_port
+            # Readonly client blocking on replica
+            set rd1 [valkey_deferring_client 0]
+            $rd1 CLIENT CAPA REDIRECT
+            assert_match "OK" [$rd1 read]
+            $rd1 READONLY
+            assert_equal OK [$rd1 read]
+            $rd1 XREAD BLOCK 0 STREAMS k 0-0
 
             wait_for_condition 50 100 {
-                [s master_failover_state] == "no-failover"
+                [s -1 blocked_clients] eq 2 &&
+                [s 0 blocked_clients] eq 1
+            } else {
+                fail "client wasn't blocked"
+            }
+
+            r -1 FAILOVER TO $replica_host $replica_port
+
+            wait_for_condition 50 100 {
+                [s -1 role] eq {slave} &&
+                [s 0 role] eq {master}
             } else {
                 fail "Failover did not complete"
             }
 
-            assert_match *slave* [r role]
-            assert_match *master* [r -1 role]
-            assert_error "REDIRECT $replica_host:$replica_port" {$rd read}
-            $rd close
+            assert_error "REDIRECT $replica_host:$replica_port" {$rd0 read}
+
+            # Check that the readonly client blocked on the new replica (old primary) is still blocked.
+            assert_equal 1 [s 0 blocked_clients]
+
+            # Check that the client blocked on the new primary (old replica) is still blocked.
+            assert_equal 1 [s -1 blocked_clients]
+
+            # Add an entry to the stream to unblock the blocking XREAD.
+            set stream_id [r XADD k * foo bar]
+            assert_equal "{k {{$stream_id {foo bar}}}}" [$rd1 read]
+            set stream_id [r XADD mystream * foo bar]
+            assert_equal "{mystream {{$stream_id {foo bar}}}}" [$rd0_ro read]
+
+            $rd0 close
+            $rd0_ro close
+            $rd1 close
         }
     }
 }
-
-start_cluster 1 1 {tags {external:skip cluster}} {
-    test "Cluster is up" {
-        wait_for_cluster_state ok
-    }
-
-    test {blocked client receives MOVED during failover} {
-        set rd [valkey_deferring_client 0]
-        $rd blpop mylist 0
-
-        R 1 cluster failover
-
-        wait_for_condition 1000 50 {
-            [s -1 role] eq {master} &&
-            [s 0 role] eq {slave}
-        } else {
-            fail "The failover does not happen"
-        }
-
-        wait_for_condition 1000 50 {
-            [catch {$rd read} err] &&
-            [string match "MOVED*" $err]
-        } else {
-            fail "Client didn't receive MOVED error"
-        }
-        $rd close
-    }
-} ;# start_cluster
 

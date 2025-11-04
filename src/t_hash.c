a8a49eb822d94cb9ffa1031f693681b021740673
Sarthak Aggarwal <sarthagg@amazon.com>
2025-11-04 09:35:38 -0800

    dup comment
    Signed-off-by: Sarthak Aggarwal <sarthagg@amazon.com>

--- src/t_hash.c
+++ src/t_hash.c
@@ -147,7 +147,6 @@ static void hashTypeTrackUpdateEntry(robj *o, entry *old_entry, entry *new_entry
 }
 
 // This is a hashtableType validateEntry callback
-// This is a hashtableType valicateEntry callback
 bool hashHashtableTypeValidate(hashtable *ht, void *entryptr) {
     UNUSED(ht);
     entry *entry = entryptr;

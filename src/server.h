4aa2f9574b4c202d6c645ca962b4a7a4de791748
Venkat Pamulapati <pamuvenk@amazon.com>
2025-11-18 19:14:51 +0000

    comment formatting
    Signed-off-by: Venkat Pamulapati <pamuvenk@amazon.com>

--- src/server.h
+++ src/server.h
@@ -347,8 +347,8 @@ extern int configOOMScoreAdjValuesDefaults[CONFIG_OOM_COUNT];
 
 /* RDB return values for rdbLoad. */
 #define RDB_OK 0
-#define RDB_NOT_EXIST 1 /* RDB file doesn't exist. */
-#define RDB_FAILED 2    /* Failed to load the RDB file. */
+#define RDB_NOT_EXIST 1    /* RDB file doesn't exist. */
+#define RDB_FAILED 2       /* Failed to load the RDB file. */
 #define RDB_INCOMPATIBLE 3 /* RDB version or signature is not compatible */
 
 /* Command doc flags */

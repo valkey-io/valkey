765395d1930ef6f3f3c000071b1bf9b67f641fc3
yzc-yzc <96833212+yzc-yzc@users.noreply.github.com>
2025-11-18 23:06:20 +0800

    Fix SCAN consistency test to only test what we guarantee (#2853)
    Test the SCAN consistency by alternating SCAN
    calls to primary and replica.
    We cannot rely on the exact order of the elements and the returned
    cursor number.
    
    ---------
    
    Signed-off-by: yzc-yzc <96833212+yzc-yzc@users.noreply.github.com>
    Co-authored-by: Viktor Söderqvist <viktor.soderqvist@est.tech>
    Signed-off-by: jjuleslasarte <jules.lasarte@gmail.com>

--- tests/integration/scan-family-consistency.tcl
+++ tests/integration/scan-family-consistency.tcl
@@ -1,60 +1,66 @@
+proc scan_interleaved {primary replica cmd {key ""} args} {
+    set cursor 0
+    set keys {}
+    set toggle [randomInt 2]
+    while {1} {
+        if {$key != ""} {
+            set cmd_args [list $key $cursor {*}$args]
+        } else {
+            set cmd_args [list $cursor {*}$args]
+        }
+
+        if {$toggle == 0} {
+            set scan_result [$primary $cmd {*}$cmd_args]
+        } else {
+            set scan_result [$replica $cmd {*}$cmd_args]
+        }
+        lappend keys {*}[lindex $scan_result 1]
+        if {[lindex $scan_result 0] eq 0} {
+            break
+        }
+        set cursor [lindex $scan_result 0]
+        set toggle [expr {1 - $toggle}]
+    }
+    return $keys
+}
+
 test {scan family consistency with configured hash seed} {
-    start_server {tags {"external:skip"}} {
+    set fixed_seed [randstring 16 16 alpha]
+    set shared_overrides [list appendonly no save "" hash-seed $fixed_seed]
 
-        set fixed_seed "aabbccddeeffgghh"
-        set shared_overrides [list appendonly no save "" hash-seed $fixed_seed activedefrag no hz 1]
+    start_multiple_servers 2 [list overrides $shared_overrides] {
+        set primary [srv -1 client]
+        set replica [srv 0 client]
 
-        start_server [list overrides $shared_overrides] {
-            set primary_host [srv 0 host]
-            set primary_port [srv 0 port]
+        set primary_host [srv -1 host]
+        set primary_port [srv -1 port]
 
-            start_server [list overrides $shared_overrides] {
-                set primary [srv -1 client]
-                set replica [srv 0 client]
+        $primary flushall
+        $replica replicaof $primary_host $primary_port
+        wait_for_sync $replica
 
-                $primary flushall
-                $replica replicaof $primary_host $primary_port
-                wait_replica_online $primary
+        set n 50
+        for {set i 0} {$i < $n} {incr i} {
+            $primary set "k:$i" x
+            $primary hset h "f:$i" $i
+            $primary sadd s "m:$i"
+            $primary zadd z $i "m:$i"
+        }
 
-                set n 50
-                for {set i 0} {$i < $n} {incr i} {
-                    $primary set "k:$i" x
-                    $primary hset h "f:$i" $i
-                    $primary sadd s "m:$i"
-                    $primary zadd z $i "m:$i"
-                }
+        wait_for_condition 200 50 {
+            [$replica dbsize] == [$primary dbsize]
+        } else {
+            fail "replica did not catch up dbsize (primary=[$primary dbsize], replica=[$replica dbsize])"
+        }
 
-                wait_for_condition 200 50 {
-                    [$replica dbsize] == [$primary dbsize]
-                } else {
-                    fail "replica did not catch up dbsize (primary=[$primary dbsize], replica=[$replica dbsize])"
-                }
-                set cursor {{0} {}}
-                while {1} {
-                    set primary_cursor_next [$primary scan [lindex $cursor 0]]
-                    set replica_cursor_next [$replica scan [lindex $cursor 0]]
-                    assert_equal $primary_cursor_next $replica_cursor_next
-                    if {[lindex $primary_cursor_next 0] eq "0"} {
-                        assert_equal "0" [lindex $replica_cursor_next 0]
-                        break
-                    }
-                    set cursor $primary_cursor_next
-                }
+        set keys [scan_interleaved $primary $replica scan]
+        set keys [lsort -unique $keys]
+        assert_equal [expr {$n+3}] [llength $keys]
 
-                foreach {cmd key} {hscan h sscan s zscan z} {
-                    set cursor {{0} {}}
-                    while {1} {
-                        set primary_cursor_next [$primary $cmd $key [lindex $cursor 0]]
-                        set replica_cursor_next [$replica $cmd $key [lindex $cursor 0]]
-                        assert_equal $primary_cursor_next $replica_cursor_next
-                        if {[lindex $primary_cursor_next 0] eq "0"} {
-                            assert_equal "0" [lindex $replica_cursor_next 0]
-                            break
-                        }
-                        set cursor $primary_cursor_next
-                    }
-                }
-            }
+        foreach {cmd key extra} {hscan h {novalues} sscan s {} zscan z {noscores}} {
+            set items [scan_interleaved $primary $replica $cmd $key {*}$extra]
+            set items [lsort -unique $items]
+            assert_equal $n [llength $items]
         }
     }
-}
+} {} {external:skip}

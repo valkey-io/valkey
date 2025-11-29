f9c3ba0710c848208984a106cdcc31c5d0d68a29
Zhijun <dszhijun@gmail.com>
2025-11-29 10:33:22 +0800

    Address feedback
    Signed-off-by: Zhijun <dszhijun@gmail.com>

--- tests/support/set_executable_path.tcl
+++ tests/support/set_executable_path.tcl
@@ -3,20 +3,20 @@
 # CMake as well, we allow changing base dir by passing ENV variable `VALKEY_BIN_DIR`.
 # This must be an absolute path.
 set ::VALKEY_BIN_DIR [expr {[info exists ::env(VALKEY_BIN_DIR)] ? $::env(VALKEY_BIN_DIR) : "[pwd]/src"}]
-puts "VALKEY_BIN_DIR is set to $VALKEY_BIN_DIR"
 
 # Helper to build absolute paths
-proc __build_absolute_path {name} {
+proc valkey_bin_absolute_path {name} {
     set p [file join $::VALKEY_BIN_DIR $name]
-    if {![file executable $p]} {
-        error "Binary not found or not executable: $p \nDo you intend to run the binaries built from Make or CMake?"
-    }
     return $p
 }
 
-set ::VALKEY_SERVER_BIN    [__build_absolute_path "valkey-server"]
-set ::VALKEY_CLI_BIN       [__build_absolute_path "valkey-cli"]
-set ::VALKEY_BENCHMARK_BIN [__build_absolute_path "valkey-benchmark"]
-set ::VALKEY_CHECK_AOF_BIN [__build_absolute_path "valkey-check-aof"]
-set ::VALKEY_CHECK_RDB_BIN [__build_absolute_path "valkey-check-rdb"]
-set ::VALKEY_SENTINEL_BIN  [__build_absolute_path "valkey-sentinel"]
+set ::VALKEY_SERVER_BIN    [valkey_bin_absolute_path "valkey-server"]
+set ::VALKEY_CLI_BIN       [valkey_bin_absolute_path "valkey-cli"]
+set ::VALKEY_BENCHMARK_BIN [valkey_bin_absolute_path "valkey-benchmark"]
+set ::VALKEY_CHECK_AOF_BIN [valkey_bin_absolute_path "valkey-check-aof"]
+set ::VALKEY_CHECK_RDB_BIN [valkey_bin_absolute_path "valkey-check-rdb"]
+set ::VALKEY_SENTINEL_BIN  [valkey_bin_absolute_path "valkey-sentinel"]
+
+if {![file executable $::VALKEY_SERVER_BIN]} {
+    error "Binary not found or not executable: $::VALKEY_SERVER_BIN"
+}

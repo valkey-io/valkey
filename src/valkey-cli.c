5a4571a37ee2fb8134af4d4e218f3f06fae8ca3e
Jacob Murphy <jkmurphy@google.com>
2025-12-03 01:06:30 +0000

    C-style comment
    Signed-off-by: Jacob Murphy <jkmurphy@google.com>

--- src/valkey-cli.c
+++ src/valkey-cli.c
@@ -7655,7 +7655,7 @@ static int clusterApplyReshardTable(list *table, clusterManagerNode *target, int
     listNode *ln;
     listRewind(table, &li);
     if (opts & CLUSTER_MANAGER_OPT_COLD) {
-        // Cold is only possible via legacy slot migration.
+        /* Cold is only possible via legacy slot migration. */
         opts &= ~CLUSTER_MANAGER_OPT_USE_ATOMIC_SLOT_MIGRATION;
     }
     while ((ln = listNext(&li)) != NULL) {

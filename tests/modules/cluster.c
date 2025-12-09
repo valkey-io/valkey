7a83b85ad4592fe593f81e471e43aaa7934ee953
Su Ko <rhtn1128@gmail.com>
2025-12-09 09:58:17 +0900

    Fix CLUSTER SLOTS crashes when used with fake-client
    Signed-off-by: Su Ko <rhtn1128@gmail.com>

--- tests/modules/cluster.c
+++ tests/modules/cluster.c
@@ -3,6 +3,29 @@
 
 #define UNUSED(x) (void)(x)
 
+void cluster_timer_handler(ValkeyModuleCtx *ctx, void *data) {
+    VALKEYMODULE_NOT_USED(data);
+
+    ValkeyModuleCallReply *rep = ValkeyModule_Call(ctx, "CLUSTER", "c", "SLOTS");
+
+    if (rep) {
+        ValkeyModule_Log(ctx, "notice", "Timer: CLUSTER SLOTS success");
+        ValkeyModule_FreeCallReply(rep);
+    } else {
+        ValkeyModule_Log(ctx, "warning", "Timer: CLUSTER SLOTS failed");
+    }
+}
+
+int test_start_cluster_timer(ValkeyModuleCtx *ctx, ValkeyModuleString **argv, int argc) {
+    VALKEYMODULE_NOT_USED(argv);
+    VALKEYMODULE_NOT_USED(argc);
+
+    ValkeyModule_CreateTimer(ctx, 1, cluster_timer_handler, NULL);
+
+    return ValkeyModule_ReplyWithSimpleString(ctx, "OK");
+}
+
+
 int test_cluster_slots(ValkeyModuleCtx *ctx, ValkeyModuleString **argv, int argc) {
     UNUSED(argv);
 
@@ -77,8 +100,12 @@ int ValkeyModule_OnLoad(ValkeyModuleCtx *ctx, ValkeyModuleString **argv, int arg
     if (ValkeyModule_CreateCommand(ctx, "test.cluster_shards", test_cluster_shards, "", 0, 0, 0) == VALKEYMODULE_ERR)
         return VALKEYMODULE_ERR;
 
+    if (ValkeyModule_CreateCommand(ctx, "test.start_cluster_timer", test_start_cluster_timer, "", 0, 0, 0) == VALKEYMODULE_ERR)
+        return VALKEYMODULE_ERR;
+
     /* Register our handlers for different message types. */
     ValkeyModule_RegisterClusterMessageReceiver(ctx, MSGTYPE_DING, DingReceiver);
     ValkeyModule_RegisterClusterMessageReceiver(ctx, MSGTYPE_DONG, DongReceiver);
+
     return VALKEYMODULE_OK;
 }

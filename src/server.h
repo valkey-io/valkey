6221ca6f016ab8b1fcd990e2a88bbeee52386311
Binbin <binloveplay1314@qq.com>
2025-12-14 15:10:57 +0800

    Strictly check CRLF when parsing querybuf (#2872)
    Currently, when parsing querybuf, we are not checking for CRLF,
    instead we assume the last two characters are CRLF by default,
    as shown in the following example:
    ```
    telnet 127.0.0.1 6379
    Trying 127.0.0.1...
    Connected to 127.0.0.1.
    Escape character is '^]'.
    *3
    $3
    set
    $3
    key
    $5
    value12
    +OK
    get key
    $5
    value
    
    *3
    $3
    set
    $3
    key
    $5
    value12345
    +OK
    -ERR unknown command '345', with args beginning with:
    ```
    
    This should actually be considered a protocol error. When a bug
    occurs in the client-side implementation, we may execute incorrect
    requests (writing incorrect data is the most serious of these).
    
    ---------
    
    Signed-off-by: Binbin <binloveplay1314@qq.com>

--- src/server.h
+++ src/server.h
@@ -2775,6 +2775,7 @@ void dictVanillaFree(dict *d, void *val);
 #define READ_FLAGS_PRIMARY (1 << 14)
 #define READ_FLAGS_DONT_PARSE (1 << 15)
 #define READ_FLAGS_AUTH_REQUIRED (1 << 16)
+#define READ_FLAGS_ERROR_INVALID_CRLF (1 << 17)
 
 /* Write flags for various write errors and states */
 #define WRITE_FLAGS_WRITE_ERROR (1 << 0)

c09c9e2188bd62366c45263bbdbcce24c00cf2eb
Viktor Söderqvist <viktor.soderqvist@est.tech>
2025-12-16 16:29:36 +0100

    spelling
    Signed-off-by: Viktor Söderqvist <viktor.soderqvist@est.tech>

--- src/rdb.c
+++ src/rdb.c
@@ -741,7 +741,7 @@ int rdbGetObjectType(robj *o, int rdbver) {
                 if (rdbver >= 80)
                     return RDB_TYPE_HASH_2;
                 else
-                    return -1; /* can't be represeted in old RDB */
+                    return -1; /* can't be stored in old RDB */
             else
                 return RDB_TYPE_HASH;
         else

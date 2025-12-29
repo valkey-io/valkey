20aa69e827b0470878779df66be79f7510666bb1
Johan Bergström <bugs@bergstroem.nu>
2025-12-29 12:13:11 +0000

    chore: increase escape function coverage
    Signed-off-by: Johan Bergström <bugs@bergstroem.nu>

--- tests/integration/logging.tcl
+++ tests/integration/logging.tcl
@@ -146,11 +146,34 @@ if {$backtrace_supported} {
 
 # test JSON log format with special characters
 start_server {overrides {log-format json}} {
-    test {JSON log format properly escapes special characters} {
+    test {JSON log format escapes backslash and double quote} {
         set log_lines [count_log_lines 0]
+        r debug log "Test \"quotes\" and \\backslash\\"
+        verify_log_message 0 {*"message":"DEBUG LOG: Test \\"quotes\\" and \\\\backslash\\\\"*} $log_lines
+    }
 
-        r debug log "Test \"quotes\" and \\backslash\\ and newline"
-        verify_log_message 0 {*\"message\":\"DEBUG LOG: Test \\"quotes\\" and \\\\backslash\\\\*} $log_lines
+    test {JSON log format escapes tab character} {
+        set log_lines [count_log_lines 0]
+        r debug log "Test\ttab"
+        verify_log_message 0 {*"message":"DEBUG LOG: Test\\ttab"*} $log_lines
+    }
+
+    test {JSON log format escapes carriage return} {
+        set log_lines [count_log_lines 0]
+        r debug log "Test\rcarriage"
+        verify_log_message 0 {*"message":"DEBUG LOG: Test\\rcarriage"*} $log_lines
+    }
+
+    test {JSON log format escapes form feed} {
+        set log_lines [count_log_lines 0]
+        r debug log "Test\fformfeed"
+        verify_log_message 0 {*"message":"DEBUG LOG: Test\\fformfeed"*} $log_lines
+    }
+
+    test {JSON log format escapes backspace} {
+        set log_lines [count_log_lines 0]
+        r debug log "Test\bbackspace"
+        verify_log_message 0 {*"message":"DEBUG LOG: Test\\bbackspace"*} $log_lines
     }
 }
 

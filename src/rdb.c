72a46bed6389d291a200f06a5d0a870468d3bdbe
Ran Shidlansik <ranshid@amazon.com>
2025-12-31 18:32:32 +0200

    fix compilation
    Signed-off-by: Ran Shidlansik <ranshid@amazon.com>

--- src/rdb.c
+++ src/rdb.c
@@ -3307,7 +3307,7 @@ int rdbLoadRioWithLoadingCtx(rio *rdb, int rdbflags, rdbSaveInfo *rsi, rdbLoadin
                 if (server.cluster_enabled && slot_id >= 0 && slot_id < CLUSTER_SLOTS) {
                     /* In cluster mode we resize individual slot specific dictionaries based on the number of keys that
                      * slot holds. */
-                    if (clusterNodeCoversSlot(clusterNodeGetPrimary(getMyClusterNode()), slot_id) || clusterIsSlotImporting(keySlot)) {
+                    if (clusterNodeCoversSlot(clusterNodeGetPrimary(getMyClusterNode()), slot_id) || clusterIsSlotImporting(slot_id)) {
                         if (slot_size) kvstoreHashtableExpand(db->keys, slot_id, slot_size);
                         if (expires_slot_size) kvstoreHashtableExpand(db->expires, slot_id, expires_slot_size);
                         if (keys_with_volatile_items_slot_size) {

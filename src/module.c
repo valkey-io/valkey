243cd2ae62131adcb38c7e2f8bdbdade6fbb70b4
arshidkv12 <arshidkv12@gmail.com>
2026-01-22 06:55:54 +0530

    Fix the memory leak in the VM_GetCommandKeysWithFlags function
    Signed-off-by: arshidkv12 <arshidkv12@gmail.com>

--- src/module.c
+++ src/module.c
@@ -14126,7 +14126,7 @@ int *VM_GetCommandKeysWithFlags(ValkeyModuleCtx *ctx,
         res[i] = result.keys[i].pos;
         if (out_flags) (*out_flags)[i] = moduleConvertKeySpecsFlags(result.keys[i].flags, 0);
     }
-     
+
     getKeysFreeResult(&result);
     return res;
 }

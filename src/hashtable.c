6962d88ac7a76472330e9e050a48daabe93ae58c
Binbin <binloveplay1314@qq.com>
2026-01-31 19:14:14 +0800

    Optimize rehash completion check for empty hash table
    It is possible to have an empty table with only empty buckets
    remaining, and we are still rehashing those empty buckets. If
    table 0 is empty, we can simply finish the rehashing process,
    since we no longer have any elements to rehash.
    
    Signed-off-by: Binbin <binloveplay1314@qq.com>

--- src/hashtable.c
+++ src/hashtable.c
@@ -562,7 +562,9 @@ static void rehashStepFinalize(hashtable *ht) {
 
     /* Advance to the next bucket. */
     ht->rehash_idx++;
-    if ((size_t)ht->rehash_idx >= numBuckets(ht->bucket_exp[0])) {
+
+    /* Check if we already rehashed the whole table. */
+    if ((size_t)ht->rehash_idx >= numBuckets(ht->bucket_exp[0]) || ht->used[0] == 0) {
         rehashingCompleted(ht);
     }
 }

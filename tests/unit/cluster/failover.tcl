c226b8adc44ef04b67e56b30ec4dde2160a3cff7
Hanxi Zhang <hanxizh@amazon.com>
2026-02-04 17:01:23 -0800

    Update failover.tcl
    Signed-off-by: Hanxi Zhang <hanxizh@amazon.com>

--- tests/unit/cluster/failover.tcl
+++ tests/unit/cluster/failover.tcl
@@ -121,7 +121,45 @@ start_cluster 3 6 {tags {external:skip cluster}} {
     }
 
     test "Make sure the replicas always get the different ranks" {
-        if {[s -3 role] == "master"} {
+        # Get logs first
+        set log3 [exec cat [srv -3 stdout]]
+        set log6 [exec cat [srv -6 stdout]]
+        
+        # Extract rank information
+        set srv3_has_rank0 [string match "*Start of election*rank #0*" $log3]
+        set srv3_has_rank1 [string match "*Start of election*rank #1*" $log3]
+        set srv6_has_rank0 [string match "*Start of election*rank #0*" $log6]
+        set srv6_has_rank1 [string match "*Start of election*rank #1*" $log6]
+        
+        # Check if test will pass
+        set role3 [s -3 role]
+        set test_will_pass 0
+        
+        if {$role3 == "master"} {
+            if {$srv3_has_rank0 && $srv6_has_rank1} {
+                set test_will_pass 1
+            }
+        } else {
+            if {$srv3_has_rank1 && $srv6_has_rank0} {
+                set test_will_pass 1
+            }
+        }
+        
+        # If test will fail, print debug info
+        if {!$test_will_pass} {
+            puts "========== TEST FAILING - DEBUG INFO =========="
+            puts "Replica -3 role: $role3"
+            puts "Replica -6 role: [s -6 role]"
+            puts ""
+            puts "Replica -3 has rank #0: $srv3_has_rank0"
+            puts "Replica -3 has rank #1: $srv3_has_rank1"
+            puts "Replica -6 has rank #0: $srv6_has_rank0"
+            puts "Replica -6 has rank #1: $srv6_has_rank1"
+            puts "========== END DEBUG INFO =========="
+        }
+        
+        # Original test logic
+        if {$role3 == "master"} {
             verify_log_message -3 "*Start of election*rank #0*" 0
             verify_log_message -6 "*Start of election*rank #1*" 0
         } else {

43fd174cdcfffcc16ea2e46b64af08b5971350c3
Viktor Söderqvist <viktor.soderqvist@est.tech>
2026-02-13 12:59:50 +0100

    clang-format
    Signed-off-by: Viktor Söderqvist <viktor.soderqvist@est.tech>

--- src/endianconv.h
+++ src/endianconv.h
@@ -26,8 +26,8 @@ static inline uint16_t bswap16(uint16_t x) {
 
 static inline uint32_t bswap32(uint32_t x) {
     return ((x & 0xFF000000U) >> 24) |
-           ((x & 0x00FF0000U) >> 8)  |
-           ((x & 0x0000FF00U) << 8)  |
+           ((x & 0x00FF0000U) >> 8) |
+           ((x & 0x0000FF00U) << 8) |
            ((x & 0x000000FFU) << 24);
 }
 
@@ -35,8 +35,8 @@ static inline uint64_t bswap64(uint64_t x) {
     return ((x & 0xFF00000000000000ULL) >> 56) |
            ((x & 0x00FF000000000000ULL) >> 40) |
            ((x & 0x0000FF0000000000ULL) >> 24) |
-           ((x & 0x000000FF00000000ULL) >> 8)  |
-           ((x & 0x00000000FF000000ULL) << 8)  |
+           ((x & 0x000000FF00000000ULL) >> 8) |
+           ((x & 0x00000000FF000000ULL) << 8) |
            ((x & 0x0000000000FF0000ULL) << 24) |
            ((x & 0x000000000000FF00ULL) << 40) |
            ((x & 0x00000000000000FFULL) << 56);

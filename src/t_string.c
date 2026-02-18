b91d6f79d8cbe14ebf9b34fb5ea7d511600e0b3d
Roshan Khatri <rvkhatri@amazon.com>
2026-02-18 22:24:19 +0000

    Update and pin github actions to full SHAs for supply chain security (#3185)
    Updates to latest versions for each of the github actions used.
    
    Pinning prevents an attack where the upstream action dependency is
    compromised and the "v4" tag for example gets edited to point to a
    malicious version.
    
    Also fixes typos in source code and test files.
    
    (cherry picked from commit 9cbe1045d56e150d3d9745217634c37cbfb6ab2b)
    Adapted for 7.2: skipped files not present in this branch.
    
    Signed-off-by: Rain Valentine <rsg000@gmail.com>
    Signed-off-by: Roshan Khatri <rvkhatri@amazon.com>

--- src/t_string.c
+++ src/t_string.c
@@ -42,7 +42,7 @@ static int checkStringLength(client *c, long long size, long long append) {
         return C_OK;
     /* 'uint64_t' cast is there just to prevent undefined behavior on overflow */
     long long total = (uint64_t)size + append;
-    /* Test configured max-bulk-len represending a limit of the biggest string object,
+    /* Test configured max-bulk-len representing a limit of the biggest string object,
      * and also test for overflow. */
     if (total > server.proto_max_bulk_len || total < size || total < append) {
         addReplyError(c,"string exceeds maximum allowed size (proto-max-bulk-len)");

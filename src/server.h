f7f1a8b4110aaec31eca2243f67685eba69132d9
curious-george-rk <r.ebu@gmail.com>
2026-02-23 15:13:17 -0800

    Replace long long with mstime_t for expire timestamps in t_hash.c
    Signed-off-by: curious-george-rk <r.ebu@gmail.com>

--- src/server.h
+++ src/server.h
@@ -3560,7 +3560,7 @@ char *hashTypeCurrentFromHashTable(hashTypeIterator *hi, int what, size_t *len);
 sds hashTypeCurrentObjectNewSds(hashTypeIterator *hi, int what);
 robj *hashTypeLookupWriteOrCreate(client *c, robj *key);
 robj *hashTypeGetValueObject(robj *o, sds field);
-int hashTypeSet(robj *o, sds field, sds value, long long expiry, int flags, bool *expired_overwritten);
+int hashTypeSet(robj *o, sds field, sds value, mstime_t expiry, int flags, bool *expired_overwritten);
 robj *hashTypeDup(robj *o);
 bool hashTypeHasVolatileFields(robj *o);
 int hashTypeUpdateAsStringRef(robj *o, sds field, const char *buf, size_t len);

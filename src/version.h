11b4037a4e5f5c339ebdd2c4ae50cfb0c7ec99ae
Roshan Khatri <rvkhatri@amazon.com>
2026-02-23 19:34:11 +0000

    Adds release notes and version.h update
    Signed-off-by: Roshan Khatri <rvkhatri@amazon.com>

--- src/version.h
+++ src/version.h
@@ -4,8 +4,8 @@
  * similar. */
 #define SERVER_NAME "valkey"
 #define SERVER_TITLE "Valkey"
-#define VALKEY_VERSION "9.0.2"
-#define VALKEY_VERSION_NUM 0x00090002
+#define VALKEY_VERSION "9.0.3"
+#define VALKEY_VERSION_NUM 0x00090003
 /* The release stage is used in order to provide release status information.
  * In unstable branch the status is always "dev".
  * During release process the status will be set to rc1,rc2...rcN.

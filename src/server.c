afb3b8bf8dc8e7c8ccf884dbad0f5d87ef11d4eb
Ran Shidlansik <ranshid@amazon.com>
2026-03-18 18:25:01 +0200

    remove duplicated lline (#3379)
    Probably added by mistake during some merge of #1566
    
    Signed-off-by: Ran Shidlansik <ranshid@amazon.com>

--- src/server.c
+++ src/server.c
@@ -2232,7 +2232,6 @@ void createSharedObjects(void) {
     for (j = 0; j < OBJ_SHARED_INTEGERS; j++) {
         shared.integers[j] = makeObjectShared(createObject(OBJ_STRING, (void *)(long)j));
         shared.integers[j]->encoding = OBJ_ENCODING_INT;
-        shared.integers[j]->encoding = OBJ_ENCODING_INT;
     }
     for (j = 0; j < OBJ_SHARED_BULKHDR_LEN; j++) {
         shared.mbulkhdr[j] = createSharedStringFromSds(sdscatprintf(sdsempty(), "*%d\r\n", j));

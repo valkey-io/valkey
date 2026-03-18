e2b4c52ae801a85daf1a40f85c0d75198c4b08b2
jjuleslasarte <jules.lasarte@gmail.com>
2026-03-18 14:31:42 -0700

    durability: add deferred task system for post-ack execution
    Add a task registry that defers side-effects (keyspace notifications,
    key invalidations, flush invalidations) until durability providers
    acknowledge the associated write offset.
    
    Each task type registers create/destroy/execute/onClientDestroy
    handlers. Tasks are created during command execution with a deferred
    offset, then moved to an official waiting list once the replication
    offset is known. When the consensus offset advances past a task's
    offset, the task is executed and freed.
    
    Key invalidation tasks track the originating client pointer and
    properly handle client disconnection before task execution.
    
    Signed-off-by: jjuleslasarte <jules.lasarte@gmail.com>
    
    Signed-off-by: Jules Lasarte <jules.lasarte@gmail.com>

---
+++ src/durable_task.c
@@ -0,0 +1,381 @@
+#include "durable_task.h"
+#include "durability_provider.h"
+#include "server.h"
+#include "zmalloc.h"
+#include <assert.h>
+#include <stdarg.h>
+
+/* Forward declarations from module.h to avoid pulling in full module internals
+ * which has header dependency issues when included before server.h */
+void moduleNotifyKeyspaceEvent(int type, const char *event, robj *key, int dbid);
+
+/*================================= Internal Data structures ======================== */
+
+/**
+ * Internal structure used to track replication offset and arguments needed in
+ * executing task when offset has been acked by required number of replicas.
+ */
+typedef struct taskWaitingAck {
+    int type; // Task type
+    int64_t offset;
+    void **argv;
+} taskWaitingAck;
+
+/**
+ * Internal structure used to define all handlers for a task type
+ */
+typedef struct taskWaitingAckType {
+    taskWaitingAck *(*createTask)(va_list);
+    void (*destroyTask)(void *);
+    void (*executeTask)(const taskWaitingAck *);
+    void (*onClientDestroy)(void *);
+} taskWaitingAckType;
+
+static taskWaitingAckType taskTypes[DURABLE_TASK_TYPE_MAX];
+
+/*================================= Keyspace Notify Task ===================== */
+
+/**
+ * Create the keyspace notify task.
+ */
+static taskWaitingAck *createKeyspaceNotifyTask(va_list ap) {
+    int argc = 4; // 4 arguments for notify function: type, event, key, dbid
+    taskWaitingAck *task = zcalloc(sizeof(taskWaitingAck));
+    task->argv = zmalloc(argc * sizeof(void *));
+    for (int i = 0; i < argc; i++) {
+        task->argv[i] = va_arg(ap, void *);
+    }
+
+    // Increase reference count to avoid the key from being deleted
+    robj *key = (robj *)task->argv[2];
+    if (key) {
+        incrRefCount(key);
+    }
+    return task;
+}
+
+/**
+ * Destroy the keyspace notify task.
+ */
+static void destroyKeyspaceNotifyTask(void *ptr) {
+    taskWaitingAck *task = (taskWaitingAck *)ptr;
+    if (task->argv[2]) {
+        robj *key = (robj *)task->argv[2];
+        decrRefCount(key);
+    }
+    zfree(task->argv);
+    zfree(task);
+}
+
+/**
+ * Execute the keyspace notify task.
+ */
+static void executeKeyspaceNotifyTask(const taskWaitingAck *task) {
+    static_assert(sizeof(long long) == sizeof(void *), "void* is not the same size as long long");
+    notifyKeyspaceEvent((int)(long long)task->argv[0],
+                        (char *)task->argv[1],
+                        (robj *)task->argv[2],
+                        (int)(long long)task->argv[3]);
+}
+
+/*================================= Key Invalidation Task ==================== */
+
+/**
+ * Create the key invalidation task.
+ */
+static taskWaitingAck *createKeyInvalidationTask(va_list ap) {
+    // A key invalidation task has 2 arguments:
+    // 1. client* which generated the modification on the key
+    // 2. serverObject* that is modified
+    int argc = 2;
+    taskWaitingAck *task = zcalloc(sizeof(taskWaitingAck));
+    task->argv = zmalloc(argc * sizeof(void *));
+    for (int i = 0; i < argc; i++) {
+        task->argv[i] = va_arg(ap, void *);
+    }
+
+    // Track the pending notification task in the referenced client
+    client *c = (client *)task->argv[0];
+    if (c != NULL) {
+        listAddNodeTail(c->clientDurabilityInfo.pending_notify_tasks, task);
+    }
+
+    // Increase reference count to avoid the key from being deleted
+    robj *key = (robj *)task->argv[1];
+    if (key) {
+        incrRefCount(key);
+    }
+    return task;
+}
+
+/**
+ * Destroy the key invalidation task.
+ */
+static void destroyKeyInvalidationTask(void *ptr) {
+    taskWaitingAck *task = (taskWaitingAck *)ptr;
+    // Remove the current task from the list of pending tasks for the client.
+    // The tasks are tracked in FIFO order so we only need to look at the first one.
+    client *c = (client *)task->argv[0];
+    if (c != NULL) {
+        serverAssert(listLength(c->clientDurabilityInfo.pending_notify_tasks) > 0);
+        listNode *first = listFirst(c->clientDurabilityInfo.pending_notify_tasks);
+        serverAssert(task == (taskWaitingAck *)listNodeValue(first));
+        listDelNode(c->clientDurabilityInfo.pending_notify_tasks, first);
+    }
+
+    // Decrement the refcount for the key
+    if (task->argv[1]) {
+        robj *key = (robj *)task->argv[1];
+        decrRefCount(key);
+    }
+    zfree(task->argv);
+    zfree(task);
+}
+
+/**
+ * De-reference the client argument from the key invalidation task
+ */
+static void destroyClientForKeyInvalidationTask(void *task_ptr) {
+    taskWaitingAck *task = (taskWaitingAck *)task_ptr;
+    // The first argument is the client pointer
+    task->argv[0] = NULL;
+}
+
+/**
+ * Execute the key invalidation task.
+ */
+static void executeKeyInvalidationTask(const taskWaitingAck *task) {
+    trackingInvalidateKey((client *)task->argv[0], (robj *)task->argv[1], 1);
+}
+
+/*================================= Flush Invalidation Task ================== */
+
+/**
+ * Create the flush invalidation task.
+ */
+static taskWaitingAck *createFlushInvalidationTask(va_list ap) {
+    // Flush invalidation task has database ID as argument
+    int argc = 1;
+    taskWaitingAck *task = zcalloc(sizeof(taskWaitingAck));
+    task->argv = zmalloc(argc * sizeof(void *));
+    for (int i = 0; i < argc; i++) {
+        task->argv[i] = va_arg(ap, void *);
+    }
+    return task;
+}
+
+/**
+ * Destroy the flush invalidation task.
+ */
+static void destroyFlushInvalidationTask(void *ptr) {
+    taskWaitingAck *task = (taskWaitingAck *)ptr;
+    zfree(task->argv);
+    zfree(task);
+}
+
+/**
+ * Execute the flush invalidation task.
+ */
+static void executeFlushInvalidationTask(const taskWaitingAck *task) {
+    bool is_flush_all = (bool)task->argv[0];
+    // Use DBID -1 for FLUSHALL, otherwise use 0 for DBID
+    // Note: This assumes the OSS Redis code below doesn't operate on the actual
+    // DBID besides differentiating between FLUSHDB and FLUSHALL.
+    trackingInvalidateKeysOnFlush(is_flush_all ? -1 : 0);
+}
+
+/*================================= Default callback ========================= */
+
+/**
+ * Default callback on client destroy doing no-op
+ */
+static void destroyClientDefaultCallback(void *task) {
+    UNUSED(task);
+    return;
+}
+
+/*================================= Task Type Registry ======================= */
+
+void initTaskTypes(void) {
+    taskTypes[DURABLE_KEYSPACE_NOTIFY_TASK] = (taskWaitingAckType){
+        createKeyspaceNotifyTask,
+        destroyKeyspaceNotifyTask,
+        executeKeyspaceNotifyTask,
+        destroyClientDefaultCallback};
+    taskTypes[DURABLE_KEY_INVALIDATION_TASK] = (taskWaitingAckType){
+        createKeyInvalidationTask,
+        destroyKeyInvalidationTask,
+        executeKeyInvalidationTask,
+        destroyClientForKeyInvalidationTask};
+    // needed
+    taskTypes[DURABLE_FLUSH_INVALIDATION_TASK] = (taskWaitingAckType){
+        createFlushInvalidationTask,
+        destroyFlushInvalidationTask,
+        executeFlushInvalidationTask,
+        destroyClientDefaultCallback};
+}
+
+/*================================= Task Registration ======================== */
+
+/**
+ * Create task based on the given task type and arguments, and append the new
+ * task to the end of the linkedlist of the pending tasks of that task type.
+ *
+ * Note that at this point in time, we might not know about the replication
+ * offset we want to configure this task with so we put it onto a pending list.
+ * And at a later point in time, when we know the replication offset, we would
+ * set it and move the task to the official tasks list.
+ */
+bool durabilityRegisterDeferredTask(int type, ...) {
+    /* Check durability is active and the type is valid */
+    if (!isPrimaryDurabilityEnabled() || (type == DURABLE_TASK_TYPE_MAX)) {
+        return false;
+    }
+
+    va_list ap;
+    bool return_code = false;
+    va_start(ap, type);
+    taskWaitingAck *task = taskTypes[type].createTask(ap);
+    if (task) {
+        task->type = type;
+        if (server.current_client != NULL) {
+            // Here the notification is triggered by an incoming client request when we
+            // don't yet know the actual replication offset after command is applied,
+            // so we need to put it onto a pending tasks list.
+            listAddNodeTail(server.durability.pending_tasks_waiting_ack[type], task);
+        } else {
+            /* This notification is triggered from a background job such as
+             * active expiry or eviction outside of a regular client command.
+             * The replication offset is already updated so we use it directly. */
+            task->offset = server.primary_repl_offset;
+            listAddNodeTail(server.durability.tasks_waiting_ack[type], task);
+        }
+        return_code = true;
+    }
+    va_end(ap);
+    return return_code;
+}
+
+/*================================= Signal Handlers ========================== */
+
+bool durabilitySignalModifiedKey(struct client *c, struct serverDb *db, struct serverObject *key) {
+    UNUSED(db);
+    /* Defer key invalidation messages until the durability providers acknowledge. */
+    return durabilityRegisterDeferredTask(DURABLE_KEY_INVALIDATION_TASK,
+                                          (void *)c, (void *)key);
+}
+
+
+bool durabilitySignalFlushedDb(int dbid) {
+    /* Defer flush invalidation messages until the durability providers acknowledge. */
+    return durabilityRegisterDeferredTask(DURABLE_FLUSH_INVALIDATION_TASK,
+                                          (void *)(long long)(dbid == -1));
+}
+
+/*================================= Task Execution =========================== */
+
+/**
+ * Find and execute deferred tasks when 'consensus_ack_offset' is acked.
+ */
+void executeDeferredTasksForAck(const long long consensus_ack_offset) {
+    listIter li;
+    listNode *ln;
+    struct durable_t *durability = &server.durability;
+
+    for (int i = 0; i < DURABLE_TASK_TYPE_MAX; i++) {
+        listRewind(durability->tasks_waiting_ack[i], &li);
+        while ((ln = listNext(&li))) {
+            taskWaitingAck *task = listNodeValue(ln);
+            if (task->offset <= consensus_ack_offset) {
+                taskTypes[i].executeTask(task);
+                listDelNode(durability->tasks_waiting_ack[i], ln);
+            } else {
+                break;
+            }
+        }
+    }
+}
+
+/**
+ * Move pending deferred tasks to the official list with the current replication offset.
+ */
+void certifyPendingDeferredTasks(void) {
+    listIter li;
+    listNode *ln;
+    for (int i = 0; i < DURABLE_TASK_TYPE_MAX; i++) {
+        listRewind(server.durability.pending_tasks_waiting_ack[i], &li);
+        while ((ln = listNext(&li))) {
+            taskWaitingAck *task = listNodeValue(ln);
+            serverAssert(task->offset == 0);
+            task->offset = server.primary_repl_offset;
+            if (task->type == DURABLE_KEYSPACE_NOTIFY_TASK) {
+                moduleNotifyKeyspaceEvent(
+                    /*type*/ (intptr_t)task->argv[0],
+                    /*event*/ (char *)task->argv[1],
+                    /*key*/ (robj *)task->argv[2],
+                    /*dbid*/ (intptr_t)task->argv[3]);
+            }
+        }
+        if (listLength(server.durability.pending_tasks_waiting_ack[i]) > 0) {
+            listJoin(server.durability.tasks_waiting_ack[i], server.durability.pending_tasks_waiting_ack[i]);
+        }
+        serverAssert(listLength(server.durability.pending_tasks_waiting_ack[i]) == 0);
+    }
+}
+
+/*================================= Client Lifecycle ========================= */
+
+/**
+ * Notify the task system that a client is being destroyed so that
+ * any tasks referencing it can de-reference the client pointer.
+ */
+void durableTaskNotifyClientDestroy(struct list *pending_notify_tasks) {
+    listIter li;
+    listNode *ln;
+    listRewind(pending_notify_tasks, &li);
+    while ((ln = listNext(&li))) {
+        taskWaitingAck *task = (taskWaitingAck *)listNodeValue(ln);
+        if (task) {
+            taskTypes[task->type].onClientDestroy(task);
+        }
+    }
+}
+
+/*================================= Init / Cleanup =========================== */
+
+/**
+ * Initialize the task lists in the durability structure.
+ * Called from durabilityInit().
+ */
+void durableTaskInitLists(void) {
+    for (int i = 0; i < DURABLE_TASK_TYPE_MAX; i++) {
+        server.durability.tasks_waiting_ack[i] = listCreate();
+        server.durability.pending_tasks_waiting_ack[i] = listCreate();
+        listSetFreeMethod(server.durability.tasks_waiting_ack[i],
+                          taskTypes[i].destroyTask);
+        listSetFreeMethod(server.durability.pending_tasks_waiting_ack[i],
+                          taskTypes[i].destroyTask);
+    }
+}
+
+/**
+ * Release (free) all task lists. Called from durabilityCleanup().
+ */
+void durableTaskCleanupLists(void) {
+    for (int i = 0; i < DURABLE_TASK_TYPE_MAX; i++) {
+        listRelease(server.durability.tasks_waiting_ack[i]);
+        server.durability.tasks_waiting_ack[i] = NULL;
+        listRelease(server.durability.pending_tasks_waiting_ack[i]);
+        server.durability.pending_tasks_waiting_ack[i] = NULL;
+    }
+}
+
+/**
+ * Empty (but don't free) all task lists. Called during primary state reset.
+ */
+void durableTaskEmptyLists(void) {
+    for (int i = 0; i < DURABLE_TASK_TYPE_MAX; i++) {
+        listEmpty(server.durability.tasks_waiting_ack[i]);
+        listEmpty(server.durability.pending_tasks_waiting_ack[i]);
+    }
+}

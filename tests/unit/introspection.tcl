cd0349dcc342bd43968c97313fcf84f886070249
Binbin <binloveplay1314@qq.com>
2026-04-08 16:26:29 +0800

    Fix slot-migration-max-failover-repl-bytes unable to accept -1 (#3443)
    In valkey.conf, slot-migration-max-failover-repl-bytes allows setting
    to -1 to disable the limit.
    ```
    Setting this to -1 will disable this limit
    ```
    
    But slot-migration-max-failover-repl-bytes is defined as MEMORY_CONFIG
    and memtoull() rejects negative inputs, making it impossible to set the
    value to -1 via config file or CONFIG SET.
    ```
    >>> 'slot-migration-max-failover-repl-bytes "-1"'
    argument must be a memory value
    ```
    
    Introduce SIGNED_MEMORY_CONFIG flag for memory configs that also accept
    plain negative number. When memtoull() fails and this flag is set, fall back to string2ll()
    for parsing. Use ll2string() for CONFIG GET and rewriteConfigNumericalOption() for
    CONFIG REWRITE when the value is negative.
    
    Add a serverAssert in initConfigValues() to enforce that PERCENT_CONFIG
    and SIGNED_MEMORY_CONFIG are never combined on the same config, since
    both use negative values with different semantics.
    
    This means we have had this issue since it was introduced in #1949.
    
    Signed-off-by: Binbin <binloveplay1314@qq.com>
    Co-authored-by: Viktor Söderqvist <viktor.soderqvist@est.tech>

--- tests/unit/introspection.tcl
+++ tests/unit/introspection.tcl
@@ -1956,3 +1956,15 @@ test {CONFIG REWRITE handles alias config properly} {
         assert_equal [r config get hash-max-listpack-entries] {hash-max-listpack-entries 100}
     }
 } {} {external:skip}
+
+test {SIGNED MEMORY CONFIG allows negative number} {
+    start_server {tags {"introspection"}} {
+        r config set slot-migration-max-failover-repl-bytes -1
+        assert_equal [lindex [r config get slot-migration-max-failover-repl-bytes] 1] -1
+        assert_error {*argument must be between -1 and *} {r config set slot-migration-max-failover-repl-bytes -2}
+
+        r config rewrite
+        restart_server 0 true false
+        assert_equal [lindex [r config get slot-migration-max-failover-repl-bytes] 1] -1
+    }
+} {} {external:skip}

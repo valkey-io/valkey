172b89f6f57be9377018f492e8c17fa1a3dcc675
liveprasad <prasad.shetkar093@gmail.com>
2026-04-14 10:33:28 +0530

    listpack: fix clang-format issues in test_listpack.cpp
    Signed-off-by: liveprasad <prasad.shetkar093@gmail.com>

--- src/unit/test_listpack.cpp
+++ src/unit/test_listpack.cpp
@@ -27,7 +27,7 @@ unsigned char *lpSkip(unsigned char *p);
 /* Macros from listpack.c needed for testing */
 #define LP_HDR_SIZE 6u /* 32 bit total len + 16 bit number of elements. */
 #define LP_EOF 0xFF
-#define LP_HDR_NUMELE_UNKNOWN (uint32_t) UINT16_MAX
+#define LP_HDR_NUMELE_UNKNOWN (uint32_t)UINT16_MAX
 #define LP_HDR_NUMELE_UNKNOWN_UL (unsigned long)UINT16_MAX
 #define LP_ENCODING_7BIT_UINT_MASK 0x80
 #define LP_ENCODING_IS_7BIT_UINT(byte) (((byte) & LP_ENCODING_7BIT_UINT_MASK) == 0)
@@ -958,7 +958,7 @@ TEST_F(ListpackTest, listpackLpFindInteger) {
     /* String entry "hello" should not match any integer search */
     ASSERT_EQ(lpFindInteger(lp, lpFirst(lp), 0, 0), nullptr);
 
-     /* String "42" and integer 42 should find the same listpack entry */
+    /* String "42" and integer 42 should find the same listpack entry */
     unsigned char *ptr_by_int = lpFindInteger(lp, lpFirst(lp), 42, 0);
     unsigned char *ptr_by_str = lpFind(lp, lpFirst(lp), (unsigned char *)"42", 2, 0);
     ASSERT_EQ(ptr_by_int, ptr_by_str);

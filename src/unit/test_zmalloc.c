42637f10a6dda45d9a6beea38fb53aa253dd2038
Sarthak Aggarwal <25262500+sarthakaggarwal97@users.noreply.github.com>
2026-04-17 13:29:15 -0700

    tests: make zmalloc unit checks baseline-safe
    (cherry picked from commit c3919a07de43f9b009deb1ba506e17ff5e62c88d)

--- src/unit/test_zmalloc.c
+++ src/unit/test_zmalloc.c
@@ -6,8 +6,7 @@ int test_zmallocInitialUsedMemory(int argc, char **argv, int flags) {
     UNUSED(argv);
     UNUSED(flags);
 
-    TEST_ASSERT(zmalloc_used_memory() == 0);
-
+    TEST_PRINT_INFO("Initial used memory: %zu\n", zmalloc_used_memory());
     return 0;
 }
 
@@ -15,23 +14,27 @@ int test_zmallocAllocReallocCallocAndFree(int argc, char **argv, int flags) {
     UNUSED(argc);
     UNUSED(argv);
     UNUSED(flags);
-
+    size_t used_memory_before = zmalloc_used_memory();
     void *ptr, *ptr2;
 
     ptr = zmalloc(123);
-    TEST_PRINT_INFO("Allocated 123 bytes; used: %zu\n", zmalloc_used_memory());
+    TEST_PRINT_INFO("Allocated 123 bytes; used: %lld\n",
+                    (long long)zmalloc_used_memory() - used_memory_before);
 
     ptr = zrealloc(ptr, 456);
-    TEST_PRINT_INFO("Reallocated to 456 bytes; used: %zu\n", zmalloc_used_memory());
+    TEST_PRINT_INFO("Reallocated to 456 bytes; used: %lld\n",
+                    (long long)zmalloc_used_memory() - used_memory_before);
 
     ptr2 = zcalloc(123);
-    TEST_PRINT_INFO("Callocated 123 bytes; used: %zu\n", zmalloc_used_memory());
+    TEST_PRINT_INFO("Callocated 123 bytes; used: %lld\n",
+                    (long long)zmalloc_used_memory() - used_memory_before);
 
     zfree(ptr);
     zfree(ptr2);
-    TEST_PRINT_INFO("Freed pointers; used: %zu\n", zmalloc_used_memory());
+    TEST_PRINT_INFO("Freed pointers; used: %lld\n",
+                    (long long)zmalloc_used_memory() - used_memory_before);
 
-    TEST_ASSERT(zmalloc_used_memory() == 0);
+    TEST_ASSERT(zmalloc_used_memory() == used_memory_before);
 
     return 0;
 }
@@ -40,14 +43,14 @@ int test_zmallocAllocZeroByteAndFree(int argc, char **argv, int flags) {
     UNUSED(argc);
     UNUSED(argv);
     UNUSED(flags);
-
+    size_t used_memory_before = zmalloc_used_memory();
     void *ptr;
 
     ptr = zmalloc(0);
     TEST_PRINT_INFO("Allocated 0 bytes; used: %zu\n", zmalloc_used_memory());
     zfree(ptr);
 
-    TEST_ASSERT(zmalloc_used_memory() == 0);
+    TEST_ASSERT(zmalloc_used_memory() == used_memory_before);
 
     return 0;
 }

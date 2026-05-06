087490028f8e5a13336698fe079eecee80c6ca2a
Madelyn Olson <madelyneolson@gmail.com>
2026-05-05 17:04:51 -0700

    Bump version to 7.2.13
    Signed-off-by: Madelyn Olson <madelyneolson@gmail.com>

--- src/version.h
+++ src/version.h
@@ -1,6 +1,6 @@
 #define SERVER_NAME "valkey"
-#define VALKEY_VERSION "7.2.12"
-#define VALKEY_VERSION_NUM 0x0007020C
+#define VALKEY_VERSION "7.2.13"
+#define VALKEY_VERSION_NUM 0x0007020D
 
 /* Redis compatibility version, should never
  * exceed 7.2.x. */

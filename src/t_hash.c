6c5e9317de51664ca57f7e942d4d823387c40801
Binbin <binloveplay1314@qq.com>
2026-05-19 01:54:16 +0800

    Fix buffered_reply assert in HFE commands with module keyspace notifications (#3743)
    When a module subscribes to NOTIFY_HASH keyspace events and blocks the
    client in the notification callback, hexpireGenericCommand,
    hgetdelCommand,
    and hpersistCommand would trigger debugServerAssert in
    notifyKeyspaceEvent()
    because addReplyArrayLen() sets buffered_reply=1 before the notification
    fires. This debug assert was added in #1819.
    
    Affected commands: HEXPIRE, HPEXPIRE, HEXPIREAT, HPEXPIREAT, HGETDEL,
    HPERSIST.
    
    ---------
    
    Signed-off-by: Binbin <binloveplay1314@qq.com>

--- src/t_hash.c
+++ src/t_hash.c
@@ -1175,6 +1175,8 @@ void hgetdelCommand(client *c) {
     bool hash_volatile_items = hashTypeHasVolatileFields(o);
     if (o && o->encoding == OBJ_ENCODING_HASHTABLE) hashtablePauseAutoShrink(objectGetVal(o));
 
+    initDeferredReplyBuffer(c);
+
     /* Reply with array of values and delete at the same time */
     addReplyArrayLen(c, num_fields);
     for (i = fields_index; i < c->argc; i++) {
@@ -1203,6 +1205,8 @@ void hgetdelCommand(client *c) {
         if (keyremoved) notifyKeyspaceEvent(NOTIFY_GENERIC, "del", c->argv[1], c->db->id);
         server.dirty += deleted;
     }
+
+    commitDeferredReplyBuffer(c, 1);
 }
 
 void hlenCommand(client *c) {
@@ -1915,6 +1919,8 @@ void hexpireGenericCommand(client *c, mstime_t basetime, int unit) {
 
     bool has_volatile_fields = hashTypeHasVolatileFields(obj);
 
+    initDeferredReplyBuffer(c);
+
     /* From this point we would return array reply */
     addReplyArrayLen(c, num_fields);
 
@@ -1972,6 +1978,8 @@ void hexpireGenericCommand(client *c, mstime_t basetime, int unit) {
             notifyKeyspaceEvent(NOTIFY_GENERIC, "del", c->argv[1], c->db->id);
         }
     }
+
+    commitDeferredReplyBuffer(c, 1);
 }
 
 void hexpireCommand(client *c) {
@@ -2023,6 +2031,8 @@ void hpersistCommand(client *c) {
     if (checkType(c, hash, OBJ_HASH))
         return;
 
+    initDeferredReplyBuffer(c);
+
     /* From this point we would return array reply */
     addReplyArrayLen(c, num_fields);
 
@@ -2043,6 +2053,8 @@ void hpersistCommand(client *c) {
         notifyKeyspaceEvent(NOTIFY_HASH, "hpersist", c->argv[1], c->db->id);
         signalModifiedKey(c, c->db, c->argv[1]);
     }
+
+    commitDeferredReplyBuffer(c, 1);
 }
 
 /* High-Level Algorithm of HTTL / HPTTL / HEXPIRETIME / HPEXPIRETIME Commands:

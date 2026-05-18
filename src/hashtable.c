e2c82a8748e195bbac17b28cb8048f81d6198eca
Jim Brunner <brunnerj@amazon.com>
2026-05-18 21:55:30 +0000

    Forkless Save
    Signed-off-by: Jim Brunner <brunnerj@amazon.com>

--- src/hashtable.c
+++ src/hashtable.c
@@ -2246,6 +2246,7 @@ bool hashtableNext(hashtableIterator *iterator, void **elemptr) {
     /* Check if iterator has been invalidated */
     if (iter->hashtable == NULL) return false;
 
+    // clang format off
     while (1) {
         if (iter->index == -1 && iter->table == 0) {
             /* It's the first call to next. */
@@ -2271,13 +2272,11 @@ bool hashtableNext(hashtableIterator *iterator, void **elemptr) {
              * child bucket in a chain, or to the next bucket index, or to the
              * next table. */
             iter->pos_in_bucket++;
-            // clang format off
             if (iter->bucket->chained
                     && iter->pos_in_bucket >= ENTRIES_PER_BUCKET - 1
                     && iter->pos_in_bucket != ITERATOR_DONE_WITH_BUCKET_IDX + 1) {
                 iter->pos_in_bucket = 0;
                 iter->bucket = getChildBucket(iter->bucket);
-            // clang format on
             } else if (iter->pos_in_bucket >= ENTRIES_PER_BUCKET) {
                 /* Bucket index done. */
                 if (isSafe(iter)) {
@@ -2327,6 +2326,7 @@ bool hashtableNext(hashtableIterator *iterator, void **elemptr) {
         }
         return true;
     }
+    // clang format on
     return false;
 }
 

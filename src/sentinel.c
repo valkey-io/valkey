hanzoai/kv

Harden SENTINEL commands and config rewrite against control-character injection (#3847)
Reject any SENTINEL SET argument containing control characters
(0x00-0x1F, 0x7F) to prevent config injection via newline-embedded
values.

---------

Signed-off-by: Eran Ifrah <eifrah@amazon.com>

src/sentinel.c
@@ -191,7 +191,7 @@ typedef struct instanceLink {
 typedef struct sentinelValkeyInstance {
     int flags;                                 /* See SRI_... defines */
     char *name;                                /* Primary name from the point of view of this sentinel. */
-    char *runid;                               /* Run ID of this instance, or unique ID if is a Sentinel.*/
+    sds runid;                                 /* Run ID of this instance, or unique ID if is a Sentinel.*/
     uint64_t config_epoch;                     /* Configuration epoch. */
     sentinelAddr *addr;                        /* Primary host. */
     instanceLink *link;                        /* Link to the instance, may be shared for Sentinels. */
@@ -229,8 +229,8 @@ typedef struct sentinelValkeyInstance {
     dict *replicas;      /* Replicas for this primary instance. */
     unsigned int quorum; /* Number of sentinels that need to agree on failure. */
     int parallel_syncs;  /* How many replicas to reconfigure at same time. */
-    char *auth_pass;     /* Password to use for AUTH against primary & replica. */
-    char *auth_user;     /* Username for ACLs AUTH against primary & replica. */
+    sds auth_pass;       /* Password to use for AUTH against primary & replica. */
+    sds auth_user;       /* Username for ACLs AUTH against primary & replica. */
 
     /* Replica specific. */
     mstime_t primary_link_down_time;        /* Replica replication link down time. */
@@ -258,8 +258,8 @@ typedef struct sentinelValkeyInstance {
     struct sentinelValkeyInstance *promoted_replica; /* Promoted replica instance. */
     /* Scripts executed to notify admin or reconfigure clients: when they
      * are set to NULL no script is executed. */
-    char *notification_script;
-    char *client_reconfig_script;
+    sds notification_script;
+    sds client_reconfig_script;
     sds info; /* cached INFO output */
 } sentinelValkeyInstance;
 
@@ -283,8 +283,8 @@ struct sentinelState {
     unsigned long simfailure_flags;    /* Failures simulation. */
     int deny_scripts_reconfig;         /* Allow SENTINEL SET ... to change script
                                           paths at runtime? */
-    char *sentinel_auth_pass;          /* Password to use for AUTH against other sentinel */
-    char *sentinel_auth_user;          /* Username for ACLs AUTH against other sentinel. */
+    sds sentinel_auth_pass;            /* Password to use for AUTH against other sentinel */
+    sds sentinel_auth_user;            /* Username for ACLs AUTH against other sentinel. */
     int resolve_hostnames;             /* Support use of hostnames, assuming DNS is well configured. */
     int announce_hostnames;            /* Announce hostnames instead of IPs when we have them. */
 } sentinel;
@@ -2056,29 +2056,31 @@ void rewriteConfigSentinelOption(struct rewriteConfigState *state) {
 
         /* sentinel notification-script */
         if (primary->notification_script) {
-            line = sdscatprintf(sdsempty(), "sentinel notification-script %s %s", primary->name,
-                                primary->notification_script);
+            line = sdscatprintf(sdsempty(), "sentinel notification-script %s ", primary->name);
+            line = sdscatrepr(line, primary->notification_script, sdslen(primary->notification_script));
             rewriteConfigRewriteLine(state, "sentinel notification-script", line, 1);
             /* rewriteConfigMarkAsProcessed is handled after the loop */
         }
 
         /* sentinel client-reconfig-script */
         if (primary->client_reconfig_script) {
-            line = sdscatprintf(sdsempty(), "sentinel client-reconfig-script %s %s", primary->name,
-                                primary->client_reconfig_script);
+            line = sdscatprintf(sdsempty(), "sentinel client-reconfig-script %s ", primary->name);
+            line = sdscatrepr(line, primary->client_reconfig_script, sdslen(primary->client_reconfig_script));
             rewriteConfigRewriteLine(state, "sentinel client-reconfig-script", line, 1);
             /* rewriteConfigMarkAsProcessed is handled after the loop */
         }
 
         /* sentinel auth-pass & auth-user */
         if (primary->auth_pass) {
-            line = sdscatprintf(sdsempty(), "sentinel auth-pass %s %s", primary->name, primary->auth_pass);
+            line = sdscatprintf(sdsempty(), "sentinel auth-pass %s ", primary->name);
+            line = sdscatrepr(line, primary->auth_pass, sdslen(primary->auth_pass));
             rewriteConfigRewriteLine(state, "sentinel auth-pass", line, 1);
             /* rewriteConfigMarkAsProcessed is handled after the loop */
         }
 
         if (primary->auth_user) {
-            line = sdscatprintf(sdsempty(), "sentinel auth-user %s %s", primary->name, primary->auth_user);
+            line = sdscatprintf(sdsempty(), "sentinel auth-user %s ", primary->name);
+            line = sdscatrepr(line, primary->auth_user, sdslen(primary->auth_user));
             rewriteConfigRewriteLine(state, "sentinel auth-user", line, 1);
             /* rewriteConfigMarkAsProcessed is handled after the loop */
         }
@@ -2135,8 +2137,9 @@ void rewriteConfigSentinelOption(struct rewriteConfigState *state) {
         while ((de = dictNext(di2)) != NULL) {
             ri = dictGetVal(de);
             if (ri->runid == NULL) continue;
-            line = sdscatprintf(sdsempty(), "sentinel known-sentinel %s %s %d %s", primary->name,
-                                announceSentinelAddr(ri->addr), ri->addr->port, ri->runid);
+            line = sdscatprintf(sdsempty(), "sentinel known-sentinel %s %s %d ", primary->name,
+                                announceSentinelAddr(ri->addr), ri->addr->port);
+            line = sdscatrepr(line, ri->runid, sdslen(ri->runid));
             rewriteConfigRewriteLine(state, "sentinel known-sentinel", line, 1);
             /* rewriteConfigMarkAsProcessed is handled after the loop */
         }
@@ -2147,7 +2150,10 @@ void rewriteConfigSentinelOption(struct rewriteConfigState *state) {
         while ((de = dictNext(di2)) != NULL) {
             sds oldname = dictGetKey(de);
             sds newname = dictGetVal(de);
-            line = sdscatprintf(sdsempty(), "sentinel rename-command %s %s %s", primary->name, oldname, newname);
+            line = sdscatprintf(sdsempty(), "sentinel rename-command %s ", primary->name);
+            line = sdscatrepr(line, oldname, sdslen(oldname));
+            line = sdscatlen(line, " ", 1);
+            line = sdscatrepr(line, newname, sdslen(newname));
             rewriteConfigRewriteLine(state, "sentinel rename-command", line, 1);
             /* rewriteConfigMarkAsProcessed is handled after the loop */
         }
@@ -2177,7 +2183,8 @@ void rewriteConfigSentinelOption(struct rewriteConfigState *state) {
 
     /* sentinel sentinel-user. */
     if (sentinel.sentinel_auth_user) {
-        line = sdscatprintf(sdsempty(), "sentinel sentinel-user %s", sentinel.sentinel_auth_user);
+        line = sdsnew("sentinel sentinel-user ");
+        line = sdscatrepr(line, sentinel.sentinel_auth_user, sdslen(sentinel.sentinel_auth_user));
         rewriteConfigRewriteLine(state, "sentinel sentinel-user", line, 1);
     } else {
         rewriteConfigMarkAsProcessed(state, "sentinel sentinel-user");
@@ -2185,7 +2192,8 @@ void rewriteConfigSentinelOption(struct rewriteConfigState *state) {
 
     /* sentinel sentinel-pass. */
     if (sentinel.sentinel_auth_pass) {
-        line = sdscatprintf(sdsempty(), "sentinel sentinel-pass %s", sentinel.sentinel_auth_pass);
+        line = sdsnew("sentinel sentinel-pass ");
+        line = sdscatrepr(line, sentinel.sentinel_auth_pass, sdslen(sentinel.sentinel_auth_pass));
         rewriteConfigRewriteLine(state, "sentinel sentinel-pass", line, 1);
     } else {
         rewriteConfigMarkAsProcessed(state, "sentinel sentinel-pass");
@@ -3083,6 +3091,27 @@ const char *getLogLevel(void) {
     return "unknown";
 }
 
+static int containsControlChars(const char *s, size_t len) {
+    for (size_t i = 0; i < len; i++) {
+        unsigned char ch = (unsigned char)s[i];
+        if (ch <= 0x1F || ch == 0x7F) return 1;
+    }
+    return 0;
+}
+
+/* Validate that arguments starting from 'first_arg' contain no control characters.
+ * Returns C_OK if all arguments are clean, or C_ERR after sending an error reply. */
+static int sentinelValidateArgs(client *c, int first_arg, const char *cmd) {
+    for (int i = first_arg; i < c->argc; i++) {
+        sds arg = objectGetVal(c->argv[i]);
+        if (containsControlChars(arg, sdslen(arg))) {
+            addReplyErrorFormat(c, "Invalid argument at index %d for %s: control characters are not allowed", i, cmd);
+            return C_ERR;
+        }
+    }
+    return C_OK;
+}
+
 /* SENTINEL CONFIG SET option value [option value ...] */
 void sentinelConfigSetCommand(client *c) {
     long long numval;
@@ -3105,6 +3134,8 @@ void sentinelConfigSetCommand(client *c) {
         populateDict(options_dict, options);
     }
     dict *set_configs = dictCreate(&stringSetDictType);
+
+    if (sentinelValidateArgs(c, 3, "SENTINEL CONFIG SET") == C_ERR) goto exit;
 
     /* Validate arguments are valid */
     for (int i = 3; i < c->argc; i++) {
@@ -3942,6 +3973,9 @@ void sentinelCommand(client *c) {
         char ip[NET_IP_STR_LEN];
 
         if (c->argc != 6) goto numargserr;
+
+        if (sentinelValidateArgs(c, 2, "SENTINEL MONITOR") == C_ERR) return;
+
         if (getLongFromObjectOrReply(c, c->argv[5], &quorum, "Invalid quorum") != C_OK) return;
         if (getLongFromObjectOrReply(c, c->argv[4], &port, "Invalid port") != C_OK) return;
 
@@ -4226,6 +4260,8 @@ void sentinelSetCommand(client *c) {
     int redacted;
 
     if ((ri = sentinelGetPrimaryByNameOrReplyError(c, c->argv[2])) == NULL) return;
+
+    if (sentinelValidateArgs(c, 3, "SENTINEL SET") == C_ERR) return;
 
     /* Process option - value pairs. */
     for (j = 3; j < c->argc; j++) {

tests/sentinel/tests/03-runtime-reconf.tcl
@@ -198,3 +198,72 @@ test "Sentinel Set with other error situations" {
 
    assert_match "ERR Failed to save config file*" $err
 }
+
+test "SENTINEL SET rejects values containing control characters" {
+    # CRLF injection into auth-pass
+    assert_error "ERR Invalid argument*" {S 0 SENTINEL SET mymaster auth-pass "pass\r\nsentinel notification-script mymaster /tmp/evil.sh"}
+    # Newline in auth-user
+    assert_error "ERR Invalid argument*" {S 0 SENTINEL SET mymaster auth-user "user\ninjected"}
+    # Carriage return alone
+    assert_error "ERR Invalid argument*" {S 0 SENTINEL SET mymaster auth-pass "has\rcr"}
+    # Null byte
+    assert_error "ERR Invalid argument*" {S 0 SENTINEL SET mymaster auth-user "has\x00null"}
+    # Tab character (0x09)
+    assert_error "ERR Invalid argument*" {S 0 SENTINEL SET mymaster auth-pass "has\ttab"}
+    # DEL character (0x7F)
+    assert_error "ERR Invalid argument*" {S 0 SENTINEL SET mymaster auth-pass "has\x7F"}
+    # Control char in option name is also rejected
+    assert_error "ERR Invalid argument*" {S 0 SENTINEL SET mymaster "bad\noption" value}
+}
+
+test "SENTINEL MONITOR rejects values containing control characters" {
+    # CRLF in name
+    assert_error "ERR Invalid argument*" {S 0 SENTINEL MONITOR "bad\r\nname" 127.0.0.1 6379 2}
+    # Newline in hostname
+    assert_error "ERR Invalid argument*" {S 0 SENTINEL MONITOR testprimary "127.0.0.1\ninjected" 6379 2}
+    # Null byte in name
+    assert_error "ERR Invalid argument*" {S 0 SENTINEL MONITOR "has\x00null" 127.0.0.1 6379 2}
+    # Tab in name
+    assert_error "ERR Invalid argument*" {S 0 SENTINEL MONITOR "has\ttab" 127.0.0.1 6379 2}
+    # DEL in hostname
+    assert_error "ERR Invalid argument*" {S 0 SENTINEL MONITOR testprimary "host\x7F" 6379 2}
+}
+
+test "SENTINEL CONFIG SET rejects values containing control characters" {
+    # CRLF in announce-ip
+    assert_error "ERR Invalid argument*" {S 0 SENTINEL CONFIG SET announce-ip "1.2.3.4\r\ninjected"}
+    # Newline in sentinel-user
+    assert_error "ERR Invalid argument*" {S 0 SENTINEL CONFIG SET sentinel-user "user\ninjected"}
+    # Null byte in sentinel-pass
+    assert_error "ERR Invalid argument*" {S 0 SENTINEL CONFIG SET sentinel-pass "pass\x00word"}
+    # Tab in announce-ip
+    assert_error "ERR Invalid argument*" {S 0 SENTINEL CONFIG SET announce-ip "1.2\t.3.4"}
+    # DEL in sentinel-pass
+    assert_error "ERR Invalid argument*" {S 0 SENTINEL CONFIG SET sentinel-pass "pass\x7F"}
+}
+
+test "Config rewrite escapes special characters via sdscatrepr" {
+    set special_pass "my\"pass\\word with spaces"
+    S 0 SENTINEL SET mymaster auth-pass $special_pass
+
+    S 0 SENTINEL FLUSHCONFIG
+
+    # Verify the config file has the value properly quoted
+    set configfile [file join "sentinel_0" "sentinel.conf"]
+    set content [exec cat $configfile]
+    set expected {sentinel auth-pass mymaster "my\"pass\\word with spaces"}
+    assert {[string first $expected $content] >= 0}
+
+    # Restart sentinel and verify it reloads without error
+    restart_instance sentinel 0
+    wait_for_condition 200 50 {
+        [catch {S 0 PING}] == 0
+    } else {
+        fail "Sentinel 0 did not come back after restart"
+    }
+
+    assert_match {*} [S 0 SENTINEL GET-PRIMARY-ADDR-BY-NAME mymaster]
+
+    S 0 SENTINEL SET mymaster auth-pass ""
+    S 0 SENTINEL FLUSHCONFIG
+}

valkey-io/valkey

src/sentinel.c
@@ -246,8 +246,8 @@ typedef struct sentinelRedisInstance {
     struct sentinelRedisInstance *promoted_slave; /* Promoted slave instance. */
     /* Scripts executed to notify admin or reconfigure clients: when they
      * are set to NULL no script is executed. */
-    char *notification_script;
-    char *client_reconfig_script;
+    sds notification_script;
+    sds client_reconfig_script;
     sds info; /* cached INFO output */
 } sentinelRedisInstance;
 
@@ -2101,8 +2101,8 @@ void rewriteConfigSentinelOption(struct rewriteConfigState *state) {
         /* sentinel notification-script */
         if (master->notification_script) {
             line = sdscatprintf(sdsempty(),
-                "sentinel notification-script %s %s",
-                master->name, master->notification_script);
+                "sentinel notification-script %s ", master->name);
+            line = sdscatrepr(line, master->notification_script, sdslen(master->notification_script));
             rewriteConfigRewriteLine(state,"sentinel notification-script",line,1);
             /* rewriteConfigMarkAsProcessed is handled after the loop */
         }
@@ -2110,8 +2110,8 @@ void rewriteConfigSentinelOption(struct rewriteConfigState *state) {
         /* sentinel client-reconfig-script */
         if (master->client_reconfig_script) {
             line = sdscatprintf(sdsempty(),
-                "sentinel client-reconfig-script %s %s",
-                master->name, master->client_reconfig_script);
+                "sentinel client-reconfig-script %s ", master->name);
+            line = sdscatrepr(line, master->client_reconfig_script, sdslen(master->client_reconfig_script));
             rewriteConfigRewriteLine(state,"sentinel client-reconfig-script",line,1);
             /* rewriteConfigMarkAsProcessed is handled after the loop */
         }
@@ -2119,16 +2119,16 @@ void rewriteConfigSentinelOption(struct rewriteConfigState *state) {
         /* sentinel auth-pass & auth-user */
         if (master->auth_pass) {
             line = sdscatprintf(sdsempty(),
-                "sentinel auth-pass %s %s",
-                master->name, master->auth_pass);
+                "sentinel auth-pass %s ", master->name);
+            line = sdscatrepr(line, master->auth_pass, sdslen(master->auth_pass));
             rewriteConfigRewriteLine(state,"sentinel auth-pass",line,1);
             /* rewriteConfigMarkAsProcessed is handled after the loop */
         }
 
         if (master->auth_user) {
             line = sdscatprintf(sdsempty(),
-                "sentinel auth-user %s %s",
-                master->name, master->auth_user);
+                "sentinel auth-user %s ", master->name);
+            line = sdscatrepr(line, master->auth_user, sdslen(master->auth_user));
             rewriteConfigRewriteLine(state,"sentinel auth-user",line,1);
             /* rewriteConfigMarkAsProcessed is handled after the loop */
         }
@@ -2191,8 +2191,9 @@ void rewriteConfigSentinelOption(struct rewriteConfigState *state) {
             ri = dictGetVal(de);
             if (ri->runid == NULL) continue;
             line = sdscatprintf(sdsempty(),
-                "sentinel known-sentinel %s %s %d %s",
-                master->name, announceSentinelAddr(ri->addr), ri->addr->port, ri->runid);
+                "sentinel known-sentinel %s %s %d ",
+                master->name, announceSentinelAddr(ri->addr), ri->addr->port);
+            line = sdscatrepr(line, ri->runid, sdslen(ri->runid));
             rewriteConfigRewriteLine(state,"sentinel known-sentinel",line,1);
             /* rewriteConfigMarkAsProcessed is handled after the loop */
         }
@@ -2204,8 +2205,11 @@ void rewriteConfigSentinelOption(struct rewriteConfigState *state) {
             sds oldname = dictGetKey(de);
             sds newname = dictGetVal(de);
             line = sdscatprintf(sdsempty(),
-                "sentinel rename-command %s %s %s",
-                master->name, oldname, newname);
+                "sentinel rename-command %s ",
+                master->name);
+            line = sdscatrepr(line, oldname, sdslen(oldname));
+            line = sdscatlen(line, " ", 1);
+            line = sdscatrepr(line, newname, sdslen(newname));
             rewriteConfigRewriteLine(state,"sentinel rename-command",line,1);
             /* rewriteConfigMarkAsProcessed is handled after the loop */
         }
@@ -2237,7 +2241,8 @@ void rewriteConfigSentinelOption(struct rewriteConfigState *state) {
 
     /* sentinel sentinel-user. */
     if (sentinel.sentinel_auth_user) {
-        line = sdscatprintf(sdsempty(), "sentinel sentinel-user %s", sentinel.sentinel_auth_user);
+        line = sdsnew("sentinel sentinel-user ");
+        line = sdscatrepr(line, sentinel.sentinel_auth_user, sdslen(sentinel.sentinel_auth_user));
         rewriteConfigRewriteLine(state,"sentinel sentinel-user",line,1);
     } else {
         rewriteConfigMarkAsProcessed(state,"sentinel sentinel-user");
@@ -2245,10 +2250,11 @@ void rewriteConfigSentinelOption(struct rewriteConfigState *state) {
 
     /* sentinel sentinel-pass. */
     if (sentinel.sentinel_auth_pass) {
-        line = sdscatprintf(sdsempty(), "sentinel sentinel-pass %s", sentinel.sentinel_auth_pass);
+        line = sdsnew("sentinel sentinel-pass ");
+        line = sdscatrepr(line, sentinel.sentinel_auth_pass, sdslen(sentinel.sentinel_auth_pass));
         rewriteConfigRewriteLine(state,"sentinel sentinel-pass",line,1);
     } else {
-        rewriteConfigMarkAsProcessed(state,"sentinel sentinel-pass");  
+        rewriteConfigMarkAsProcessed(state,"sentinel sentinel-pass");
     }
 
     dictReleaseIterator(di);
@@ -3196,6 +3202,27 @@ const char* getLogLevel(void) {
     return "unknown";
 }
 
+static int containsControlChars(const char *s, size_t len) {
+    for (size_t i = 0; i < len; i++) {
+        unsigned char ch = (unsigned char)s[i];
+        if (ch <= 0x1F || ch == 0x7F) return 1;
+    }
+    return 0;
+}
+
+/* Validate that arguments starting from 'first_arg' contain no control characters.
+ * Returns C_OK if all arguments are clean, or C_ERR after sending an error reply. */
+static int sentinelValidateArgs(client *c, int first_arg, const char *cmd) {
+    for (int i = first_arg; i < c->argc; i++) {
+        sds arg = c->argv[i]->ptr;
+        if (containsControlChars(arg, sdslen(arg))) {
+            addReplyErrorFormat(c, "Invalid argument at index %d for %s: control characters are not allowed", i, cmd);
+            return C_ERR;
+        }
+    }
+    return C_OK;
+}
+
 /* SENTINEL CONFIG SET option value [option value ...] */
 void sentinelConfigSetCommand(client *c) {
     long long numval;
@@ -3217,6 +3244,8 @@ void sentinelConfigSetCommand(client *c) {
         populateDict(options_dict, options);
     }
     dict *set_configs = dictCreate(&stringSetDictType);
+
+    if (sentinelValidateArgs(c, 3, "SENTINEL CONFIG SET") == C_ERR) goto exit;
 
     /* Validate arguments are valid */
     for (int i = 3; i < c->argc; i++) {
@@ -4051,6 +4080,9 @@ NULL
         char ip[NET_IP_STR_LEN];
 
         if (c->argc != 6) goto numargserr;
+
+        if (sentinelValidateArgs(c, 2, "SENTINEL MONITOR") == C_ERR) return;
+
         if (getLongFromObjectOrReply(c,c->argv[5],&quorum,"Invalid quorum")
             != C_OK) return;
         if (getLongFromObjectOrReply(c,c->argv[4],&port,"Invalid port")
@@ -4350,6 +4382,8 @@ void sentinelSetCommand(client *c) {
 
     if ((ri = sentinelGetMasterByNameOrReplyError(c,c->argv[2]))
         == NULL) return;
+
+    if (sentinelValidateArgs(c, 3, "SENTINEL SET") == C_ERR) return;
 
     /* Process option - value pairs. */
     for (j = 3; j < c->argc; j++) {

tests/sentinel/tests/03-runtime-reconf.tcl
@@ -223,3 +223,72 @@ test "Sentinel Set with other error situations" {
 
    assert_match "ERR Failed to save config file*" $err
 }
+
+test "SENTINEL SET rejects values containing control characters" {
+    # CRLF injection into auth-pass
+    assert_error "ERR Invalid argument*" {S 0 SENTINEL SET mymaster auth-pass "pass\r\nsentinel notification-script mymaster /tmp/evil.sh"}
+    # Newline in auth-user
+    assert_error "ERR Invalid argument*" {S 0 SENTINEL SET mymaster auth-user "user\ninjected"}
+    # Carriage return alone
+    assert_error "ERR Invalid argument*" {S 0 SENTINEL SET mymaster auth-pass "has\rcr"}
+    # Null byte
+    assert_error "ERR Invalid argument*" {S 0 SENTINEL SET mymaster auth-user "has\x00null"}
+    # Tab character (0x09)
+    assert_error "ERR Invalid argument*" {S 0 SENTINEL SET mymaster auth-pass "has\ttab"}
+    # DEL character (0x7F)
+    assert_error "ERR Invalid argument*" {S 0 SENTINEL SET mymaster auth-pass "has\x7F"}
+    # Control char in option name is also rejected
+    assert_error "ERR Invalid argument*" {S 0 SENTINEL SET mymaster "bad\noption" value}
+}
+
+test "SENTINEL MONITOR rejects values containing control characters" {
+    # CRLF in name
+    assert_error "ERR Invalid argument*" {S 0 SENTINEL MONITOR "bad\r\nname" 127.0.0.1 6379 2}
+    # Newline in hostname
+    assert_error "ERR Invalid argument*" {S 0 SENTINEL MONITOR testprimary "127.0.0.1\ninjected" 6379 2}
+    # Null byte in name
+    assert_error "ERR Invalid argument*" {S 0 SENTINEL MONITOR "has\x00null" 127.0.0.1 6379 2}
+    # Tab in name
+    assert_error "ERR Invalid argument*" {S 0 SENTINEL MONITOR "has\ttab" 127.0.0.1 6379 2}
+    # DEL in hostname
+    assert_error "ERR Invalid argument*" {S 0 SENTINEL MONITOR testprimary "host\x7F" 6379 2}
+}
+
+test "SENTINEL CONFIG SET rejects values containing control characters" {
+    # CRLF in announce-ip
+    assert_error "ERR Invalid argument*" {S 0 SENTINEL CONFIG SET announce-ip "1.2.3.4\r\ninjected"}
+    # Newline in sentinel-user
+    assert_error "ERR Invalid argument*" {S 0 SENTINEL CONFIG SET sentinel-user "user\ninjected"}
+    # Null byte in sentinel-pass
+    assert_error "ERR Invalid argument*" {S 0 SENTINEL CONFIG SET sentinel-pass "pass\x00word"}
+    # Tab in announce-ip
+    assert_error "ERR Invalid argument*" {S 0 SENTINEL CONFIG SET announce-ip "1.2\t.3.4"}
+    # DEL in sentinel-pass
+    assert_error "ERR Invalid argument*" {S 0 SENTINEL CONFIG SET sentinel-pass "pass\x7F"}
+}
+
+test "Config rewrite escapes special characters via sdscatrepr" {
+    set special_pass "my\"pass\\word with spaces"
+    S 0 SENTINEL SET mymaster auth-pass $special_pass
+
+    S 0 SENTINEL FLUSHCONFIG
+
+    # Verify the config file has the value properly quoted
+    set configfile [file join "sentinel_0" "sentinel.conf"]
+    set content [exec cat $configfile]
+    set expected {sentinel auth-pass mymaster "my\"pass\\word with spaces"}
+    assert {[string first $expected $content] >= 0}
+
+    # Restart sentinel and verify it reloads without error
+    restart_instance sentinel 0
+    wait_for_condition 200 50 {
+        [catch {S 0 PING}] == 0
+    } else {
+        fail "Sentinel 0 did not come back after restart"
+    }
+
+    assert_match {*} [S 0 SENTINEL GET-MASTER-ADDR-BY-NAME mymaster]
+
+    S 0 SENTINEL SET mymaster auth-pass ""
+    S 0 SENTINEL FLUSHCONFIG
+}
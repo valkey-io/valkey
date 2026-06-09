4a843c2a2514b7ac259ecec4be9cb8f98afd3a89
Su Ko <rhtn1128@gmail.com>
2026-06-09 20:27:29 +0900

    Test move cluster_raft step-down test to a non-proto suite
    Signed-off-by: Su Ko <rhtn1128@gmail.com>

---
+++ tests/unit/cluster/cluster-raft.tcl
@@ -0,0 +1,51 @@
+# Test higher-level Raft cluster behavior that does not require direct
+# wire-protocol interaction from Tcl.
+
+tags {external:skip cluster singledb} {
+
+test "Raft: leader steps down after losing quorum freshness" {
+    start_multiple_servers 3 {overrides {cluster-enabled yes cluster-protocol raft cluster-node-timeout 1000}} {
+        [srv 0 client] CLUSTER MEET [srv -1 host] [srv -1 port]
+        [srv 0 client] CLUSTER MEET [srv -2 host] [srv -2 port]
+
+        wait_for_condition 50 100 {
+            [CI 0 cluster_size] == 3 &&
+            [CI 1 cluster_size] == 3 &&
+            [CI 2 cluster_size] == 3
+        } else {
+            fail "Cluster did not form: sizes=[CI 0 cluster_size],[CI 1 cluster_size],[CI 2 cluster_size]"
+        }
+
+        set leader_idx -999
+        foreach idx {0 1 2} {
+            if {[CI $idx cluster_raft_role] eq "leader"} {
+                set leader_idx $idx
+                break
+            }
+        }
+        assert {$leader_idx != -999}
+
+        set paused [list]
+        foreach idx {0 1 2} {
+            if {$idx == $leader_idx} continue
+            pause_process [srv [expr {-$idx}] pid]
+            lappend paused $idx
+        }
+
+        wait_for_condition 100 50 {
+            [CI $leader_idx cluster_raft_role] eq "follower" &&
+            [CI $leader_idx cluster_raft_leader] eq ""
+        } else {
+            foreach idx $paused {
+                resume_process [srv [expr {-$idx}] pid]
+            }
+            fail "Leader did not step down after losing quorum freshness: role=[CI $leader_idx cluster_raft_role] leader=[CI $leader_idx cluster_raft_leader]"
+        }
+
+        foreach idx $paused {
+            resume_process [srv [expr {-$idx}] pid]
+        }
+    }
+}
+
+} ;# tags

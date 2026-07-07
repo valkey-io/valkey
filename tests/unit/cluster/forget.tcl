a60f6af51cf77ba9da22875c5d4be87bc66d5685
AlisinaDevelo <Alisinakarimi.2003@gmail.com>
2026-07-07 20:04:31 +0200

    Assert refused MEET log lines in forget test
    Signed-off-by: AlisinaDevelo <Alisinakarimi.2003@gmail.com>

--- tests/unit/cluster/forget.tcl
+++ tests/unit/cluster/forget.tcl
@@ -26,5 +26,10 @@ start_cluster 2 1 {tags {external:skip cluster} overrides {cluster-node-timeout 
             assert {[cluster_get_node_by_id 0 $forgotten_id] eq {}}
             assert {[cluster_get_node_by_id 1 $forgotten_id] eq {}}
         }
+
+        # The node stayed out because its MEET packets were refused,
+        # not because it never tried.
+        verify_log_message 0 "*Ignoring MEET packet from blacklisted node $forgotten_id*" 0
+        verify_log_message -1 "*Ignoring MEET packet from blacklisted node $forgotten_id*" 0
     }
 }

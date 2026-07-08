529a11062897b076571d186ef4e33b9c4e488119
Binbin <binloveplay1314@qq.com>
2026-07-08 10:07:11 +0800

    Fix format
    Signed-off-by: Binbin <binloveplay1314@qq.com>

--- src/cluster_legacy.h
+++ src/cluster_legacy.h
@@ -69,7 +69,7 @@ typedef struct clusterLink {
                                                                                    * myself will gossip this flag to other replica in the   \
                                                                                    * shard so that the replicas can make a better ranking   \
                                                                                    * decisions to help with the failover. */
-#define CLUSTER_NODE_MAX CLUSTER_NODE_MY_PRIMARY_FAIL                         /* Max bit for CLUSTER_NODE_* flag, update while adding a new flag. */
+#define CLUSTER_NODE_MAX CLUSTER_NODE_MY_PRIMARY_FAIL                             /* Max bit for CLUSTER_NODE_* flag, update while adding a new flag. */
 
 /* Ensure cluster node flags never silently grew beyond 16 bits.
  * The flags in clusterMsg and clusterMsgDataGossip are uint16_t. */

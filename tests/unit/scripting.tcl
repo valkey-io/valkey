dc8ff2db35e4aaf1e0ca50198a0b693b16ec55ff
Binbin <binloveplay1314@qq.com>
2026-07-16 21:07:02 +0000

    Fix aof race in shutdown nosave timedout script test (#1156)
    Ci report this failure:
    ```
    *** [err]: SHUTDOWN NOSAVE can kill a timedout script anyway in tests/unit/scripting.tcl
    Expected 'BUSY Valkey is busy running a script. *' to match '*connection refused*' (context: type eval line 8 cmd {assert_match {*connection refused*} $e} proc ::test)
    ```
    
    We can see the logs the shutdown got rejected because there is an AOFRW
    pending:
    ```
    Writing initial AOF, can't exit.
    Errors trying to shut down the server. Check the logs for more information.
    ```
    
    The reason is that the previous test enabled the aof.
    
    (cherry picked from commit e50f31ef3a7e604236e178a737b8e28590faf1c9)
    Signed-off-by: Binbin <binloveplay1314@qq.com>
    Signed-off-by: Sarthak Aggarwal <sarthagg@amazon.com>

--- tests/unit/scripting.tcl
+++ tests/unit/scripting.tcl
@@ -1414,6 +1414,7 @@ start_server {tags {"scripting"}} {
         $rd close
         $rd2 close
         $r3 close
+        r config set appendonly no
         r DEBUG set-disable-deny-scripts 0
     } {OK} {external:skip needs:debug}
 

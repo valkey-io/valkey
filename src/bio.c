valkey-io/valkey

commit 1aa8e03c7e356f732a82013d4251acc97490f9c4

Fix error "SSL routines::bad length" when connTLSWrite is called second time with smaller buffer (#1737)
Issue described in #1135

When call to `connTLSWrite` fails on next attempt `connTLSWritev` should
provide buffer with at least same amount of bytes,
but if first call was with buffer smaller than
`NET_MAX_WRITES_PER_EVENT` then buffer gets larger and exceed
`NET_MAX_WRITES_PER_EVENT`, `connTLSWritev` will not combine `iov` into
one buffer resulting into chance that first element of `iov` is smaller
than last failed call to `connTLSWrite` and causing `SSL routines::bad
length`.

This change force combining `iov`s into one buffer if first element of
`iov` is smaller than last failed write by `connTLSWrite`

---------

Signed-off-by: Marek Zoremba <marek@janeasystems.com>
Signed-off-by: Madelyn Olson <madelyneolson@gmail.com>
Signed-off-by: ranshid <ranshid@amazon.com>
Co-authored-by: Madelyn Olson <madelyneolson@gmail.com>
Co-authored-by: ranshid <ranshid@amazon.com>
Signed-off-by: Jacob Murphy <jkmurphy@google.com>

src/tls.c
@@ -453,6 +453,10 @@ typedef struct tls_connection {
     SSL *ssl;
     char *ssl_error;
     listNode *pending_list_node;
+    /* Per https://docs.openssl.org/master/man3/SSL_write, after a write call with partially written data,
+     * we must make subsequent write calls with the same length. We use this field to keep track of
+     * the previous write length. */
+    size_t last_failed_write_data_len;
 } tls_connection;
 
 static connection *createTLSConnection(int client_side) {
@@ -888,11 +892,23 @@ static int connTLSWrite(connection *conn_, const void *data, size_t data_len) {
 
     if (conn->c.state != CONN_STATE_CONNECTED) return -1;
     ERR_clear_error();
+    /* In case when last write failed due to some internal reason, retry has to provide
+     * at least the same amount of bytes (https://docs.openssl.org/master/man3/SSL_write).
+     * If that condition is not met, OpenSSL will return "SSL routines::bad length".
+     * Currently we only suspect this can happen during primary cron sending '\n'
+     * indication to the replica, so we silently return from this function without
+     * impacting the connection state. */
+    if (data_len < conn->last_failed_write_data_len) {
+        // TODO: place debugAssert for this case once the known issue described is resolved
+        return -1;
+    }
     ret = SSL_write(conn->ssl, data, data_len);
+    conn->last_failed_write_data_len = ret <= 0 ? data_len : 0;
     return updateStateAfterSSLIO(conn, ret, 1);
 }
 
 static int connTLSWritev(connection *conn_, const struct iovec *iov, int iovcnt) {
+    tls_connection *conn = (tls_connection *)conn_;
     if (iovcnt == 1) return connTLSWrite(conn_, iov[0].iov_base, iov[0].iov_len);
 
     /* Accumulate the amount of bytes of each buffer and check if it exceeds NET_MAX_WRITES_PER_EVENT. */
@@ -902,10 +918,15 @@ static int connTLSWritev(connection *conn_, const struct iovec *iov, int iovcnt)
         if (iov_bytes_len > NET_MAX_WRITES_PER_EVENT) break;
     }
 
-    /* The amount of all buffers is greater than NET_MAX_WRITES_PER_EVENT, 
-     * which is not worth doing so much memory copying to reduce system calls,
-     * therefore, invoke connTLSWrite() multiple times to avoid memory copies. */
-    if (iov_bytes_len > NET_MAX_WRITES_PER_EVENT) {
+    /* In case the amount of all buffers is greater than NET_MAX_WRITES_PER_EVENT,
+     * it might not worth doing so much memory copying to reduce system calls,
+     * therefore, invoke connTLSWrite() multiple times to avoid memory copies.
+     * However, in case when last write failed we still have to repeat sending last_failed_write_data_len
+     * bytes. Because of openssl implementation we cannot repeat sending writes with length smaller than
+     * the last failed write (https://docs.openssl.org/master/man3/SSL_write) so in case the first io buffer
+     * does not provide at least the same amount of bytes as previous failed write, we will have to fallback to
+     * memory copy to a static buffer before calling SSL_write. */
+    if (iov_bytes_len > NET_MAX_WRITES_PER_EVENT && iovcnt > 0 && iov[0].iov_len >= conn->last_failed_write_data_len) {
         ssize_t tot_sent = 0;
         for (int i = 0; i < iovcnt; i++) {
             ssize_t sent = connTLSWrite(conn_, iov[i].iov_base, iov[i].iov_len);
@@ -916,13 +937,17 @@ static int connTLSWritev(connection *conn_, const struct iovec *iov, int iovcnt)
         return tot_sent;
     }
 
-    /* The amount of all buffers is less than NET_MAX_WRITES_PER_EVENT, 
-     * which is worth doing more memory copies in exchange for fewer system calls, 
-     * so concatenate these scattered buffers into a contiguous piece of memory 
-     * and send it away by one call to connTLSWrite(). */
+    /* The amount of all buffers is less than NET_MAX_WRITES_PER_EVENT,
+     * which is worth doing more memory copies in exchange for fewer system calls,
+     * so concatenate these scattered buffers into a contiguous piece of memory
+     * and send it away by one call to connTLSWrite().
+     * However, code can fallback here in case when last write failed and first
+     * element of io is buffer not big enough to provide required amount of bytes
+     * to retry, so iov_bytes_len may exceed NET_MAX_WRITES_PER_EVENT by the amount
+     * of remaining bytes from last taken io. */
     char buf[iov_bytes_len];
     size_t offset = 0;
-    for (int i = 0; i < iovcnt; i++) {
+    for (int i = 0; i < iovcnt && offset < iov_bytes_len; i++) {
         memcpy(buf + offset, iov[i].iov_base, iov[i].iov_len);
         offset += iov[i].iov_len;
     }

commit 8ee7b372fad7acb757a3b62f22e35a1a903a72f9

Fix VLA stack overflow in TLS writev after write failure (#4307)
### Problem

The bug was originally introduced by #1737

In tls.c, we normally avoid consolidating IO vectors into a buffer if
they exceed a capped amount (`NET_MAX_WRITES_PER_EVENT`, which is 64KB).
However, on OpenSSL write errors, we must never trigger a subsequent
write that is smaller than the previous failed write. In this path, we
must consolidate the IO vectors to ensure the write is at least as large
as the last write we made on the socket:

```c
    char buf[iov_bytes_len];
    size_t offset = 0;
    for (int i = 0; i < iovcnt && offset < iov_bytes_len; i++) {
        memcpy(buf + offset, iov[i].iov_base, iov[i].iov_len);
```

However, `iov_bytes_len` can easily exceed the stack boundaries if one
of the IO vectors is large (e.g. a large key read), leading to a crash:

```
Thread 1 (Thread 0x7ffff7b65700 (LWP 191) "valkey-server"):
#0  0x00005555557a04bc in connTLSWritev (conn_=0x7ffff6e518c0, iov=<optimized out>, iovcnt=5) at src/tls.c:1713
#1  0x00005555556d2a21 in connWritev (conn=<optimized out>, iov=0x7fffffff9240, iovcnt=5) at src/connection.h:256
#2  writevToClient (c=<optimized out>) at src/networking.c:2814
#3  _writeToClient (c=<optimized out>) at src/networking.c:2868
#4  0x00005555556d326c in writeToClient (c=0x7ffff6e7a780) at src/networking.c:3117
#5  writeToClient (c=0x7ffff6e7a780) at src/networking.c:3108
#6  sendReplyToClient (conn=<optimized out>) at src/networking.c:3127
#7  0x00005555557a0ffd in callHandler (conn=0x7ffff6e518c0, handler=<optimized out>) at src/connhelpers.h:79
#8  tlsHandleEvent (conn=0x7ffff6e518c0, mask=<optimized out>) at src/tls.c:1507
#9  0x00005555555fbad5 in aeProcessEvents (flags=27, eventLoop=0x7ffff6e45f80) at src/ae.c:504
#10 aeMain (eventLoop=0x7ffff6e45f80) at src/ae.c:543
#11 0x00005555555eb83a in main (argc=2, argv=0x7fffffffd588) at src/server.c:7833
```

### Fix

To fix this, we simply avoid allocating large buffers on the stack by

src/bio.c
@@ -150,14 +150,10 @@ __attribute__((noinline)) static bio_job *allocBioJob(size_t extra) {
     return zmalloc(sizeof(bio_job) + extra);
 }
 
-/* Make sure we have enough stack to perform all the things we do in the
- * main thread. */
-#define VALKEY_THREAD_STACK_SIZE (1024 * 1024 * 4)
 
 /* Initialize the background system, spawning the thread. */
 void bioInit(void) {
     pthread_attr_t attr;
-    size_t stacksize;
 
     /* Initialization of state vars and objects */
     for (bio_worker_data *bwd = bio_workers; bwd != bio_worker_end; ++bwd) {
@@ -165,11 +161,7 @@ void bioInit(void) {
     }
 
     /* Set the stack size as by default it may be small in some system */
-    pthread_attr_init(&attr);
-    pthread_attr_getstacksize(&attr, &stacksize);
-    if (!stacksize) stacksize = 1; /* The world is full of Solaris Fixes */
-    while (stacksize < VALKEY_THREAD_STACK_SIZE) stacksize *= 2;
-    pthread_attr_setstacksize(&attr, stacksize);
+    serverInitThreadAttribute(&attr);
 
     /* Ready to spawn our threads. We use the single argument the thread
      * function accepts in order to pass a pointer to the data that the
@@ -181,6 +173,7 @@ void bioInit(void) {
             exit(1);
         }
     }
+    pthread_attr_destroy(&attr);
 }
 
 void bioSubmitJob(int type, bio_job *job) {

src/debug.c
@@ -40,6 +40,8 @@
 #include "sds.h"
 #include "module.h"
 
+#include "tls.h"
+
 #include <arpa/inet.h>
 #include <signal.h>
 #include <dlfcn.h>
@@ -537,6 +539,8 @@ void debugCommand(client *c) {
             "    Force freeClient on primary to use async path.",
             "PROTECT-CLIENT <id>",
             "    Protect a client from being freed, forcing deferred close.",
+            "FORCE-TLS-WRITE-ERROR <0|1>",
+            "    Force TLS write error for testing.",
             NULL};
         addExtendedReplyHelp(c, help, clusterDebugCommandExtendedHelp());
     } else if (!strcasecmp(objectGetVal(c->argv[1]), "segfault")) {
@@ -1099,6 +1103,19 @@ void debugCommand(client *c) {
         } else {
             addReplyError(c, "No such client");
         }
+    } else if (!strcasecmp(objectGetVal(c->argv[1]), "force-tls-write-error") && c->argc == 3) {
+#ifdef USE_OPENSSL
+        long val;
+        if (getLongFromObjectOrReply(c, c->argv[2], &val, NULL) != C_OK) return;
+        if (val < 0 || val > 1) {
+            addReplyError(c, "Value must be 0 or 1");
+            return;
+        }
+        server.debug_force_tls_write_error = val;
+        addReply(c, shared.ok);
+#else
+        addReplyError(c, "TLS is not enabled");
+#endif
     } else if (!handleDebugClusterCommand(c)) {
         addReplySubcommandSyntaxError(c);
         return;

src/io_threads.c
@@ -393,7 +393,12 @@ static void createIOThread(int id) {
     pthread_t tid;
     pthread_mutex_init(&io_threads_mutex[id], NULL);
     pthread_mutex_lock(&io_threads_mutex[id]); /* Thread will be stopped. */
-    int err = pthread_create(&tid, NULL, IOThreadMain, (void *)(long)id);
+
+    pthread_attr_t attr;
+    serverInitThreadAttribute(&attr);
+
+    int err = pthread_create(&tid, &attr, IOThreadMain, (void *)(long)id);
+    pthread_attr_destroy(&attr);
     if (err) {
         serverLog(LL_WARNING, "Fatal: Can't initialize IO thread, pthread_create failed with: %s", strerror(err));
         exit(1);

src/server.c
@@ -2354,6 +2354,7 @@ void initServerConfig(void) {
     server.page_size = sysconf(_SC_PAGESIZE);
     server.extended_redis_compat = 0;
     server.pause_cron = 0;
+    server.debug_force_tls_write_error = 0;
     server.dict_resizing = 1;
     server.import_mode = 0;
 
@@ -7497,6 +7498,17 @@ int serverIsSupervised(int mode) {
 int iAmPrimary(void) {
     return ((!server.cluster_enabled && server.primary_host == NULL) ||
             (server.cluster_enabled && clusterNodeIsPrimary(getMyClusterNode())));
+}
+
+/* Initialize thread attribute with a guarded stack size.
+ * Doubling the stack size until it is at least VALKEY_THREAD_STACK_SIZE. */
+void serverInitThreadAttribute(pthread_attr_t *attr) {
+    size_t stacksize;
+    pthread_attr_init(attr);
+    pthread_attr_getstacksize(attr, &stacksize);
+    if (!stacksize) stacksize = 1;
+    while (stacksize < VALKEY_THREAD_STACK_SIZE) stacksize *= 2;
+    pthread_attr_setstacksize(attr, stacksize);
 }
 
 /* Main is marked as weak so that unit tests can use their own main function. */

src/server.h
@@ -142,6 +142,7 @@ struct ValkeyModule;
 #define CRON_DBS_PER_CALL 16
 #define CRON_DICTS_PER_DB 16
 #define NET_MAX_WRITES_PER_EVENT (1024 * 64)
+#define VALKEY_THREAD_STACK_SIZE (1024 * 1024 * 4)
 #define PROTO_SHARED_SELECT_CMDS 10
 #define OBJ_SHARED_INTEGERS 10000
 #define OBJ_SHARED_BULKHDR_LEN 32
@@ -2401,6 +2402,7 @@ struct valkeyServer {
     /* Local environment */
     char *locale_collate;
     char *debug_context; /* A free-form string that has no impact on server except being included in a crash report. */
+    int debug_force_tls_write_error;
 };
 
 #define MAX_KEYS_BUFFER 256
@@ -4337,6 +4339,7 @@ void debugPauseProcess(void);
 #define serverDebug(fmt, ...) printf("DEBUG %s:%d > " fmt "\n", __FILE__, __LINE__, __VA_ARGS__)
 #define serverDebugMark() printf("-- MARK %s:%d --\n", __FILE__, __LINE__)
 
+void serverInitThreadAttribute(pthread_attr_t *attr);
 int iAmPrimary(void);
 
 #define STRINGIFY_(x) #x

src/tls.c
@@ -1662,6 +1662,14 @@ static int connTLSWrite(connection *conn_, const void *data, size_t data_len) {
     int ret;
 
     if (conn->c.state != CONN_STATE_CONNECTED) return -1;
+    if (server.debug_force_tls_write_error) {
+        conn->last_failed_write_data_len = data_len;
+        conn->flags &= ~(TLS_CONN_FLAG_WRITE_WANT_READ | TLS_CONN_FLAG_READ_WANT_WRITE);
+        conn->flags |= TLS_CONN_FLAG_READ_WANT_WRITE;
+        updateSSLEvent(conn);
+        errno = EAGAIN;
+        return -1;
+    }
     ERR_clear_error();
     /* In case when last write failed due to some internal reason, retry has to provide
      * at least the same amount of bytes (https://docs.openssl.org/master/man3/SSL_write).
@@ -1682,11 +1690,10 @@ static int connTLSWritev(connection *conn_, const struct iovec *iov, int iovcnt)
     tls_connection *conn = (tls_connection *)conn_;
     if (iovcnt == 1) return connTLSWrite(conn_, iov[0].iov_base, iov[0].iov_len);
 
-    /* Accumulate the amount of bytes of each buffer and check if it exceeds NET_MAX_WRITES_PER_EVENT. */
-    size_t iov_bytes_len = 0;
+    /* Accumulate the total amount of bytes of all buffers. */
+    size_t total_len = 0;
     for (int i = 0; i < iovcnt; i++) {
-        iov_bytes_len += iov[i].iov_len;
-        if (iov_bytes_len > NET_MAX_WRITES_PER_EVENT) break;
+        total_len += iov[i].iov_len;
     }
 
     /* In case the amount of all buffers is greater than NET_MAX_WRITES_PER_EVENT,
@@ -1697,7 +1704,7 @@ static int connTLSWritev(connection *conn_, const struct iovec *iov, int iovcnt)
      * the last failed write (https://docs.openssl.org/master/man3/SSL_write) so in case the first io buffer
      * does not provide at least the same amount of bytes as previous failed write, we will have to fallback to
      * memory copy to a static buffer before calling SSL_write. */
-    if (iov_bytes_len > NET_MAX_WRITES_PER_EVENT && iovcnt > 0 && iov[0].iov_len >= conn->last_failed_write_data_len) {
+    if (total_len > NET_MAX_WRITES_PER_EVENT && iovcnt > 0 && iov[0].iov_len >= conn->last_failed_write_data_len) {
         ssize_t tot_sent = 0;
         for (int i = 0; i < iovcnt; i++) {
             ssize_t sent = connTLSWrite(conn_, iov[i].iov_base, iov[i].iov_len);
@@ -1708,21 +1715,36 @@ static int connTLSWritev(connection *conn_, const struct iovec *iov, int iovcnt)
         return tot_sent;
     }
 
-    /* The amount of all buffers is less than NET_MAX_WRITES_PER_EVENT,
-     * which is worth doing more memory copies in exchange for fewer system calls,
-     * so concatenate these scattered buffers into a contiguous piece of memory
-     * and send it away by one call to connTLSWrite().
-     * However, code can fallback here in case when last write failed and first
-     * element of io is buffer not big enough to provide required amount of bytes
-     * to retry, so iov_bytes_len may exceed NET_MAX_WRITES_PER_EVENT by the amount
-     * of remaining bytes from last taken io. */
-    char buf[iov_bytes_len];
-    size_t offset = 0;
-    for (int i = 0; i < iovcnt && offset < iov_bytes_len; i++) {
-        memcpy(buf + offset, iov[i].iov_base, iov[i].iov_len);
-        offset += iov[i].iov_len;
+    /* We concatenate scattered buffers into a contiguous piece of memory
+     * and send it away by one call to connTLSWrite() to reduce system calls.
+     * To avoid stack overflow (VLA) and heap allocation, we use a fixed-size buffer
+     * of NET_MAX_WRITES_PER_EVENT and copy only up to this limit. The remaining
+     * data will be sent in subsequent socket writable events (partial writes). */
+    char buf[NET_MAX_WRITES_PER_EVENT];
+    size_t to_write = 0;
+
+    for (int i = 0; i < iovcnt && to_write < NET_MAX_WRITES_PER_EVENT; i++) {
+        size_t available = NET_MAX_WRITES_PER_EVENT - to_write;
+        size_t copy_len = iov[i].iov_len;
+        if (copy_len > available) {
+            copy_len = available;
+        }
+        memcpy(buf + to_write, iov[i].iov_base, copy_len);
+        to_write += copy_len;
     }
-    return connTLSWrite(conn_, buf, iov_bytes_len);
+
+    /* Verify OpenSSL retry constraint: we must have copied at least the amount
+     * of bytes that failed in the previous write attempt. */
+    if (to_write < conn->last_failed_write_data_len) {
+        serverLog(LL_WARNING, "connTLSWritev: cannot satisfy last_failed_write_data_len (%zu < %zu)",
+                  to_write, conn->last_failed_write_data_len);
+        conn->c.last_errno = EIO;
+        conn->c.state = CONN_STATE_ERROR;
+        errno = EIO;
+        return -1;
+    }
+
+    return connTLSWrite(conn_, buf, to_write);
 }
 
 static int connTLSRead(connection *conn_, void *buf, size_t buf_len) {

tests/unit/tls.tcl
@@ -2,6 +2,18 @@ start_server {tags {"tls"}} {
     if {$::tls} {
         package require tls
 
+        proc check_client_stuck {control_client client_port {min_omem 1}} {
+            set clients [$control_client CLIENT LIST]
+            foreach client [split $clients "\n"] {
+                if {[regexp "addr=127.0.0.1:$client_port" $client]} {
+                    if {[regexp {omem=([0-9]+)} $client -> omem]} {
+                        return [expr {$omem >= $min_omem}]
+                    }
+                }
+            }
+            return 0
+        }
+
         test {TLS: Not accepting non-TLS connections on a TLS port} {
             set s [valkey [srv 0 host] [srv 0 port]]
             catch {$s PING} e
@@ -801,6 +813,107 @@ start_server {tags {"tls"}} {
 
             $tls_client close
             $plain_client close
+        }
+
+        test {TLS: connTLSWritev stack overflow crash reproduction} {
+            # Regression test for a bug where a previously failed OpenSSL write for a
+            # small server response would trigger a stack overflow if immediately
+            # followed by any large server response.
+            # Prepare data on server
+            # We use a control client (TCP) to avoid TLS write errors on control connection
+            set plain_port [srv 0 pport]
+            set control_client [valkey [srv 0 host] $plain_port]
+            
+            $control_client SELECT 0
+            $control_client SET large_key [string repeat "A" 10485760] ;# 10MB
+            $control_client SET small_key [string repeat "B" 10240]    ;# 10KB
+            
+            # Connect raw TLS client
+            set fd [::tls::socket [srv 0 host] [srv 0 port]]
+            fconfigure $fd -translation binary -blocking 1
+            
+            # 1. Enable forced TLS write errors globally
+            assert_equal OK [$control_client DEBUG FORCE-TLS-WRITE-ERROR 1]
+            
+            # 2. Send Batch 1 on TLS client: 2x GET small_key
+            # They will be combined by server and fail to write, setting last_failed to ~20KB
+            set payload1 ""
+            append payload1 "*2\r\n\$3\r\nGET\r\n\$9\r\nsmall_key\r\n"
+            append payload1 "*2\r\n\$3\r\nGET\r\n\$9\r\nsmall_key\r\n"
+            puts -nonewline $fd $payload1
+            flush $fd
+            
+            # Get local port of raw client
+            set client_port [lindex [fconfigure $fd -sockname] 2]
+            
+            # Wait until the server has accumulated the replies for Batch 1
+            # and is stuck (omem > 0).
+            wait_for_condition 50 100 {
+                [check_client_stuck $control_client $client_port]
+            } else {
+                fail "Timeout waiting for client replies to stack up"
+            }
+            
+            # 3. Send Batch 2 on TLS client: GET large_key
+            # This is appended to the reply list
+            set payload2 ""
+            append payload2 "*2\r\n\$3\r\nGET\r\n\$9\r\nlarge_key\r\n"
+            puts -nonewline $fd $payload2
+            flush $fd
+            
+            # Wait until the server has processed Batch 2 and queued the large reply.
+            # Total expected omem is at least 10MB.
+            wait_for_condition 50 100 {
+                [check_client_stuck $control_client $client_port 10000000]
+            } else {
+                fail "Timeout waiting for large key reply to be queued"
+            }
+            
+            # 4. Disable forced TLS write errors globally
+            # This will trigger the server to resume writing to the TLS client.
+            # It will call connTLSWritev with iov[0].iov_len (10KB) < last_failed (20KB),
+            # and iov_bytes_len > 64KB (due to large_key), triggering the fallback path.
+            assert_equal OK [$control_client DEBUG FORCE-TLS-WRITE-ERROR 0]
+            
+            # 5. Read replies from TLS client.
+            # If the server crashed, this will fail with I/O error.
+            # We expect:
+            # - Reply 1: 10KB of 'B's (plus protocol helper)
+            # - Reply 2: 10KB of 'B's (plus protocol helper)
+            # - Reply 3: 10MB of 'A's (plus protocol helper)
+            # Total expected bytes:
+            # small_key reply: "$10240\r\n" (8 bytes) + 10240 bytes + "\r\n" (2 bytes) = 10250 bytes
+            # large_key reply: "$10485760\r\n" (11 bytes) + 10485760 bytes + "\r\n" (2 bytes) = 10485773 bytes
+            # Total = 10250 + 10250 + 10485773 = 10506273 bytes
+            # Let's just read the expected number of bytes.
+            
+            set expected_bytes [expr {10250 + 10250 + 10485773}]
+            set got 0
+            set data ""
+            while {$got < $expected_bytes} {
+                set chunk [read $fd [expr {$expected_bytes - $got}]]
+                if {[string length $chunk] == 0} {
+                    if {[eof $fd]} {
+                        error "EOF reached before reading all bytes"
+                    }
+                    # Keep trying if not EOF
+                    after 10
+                    continue
+                }
+                incr got [string length $chunk]
+                # We only keep the last 100 bytes to check integrity without using too much memory
+                append data $chunk
+                if {[string length $data] > 100} {
+                    set data [string range $data end-99 end]
+                }
+            }
+            
+            # Assert we got everything and the tail is correct (ends with 'A's + \r\n)
+            assert_equal $expected_bytes $got
+            assert_match "*[string repeat "A" 80]\r\n" $data
+            
+            close $fd
+            $control_client close
         }
     }
 }
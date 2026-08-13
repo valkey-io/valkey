917de6ac19a63d7c9aa5d57b8151e4d07c424862
michellee-10 <michellee.3104@gmail.com>
2026-08-13 14:41:01 -0700

    Log EXEC in commandlog (#4267)
    Log EXEC in commandlog.  Catches cases where there's no individually slow command.
    ---------
    
    Signed-off-by: Michelle Lee <michellee.3104@gmail.com>

--- tests/unit/commandlog.tcl
+++ tests/unit/commandlog.tcl
@@ -291,17 +291,42 @@ start_server {tags {"commandlog"} overrides {commandlog-execution-slower-than 10
         lindex $e 3
     } {sadd set foo {AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA... (1 more bytes)}}
 
-    test {COMMANDLOG slow - EXEC is not logged, just executed commands} {
+    test {COMMANDLOG slow - EXEC is logged alongside slow inner commands} {
         r config set commandlog-execution-slower-than 100000
         r commandlog reset slow
         assert_equal [r commandlog len slow] 0
         r multi
         r debug sleep 0.2
         r exec
+        assert_equal [r commandlog len slow] 2
+        set entries [r commandlog get -1 slow]
+        assert_equal [lindex [lindex $entries 0] 3] {exec}
+        assert_equal [lindex [lindex $entries 1] 3] {debug sleep 0.2}
+    } {} {needs:debug}
+
+    test {COMMANDLOG slow - EXEC records total transaction time when inner commands are individually fast} {
+        r config set commandlog-execution-slower-than 100000
+        r commandlog reset slow
+        r multi
+        for {set i 0} {$i < 10} {incr i} {
+            r debug sleep 0.03
+        }
+        r exec
         assert_equal [r commandlog len slow] 1
         set e [lindex [r commandlog get -1 slow] 0]
-        assert_equal [lindex $e 3] {debug sleep 0.2}
+        assert_equal [lindex $e 3] {exec}
+        assert {[lindex $e 2] >= 100000}
     } {} {needs:debug}
+
+    test {COMMANDLOG slow - EXEC is not logged when transaction is below threshold} {
+        r config set commandlog-execution-slower-than 100000
+        r commandlog reset slow
+        r multi
+        r set foo bar
+        r get foo
+        r exec
+        assert_equal [r commandlog len slow] 0
+    }
 
     test {COMMANDLOG slow - can clean older entries} {
         r client setname lastentry_client

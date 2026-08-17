88376f6b47629fde746c27f4d669ca4ba36ed088
Viktor Söderqvist <viktor.soderqvist@est.tech>
2026-08-17 23:03:09 +0200

    Update src/aof.c
    Signed-off-by: Viktor Söderqvist <viktor.soderqvist@est.tech>

--- src/aof.c
+++ src/aof.c
@@ -1494,7 +1494,7 @@ struct client *createAOFClient(void) {
     c->id = CLIENT_ID_AOF; /* So modules can identify it's the AOF client. */
 
     /* The AOF client is not subject to ACL checks because all commands present
-       in the AOF file must be replayed. */
+     * in the AOF file must be replayed. */
     c->user = NULL;
 
     /*

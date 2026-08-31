d849e188b95b6e927f8df1dd82fd97dc6f782589
Binbin <binloveplay1314@qq.com>
2026-08-31 15:21:10 +0800

    Include module version in module load log message
    The module load log lines only reported the module name (and the path
    it was loaded from). Also log the module version, so it is easy to tell
    which version of a module was loaded.
    
    Signed-off-by: Binbin <binloveplay1314@qq.com>

--- src/module.c
+++ src/module.c
@@ -13476,9 +13476,9 @@ static int moduleInitPostOnLoadResolved(ModuleLoadFunc onload,
         ACLRecomputeCommandBitsFromCommandRulesAllUsers();
     }
     if (is_static) {
-        serverLog(LL_NOTICE, "Static Module '%s' successfully loaded", ctx.module->name);
+        serverLog(LL_NOTICE, "Static Module '%s' (version %d) successfully loaded", ctx.module->name, ctx.module->ver);
     } else {
-        serverLog(LL_NOTICE, "Module '%s' loaded from %s", ctx.module->name, display_name);
+        serverLog(LL_NOTICE, "Module '%s' (version %d) loaded from %s", ctx.module->name, ctx.module->ver, display_name);
     }
     ctx.module->onload = 0;
 

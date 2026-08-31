a7553292d4eb7ab58a76485bbc6536bbf2308f65
Binbin <binloveplay1314@qq.com>
2026-08-31 16:09:22 +0800

    code review from bot
    Signed-off-by: Binbin <binloveplay1314@qq.com>

--- src/module.c
+++ src/module.c
@@ -13476,9 +13476,9 @@ static int moduleInitPostOnLoadResolved(ModuleLoadFunc onload,
         ACLRecomputeCommandBitsFromCommandRulesAllUsers();
     }
     if (is_static) {
-        serverLog(LL_NOTICE, "Static Module '%s' (version %d) successfully loaded", ctx.module->name, ctx.module->ver);
+        serverLog(LL_NOTICE, "Static Module '%s' successfully loaded (version %d)", ctx.module->name, ctx.module->ver);
     } else {
-        serverLog(LL_NOTICE, "Module '%s' (version %d) loaded from %s", ctx.module->name, ctx.module->ver, display_name);
+        serverLog(LL_NOTICE, "Module '%s' loaded from %s (version %d)", ctx.module->name, display_name, ctx.module->ver);
     }
     ctx.module->onload = 0;
 

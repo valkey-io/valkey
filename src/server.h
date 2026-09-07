4309b537c93d4ae197e794b92d4866d7e57e9d7b
Bara' Hasheesh <bara.hasheesh@gmail.com>
2026-09-07 14:25:19 +0000

    Fix memory holes in serverCommand
    Signed-off-by: Bara' Hasheesh <bara.hasheesh@gmail.com>

--- src/server.h
+++ src/server.h
@@ -2754,31 +2754,38 @@ typedef int *commandDbIdArgs(robj **argv, int argc, int *count);
  *    TYPE, EXPIRE*, PEXPIRE*, TTL, PTTL, ...
  */
 struct serverCommand {
-    /* Declarative data */
-    const char *declared_name;    /* A string representing the command declared_name.
-                                   * It is a const char * for native commands and SDS for module commands. */
-    const char *summary;          /* Summary of the command (optional). */
-    const char *complexity;       /* Complexity description (optional). */
-    const char *since;            /* Debut version of the command (optional). */
-    int doc_flags;                /* Flags for documentation (see CMD_DOC_*). */
-    const char *replaced_by;      /* In case the command is deprecated, this is the successor command. */
-    const char *deprecated_since; /* In case the command is deprecated, when did it happen? */
-    serverCommandGroup group;     /* Command group */
-    commandHistory *history;      /* History of the command */
-    int num_history;
-    const char **tips; /* An array of strings that are meant to be tips for clients/proxies regarding this command */
-    int num_tips;
+    /* Declarative data. */
+    const char *declared_name;      /* A string representing the command declared_name.
+                                     * It is a const char * for native commands and SDS for module commands. */
+    const char *summary;            /* Summary of the command (optional). */
+    const char *complexity;         /* Complexity description (optional). */
+    const char *since;              /* Debut version of the command (optional). */
+    const char *replaced_by;        /* In case the command is deprecated, this is the successor command. */
+    const char *deprecated_since;   /* In case the command is deprecated, when did it happen? */
+    commandHistory *history;        /* History of the command */
+    const char **tips;              /* An array of strings that are meant to be tips for clients/proxies regarding this command */
     serverCommandProc *proc;        /* Command implementation */
-    int arity;                      /* Number of arguments, it is possible to use -N to say >= N */
     uint64_t flags;                 /* Command flags, see CMD_*. */
     uint64_t acl_categories;        /* ACl categories, see ACL_CATEGORY_*. */
     commandDbIdArgs *get_dbid_args; /* Function to get database IDs used by this command */
     keySpec *key_specs;
-    int key_specs_num;
     /* Use a function to determine keys arguments in a command line.
      * Used for Cluster redirect (may be NULL) */
     serverGetKeysProc *getkeys_proc;
+    int doc_flags;            /* Flags for documentation (see CMD_DOC_*). */
+    serverCommandGroup group; /* Command group */
+    int num_history;
+    int num_tips;
+    int arity; /* Number of arguments, it is possible to use -N to say >= N */
+    int key_specs_num;
     int num_args; /* Length of args array. */
+    int id;       /* Command ID. This is a progressive ID starting from 0 that
+                     is assigned at runtime, and is used in order to check
+                     ACLs. A connection is able to execute a given command if
+                     the user associated to the connection has this command
+                     bit set in the bitmap of allowed commands.
+                     Placed here (rather than with the other runtime data) so it
+                     fills the padding that would otherwise follow the ints above. */
     /* Array of subcommands (may be NULL) */
     struct serverCommand *subcommands;
     /* Array of arguments (may be NULL) */
@@ -2790,11 +2797,6 @@ struct serverCommand {
 
     /* Runtime populated data */
     long long microseconds, calls, rejected_calls, failed_calls;
-    int id;           /* Command ID. This is a progressive ID starting from 0 that
-                         is assigned at runtime, and is used in order to check
-                         ACLs. A connection is able to execute a given command if
-                         the user associated to the connection has this command
-                         bit set in the bitmap of allowed commands. */
     sds fullname;     /* Includes parent name if any: "parentcmd|childcmd". Unchanged if command is renamed. */
     sds current_name; /* Same as fullname, becomes a separate string if command is renamed. */
     struct hdr_histogram

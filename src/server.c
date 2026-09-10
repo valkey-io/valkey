8d50562a31050ebbbb748f3f9654207c46cc3473
Gavin D'Mello <dmellogavin5000@gmail.com>
2026-09-10 21:18:31 +0100

    safety check for incrby value
    Co-authored-by: Jacob Murphy <jkmurphy@google.com>
    Signed-off-by: Gavin D'Mello <dmellogavin5000@gmail.com>

--- src/server.c
+++ src/server.c
@@ -8276,7 +8276,7 @@ int parseExtendedCommandArgumentsOrReply(client *c, int command_type, int start_
                    !(*flags & ARGS_BYINT) && !(*flags & ARGS_BYFLOAT) && next)
         {
             *flags |= ARGS_BYFLOAT;
-            *incrby_val = next;
+            if (incrby_val) *incrby_val = next;
             j++;
         } else {
             addReplyErrorObject(c, shared.syntaxerr);

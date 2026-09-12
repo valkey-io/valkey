9d14f10ec192985e26b20f33458834ad9aa5abe4
Gavin D'Mello <dmellogavin5000@gmail.com>
2026-09-12 14:04:19 +0100

    Update  increx docstring
    Co-authored-by: Jacob Murphy <jkmurphy@google.com>
    Signed-off-by: Gavin D'Mello <dmellogavin5000@gmail.com>

--- src/server.c
+++ src/server.c
@@ -8129,7 +8129,6 @@ __attribute__((weak)) int main(int argc, char **argv) {
  * Common command extended options - EX/EXAT/PX/PXAT/KEEPTTL
  *
  * Function takes pointers to client, flags, unit, expire_idx, pointer to pointer of expire obj,
- * Function takes pointers to client, flags, unit, expire_idx, pointer to pointer of expire obj,
  * pointer to pointer of compare obj, pointer to pointer of incrby obj, and command_type
  * which can be COMMAND_*.
  *

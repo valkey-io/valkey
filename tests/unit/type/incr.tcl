8afaa2f7dec92f4df607bb6d7382ec481b440453
Jacob Murphy <jkmurphy@google.com>
2026-09-17 00:48:33 +0000

    Verify PERSIST emits no notification when key has no TTL
    Assert that INCREX ... PERSIST on a key without an active TTL emits
    only the incrby notification and does not emit a persist notification.
    
    Signed-off-by: Jacob Murphy <jkmurphy@google.com>

--- tests/unit/type/incr.tcl
+++ tests/unit/type/incr.tcl
@@ -198,6 +198,13 @@ start_server {tags {"incr"}} {
         assert_equal "pmessage * __keyevent@${db}__:incrby foo" [$rd read]
         assert_equal "pmessage * __keyspace@${db}__:foo persist" [$rd read]
         assert_equal "pmessage * __keyevent@${db}__:persist foo" [$rd read]
+        # PERSIST on a key without TTL does not emit a persist event.
+        r increx foo byint 1 persist
+        assert_equal "pmessage * __keyspace@${db}__:foo incrby" [$rd read]
+        assert_equal "pmessage * __keyevent@${db}__:incrby foo" [$rd read]
+        r del foo
+        assert_equal "pmessage * __keyspace@${db}__:foo del" [$rd read]
+        assert_equal "pmessage * __keyevent@${db}__:del foo" [$rd read]
         $rd close
         r config set notify-keyspace-events ""
     }
